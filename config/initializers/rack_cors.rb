1aa645ad768e0a6f6a5a52024186c3748d22d0f1
Steve Kenworthy <steveyken@gmail.com>
2014-07-12 17:38:24 +0800

    Locking CORS access down

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use Rack::Cors do
   allow do
-    origins '*'
+    origins 'app.goodcity.hk', 'localhost:3000', 'localhost:4200'
     resource '*', headers: :any,
                   methods: [:get, :post, :put, :delete, :options]
   end
