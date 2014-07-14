8f4a2046dfe504fcf0a21543b06643044d8ae1cf
swatijadhav <swati.jadhav@kiprosh.com>
2014-07-14 19:47:56 +0530

    store image using public-image-id

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,27 +1,17 @@
 module Api::V1
   class ItemsController < Api::V1::ApiController
+    include CloudinaryHelper
 
     load_and_authorize_resource :item, parent: false
-
-    # /items?ids=1,2,3,4
-    def index
-      if params[:ids].present?
-        @items = @items.find( params[:ids].split(",") )
-      end
-      render json: @items, each_serializer: serializer
-    end
 
     def create
       @item.attributes = item_params
       if @item.save
+        store_images
         render json: @item, serializer: serializer, status: 201
       else
         render json: @item.errors.to_json, status: 500
       end
-    end
-
-    def show
-      render json: @item, serializer: serializer
     end
 
     def generate_cloudinary_signature
@@ -35,15 +25,34 @@ module Api::V1
         timestamp: unix_timestamp }.to_json
     end
 
+    # /items?ids=1,2,3,4
+    def index
+      if params[:ids].present?
+        @items = @items.find( params[:ids].split(",") )
+      end
+      render json: @items, each_serializer: serializer
+    end
+
+    def show
+      render json: @item, serializer: serializer
+    end
+
     private
+
+    def item_params
+      params.require(:item).permit(:donor_description, :donor_condition,
+        :state, :offer_id, :item_type_id, :rejection_reason_id,
+        :rejection_other_reason)
+    end
 
     def serializer
       Api::V1::ItemSerializer
     end
 
-    def item_params
-      params.require(:item).permit(:donor_description, :donor_condition, :state, :offer_id,
-        :item_type_id, :rejection_reason_id, :rejection_other_reason)
+    def store_images
+      params[:item][:image_identifiers].split(',').each do |img|
+        Image.create(remote_image_url: cl_image_path(img), parent: @item)
+      end
     end
 
   end
