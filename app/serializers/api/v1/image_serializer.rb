9c2cdcf9eb2208d430342616faf8af8bc3b56c92
swatijadhav <swati.jadhav@kiprosh.com>
2014-07-16 18:28:00 +0530

    add attributes to image serializer

--- app/serializers/api/v1/image_serializer.rb
+++ app/serializers/api/v1/image_serializer.rb
@@ -3,7 +3,7 @@ module Api::V1
   class ImageSerializer < ActiveModel::Serializer
     embed :ids, include: true
 
-    attributes :id, :image_url, :thumb_image_url, :favourite, :order
+    attributes :id, :image_url, :thumb_image_url, :standard_image_url, :favourite, :order
 
     def image_url
       object.image.url
@@ -11,6 +11,10 @@ module Api::V1
 
     def thumb_image_url
       object.image.thumbnail.url
+    end
+
+    def standard_image_url
+      object.image.standard.url
     end
   end
 end
