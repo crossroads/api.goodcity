3250a5c2635587904074bc7bf5f64ab7efdbd136
swatijadhav <swati.jadhav@kiprosh.com>
2014-07-22 17:46:33 +0530

    update image public id at creation of image

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,12 @@ class Image < ActiveRecord::Base
 
   belongs_to :parent, polymorphic: true
 
+  before_create :set_image_public_id
+
+  private
+
+  def set_image_public_id
+    self.image = "v" + self.image
+  end
+
 end
