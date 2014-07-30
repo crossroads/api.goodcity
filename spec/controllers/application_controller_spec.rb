591c511a100bc8ae9d8725deba5b93e2bd9dfbcc
Shivani Thakur <shivanibhanwal@gmail.com>
2014-07-30 17:10:37 +0530

    Added controller macro set_locale(change_to_language, default_language='en') to set the locate in the controllers

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,17 +5,19 @@ RSpec.describe ApplicationController, :type => :controller do
   context "set_locale" do
 
     it "should set locale to zh-tw" do
-      I18n.locale = 'en'
-      request.env['HTTP_ACCEPT_LANGUAGE'] = 'zh-tw'
-      HttpAcceptLanguage::Middleware.new(lambda {|env| env }).call(request.env)
+      set_locale('zh-tw')
+      # I18n.locale = 'en'
+      # request.env['HTTP_ACCEPT_LANGUAGE'] = 'zh-tw'
+      # HttpAcceptLanguage::Middleware.new(lambda {|env| env }).call(request.env)
       controller.send(:set_locale)
       expect(I18n.locale).to eql(:'zh-tw')
     end
 
     it "should set locale to en" do
-      I18n.locale = 'zh-tw'
-      request.env['HTTP_ACCEPT_LANGUAGE'] = 'en'
-      HttpAcceptLanguage::Middleware.new(lambda {|env| env }).call(request.env)
+      set_locale('en', 'zh-tw')
+      # I18n.locale = 'zh-tw'
+      # request.env['HTTP_ACCEPT_LANGUAGE'] = 'en'
+      # HttpAcceptLanguage::Middleware.new(lambda {|env| env }).call(request.env)
       controller.send(:set_locale)
       expect(I18n.locale).to eql(:en)
     end
