da6ffd6f02659f1b641d7e6d04e7bb38231ad289
swatijadhav <swati.jadhav@kiprosh.com>
2014-07-31 20:13:25 +0530

    delete image from cloudinary

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
 class Image < ActiveRecord::Base
 
   belongs_to :parent, polymorphic: true
+  before_destroy :delete_image_from_cloudinary
 
   scope :get_favourite, -> { where(favourite: true).first }
 
@@ -12,4 +13,11 @@ class Image < ActiveRecord::Base
     update_column(:favourite, false)
   end
 
+  private
+
+  def delete_image_from_cloudinary
+    public_id = image.split('/').last.split('.').first rescue nil
+    Cloudinary::Api.delete_resources([public_id]) if public_id
+    true
+  end
 end
