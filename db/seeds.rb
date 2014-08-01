fa1e62ba361426651d53832df95c36535fd8761c
Steve Kenworthy <steveyken@gmail.com>
2014-08-01 15:20:31 +0800

    Donor conditions should be generated in the correct order

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+donor_conditions = YAML.load_file("#{Rails.root}/db/donor_conditions.yml")
+donor_conditions.each do |name, value|
+  FactoryGirl.create :donor_condition, name: name, name_zh_tw: value[:name_zh_tw]
+end
+
 item_types = YAML.load_file("#{Rails.root}/db/item_types.yml")
 item_types.each do |code, value|
   FactoryGirl.create :item_type, code: code, name_en: value[:name_en], name_zh_tw: value[:name_zh_tw]
