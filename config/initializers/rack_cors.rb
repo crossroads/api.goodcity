805b2ef52c0a28cb0f3c8dc13905aa5b75dbf7eb
Steve Kenworthy <steveyken@gmail.com>
2014-08-09 11:46:03 +0800

    Enable mobile app and local dev apps to access api

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.use Rack::Cors do
   allow do
-    if Rails.env == 'production'
-      origins 'app.goodcity.hk'
-    else
+    #~ if Rails.env == 'production'
+      #~ origins 'app.goodcity.hk'
+    #~ else
       origins '*'
-    end
+    #~ end
     resource '*', headers: :any,
                   methods: [:get, :post, :put, :delete, :options]
   end
