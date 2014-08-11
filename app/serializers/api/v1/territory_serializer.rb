8c30a1b60dda99e7b213b7295dc456242d246dd4
Shivani Thakur <shivanibhanwal@gmail.com>
2014-08-11 22:10:36 +0530

    As Territory has districts so I have changed the call so that based on terroritory we get distrtics

--- app/serializers/api/v1/territory_serializer.rb
+++ app/serializers/api/v1/territory_serializer.rb
@@ -3,6 +3,8 @@ module Api::V1
   class TerritorySerializer < ActiveModel::Serializer
     embed :ids, include: true
     attributes :id, :name
+
+    has_many :districts, serializer: DistrictSerializer
   end
 
 end
