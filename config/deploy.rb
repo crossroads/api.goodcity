b5c2dd70143c9934ba5a48ce8d73a9ab5ea17f8e
Steve Kenworthy <steveyken@gmail.com>
2014-08-21 14:20:23 +0800

    Stop bundler generating binstubs

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,8 @@ set :linked_files, %w{ config/database.yml .env }
 
 # Default value for linked_dirs is []
 #~ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache}
+set :linked_dirs, %w{log tmp/pids tmp/cache}
+set :bundle_binstubs, nil
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
