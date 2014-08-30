89342a9ad7b37b76c176d4a3cc5e8926b6fa81a3
swatijadhav <swati.jadhav@kiprosh.com>
2014-08-30 20:14:35 +0530

    specs for address and contact model

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Address, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Address, type: :model do
+  describe 'Association' do
+    it { should belong_to :district }
+    it { should belong_to :addressable }
+  end
+
+  describe 'Database columns' do
+    it{ should have_db_column(:flat).of_type(:string)}
+    it{ should have_db_column(:building).of_type(:string)}
+    it{ should have_db_column(:street).of_type(:string)}
+    it{ should have_db_column(:district_id).of_type(:integer)}
+    it{ should have_db_column(:addressable_id).of_type(:integer)}
+    it{ should have_db_column(:addressable_type).of_type(:string)}
+    it{ should have_db_column(:address_type).of_type(:string)}
+  end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Contact, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Contact, type: :model do
+  describe 'Association' do
+    it { should have_one :address }
+  end
+
+  describe 'Database columns' do
+    it{ should have_db_column(:name).of_type(:string)}
+    it{ should have_db_column(:mobile).of_type(:string)}
+  end
 end
