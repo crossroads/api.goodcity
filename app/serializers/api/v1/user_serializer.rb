02cc05bd222b520a28527fed053d8369e8ffc15c
swatijadhav <swati.jadhav@kiprosh.com>
2014-09-03 20:34:13 +0530

    fixed issue with duplication of offer-records

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -4,8 +4,6 @@ module Api::V1
     embed :ids, include: true
 
     attributes :id, :first_name, :last_name, :created_at, :updated_at
-
-    has_many :offers, serializer: OfferSerializer
   end
 
 end
