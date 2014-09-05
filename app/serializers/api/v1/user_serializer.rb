6f07179769071408560f0e65e2553c2929c1d4b5
swatijadhav <swati.jadhav@kiprosh.com>
2014-09-05 20:15:25 +0530

    update details in user serializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -3,9 +3,21 @@ module Api::V1
   class UserSerializer < ActiveModel::Serializer
     embed :ids, include: true
 
-    attributes :id, :first_name, :last_name, :created_at, :updated_at
+    attributes :id, :first_name, :last_name, :mobile, :full_name, :district
 
     has_many :permissions, serializer: PermissionSerializer
+
+    def mobile
+      object.try(:mobile) && object.mobile.slice!(4..-1)
+    end
+
+    def full_name
+      object.try(:full_name)
+    end
+
+    def district
+      object.try(:address).try(:district).try(:name_en)
+    end
   end
 
 end
