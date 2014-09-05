58381c3c6d8feda7bd5f2e1dad202e131b881ca0
Shivani Thakur <shivanibhanwal@gmail.com>
2014-09-05 19:06:17 +0530

    changes name for the slot field to make it consistent. Added mobile method to slice 852

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :schedule do
     id { generate(:schedules).keys.sample }
     resource { generate(:schedules)[id][:resource] }
-    slot { generate(:schedules)[id][:slot_id] }
+    slot { generate(:schedules)[id][:slot] }
     slot_name { generate(:schedules)[id][:slot_name] }
     zone { generate(:schedules)[id][:zone] }
     scheduled_at {Time.now + 1.weeks + generate(:schedules)[id][:scheduled_at].day}
