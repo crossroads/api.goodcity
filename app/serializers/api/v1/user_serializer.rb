5070519702b6e215238409721fe68d1122d3f289
swatijadhav <swati.jadhav@kiprosh.com>
2014-09-12 15:33:25 +0530

    message-ids in user serializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -3,13 +3,17 @@ module Api::V1
   class UserSerializer < ActiveModel::Serializer
     embed :ids, include: true
 
-    attributes :id, :first_name, :last_name, :mobile
+    attributes :id, :first_name, :last_name, :mobile, :message_ids
 
     has_many :permissions, serializer: PermissionSerializer
     has_one :address, serializer: AddressSerializer
 
     def mobile
       object.try(:mobile) && object.mobile.slice!(4..-1)
+    end
+
+    def message_ids
+      object.messages.pluck(:id) + object.sent_messages.pluck(:id)
     end
   end
 
