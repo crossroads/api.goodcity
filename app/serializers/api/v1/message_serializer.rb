087fc919af6d01e43aaf20a9f97345a313c515b6
Shivani Thakur <shivanibhanwal@gmail.com>
2014-09-18 12:29:38 +0800

    Fixed the serializer to set the permission and address id on the user

--- app/serializers/api/v1/message_serializer.rb
+++ app/serializers/api/v1/message_serializer.rb
@@ -6,9 +6,8 @@ module Api::V1
     attributes :id, :body, :state, :recipient_id, :sender_id,
       :is_private, :created_at, :updated_at, :offer_id, :item_id
 
-    has_one :sender, serializer: UserCompactSerializer, root: :user
-    has_one :recipient, serializer: UserCompactSerializer, root: :user
-
+    has_one :sender, serializer: UserSerializer, root: :user
+    has_one :recipient, serializer: UserSerializer, root: :user
   end
 
 end
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -3,13 +3,21 @@ module Api::V1
   class UserSerializer < ActiveModel::Serializer
     embed :ids, include: true
 
-    attributes :id, :first_name, :last_name, :mobile
+    attributes :id, :first_name, :last_name, :mobile, :address_id, :permission_id
 
-    has_one :permission, serializer: PermissionSerializer
     has_one :address, serializer: AddressSerializer
+    has_one :permission, serializer: PermissionSerializer
 
     def mobile
       object.try(:mobile) && object.mobile.slice!(4..-1)
+    end
+
+    def address_id
+      object.address.try(:id)
+    end
+
+    def permission_id
+      object.permission.try(:id)
     end
   end
 
