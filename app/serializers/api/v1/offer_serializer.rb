2abbce5fc6af656901eb9b1cdd5319a1ec014af4
swatijadhav <swati.jadhav@kiprosh.com>
2014-09-20 15:03:29 +0530

    fixed offer serializer

--- app/serializers/api/v1/offer_serializer.rb
+++ app/serializers/api/v1/offer_serializer.rb
@@ -5,8 +5,8 @@ module Api::V1
 
     attributes :id, :language, :state, :origin, :stairs, :parking,
       :estimated_size, :notes, :created_by_id, :created_at,
-      :updated_at, :submitted_at, :user_name, :user_phone, :user_ids, :delivery_id,
-      :reviewed_by_id, :reviewed_at
+      :updated_at, :submitted_at, :user_name, :user_phone, :user_id,
+      :delivery_id, :reviewed_by_id, :reviewed_at
 
     has_many :items, serializer: ItemSerializer
     has_many :messages, serializer: MessageSerializer
@@ -22,8 +22,8 @@ module Api::V1
       object.created_by.try(:mobile) && object.created_by.mobile.slice!(4..-1)
     end
 
-    def user_ids
-      [object.created_by_id, object.reviewed_by_id]
+    def user_id
+      object.created_by_id
     end
 
     def delivery_id
