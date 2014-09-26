fa0aeae7c762d775beb4e4f2be81fb1a03cd7636
swatijadhav <swati.jadhav@kiprosh.com>
2014-09-26 13:53:42 +0530

    serialized offer donation details

--- app/serializers/api/v1/contact_serializer.rb
+++ app/serializers/api/v1/contact_serializer.rb
@@ -1,10 +1,14 @@
 module Api::V1
 
   class ContactSerializer < ActiveModel::Serializer
-    # embed :ids, include: true
-    attributes :id, :name, :mobile
+    embed :ids, include: true
+    attributes :id, :name, :mobile, :address_id
 
-    has_one :delivery, Serializer: DeliverySerializer
+    has_one :address, serializer: AddressSerializer
+
+    def address_id
+      object.address.try(:id)
+    end
   end
 
 end
