5173c27255261c4b9712cd803a8c58059dbea618
swatijadhav <swati.jadhav@kiprosh.com>
2014-10-10 13:14:24 +0530

    api doc and specs for territories controller

--- app/controllers/api/v1/territories_controller.rb
+++ app/controllers/api/v1/territories_controller.rb
@@ -4,6 +4,23 @@ module Api::V1
     skip_before_action :validate_token, only: [:index, :show]
     load_and_authorize_resource :territory, parent: false
 
+    resource_description do
+      short 'List Territories.'
+      formats ['json']
+      error 401, "Unauthorized"
+      error 404, "Not Found"
+      error 422, "Validation Error"
+      error 500, "Internal Server Error"
+    end
+
+    def_param_group :offer do
+      param :territory, Hash, required: true do
+        param :name, String, desc: "Name of Territory"
+      end
+    end
+
+    api :GET, '/v1/territories', "List all territories"
+    param :ids, Array, of: Integer, desc: "Filter by territory ids e.g. ids = [1,2,3,4]"
     def index
       if params[:ids].blank?
         render json: Territory.cached_json
@@ -14,6 +31,7 @@ module Api::V1
       render json: @territories, each_serializer: serializer
     end
 
+    api :GET, '/v1/territory/1', "List a territory"
     def show
       render json: @territory, serializer: serializer
     end
---
+++ spec/controllers/api/v1/territories_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::TerritoriesController, type: :controller do
+
+  let(:territory) { create(:territory_districts) }
+  let(:serialized_territory) { Api::V1::TerritorySerializer.new(territory) }
+  let(:serialized_territory_json) { JSON.parse( serialized_territory.to_json ) }
+
+  let(:territories) { create_list(:territory, 2) }
+
+  describe "GET territory" do
+    it "returns 200" do
+      get :show, id: territory.id
+      expect(response.status).to eq(200)
+    end
+
+    it "return serialized territory", :show_in_doc do
+      get :show, id: territory.id
+      body = JSON.parse(response.body)
+      expect( body ).to eq(serialized_territory_json)
+    end
+  end
+
+  describe "GET territories" do
+    it "returns 200" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+
+    it "return serialized territories" do
+      get :index
+      body = JSON.parse(response.body)
+      expect( body['territories'].length ).to eq(Territory.count)
+    end
+  end
+
+end
