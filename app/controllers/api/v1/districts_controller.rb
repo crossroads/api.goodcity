8cb0b5b52793f27c0622e0e38a64c65404a8127d
swatijadhav <swati.jadhav@kiprosh.com>
2014-10-10 13:54:47 +0530

    api doc and specs for districts controller

--- app/controllers/api/v1/districts_controller.rb
+++ app/controllers/api/v1/districts_controller.rb
@@ -1,8 +1,27 @@
 module Api::V1
   class DistrictsController < Api::V1::ApiController
 
+    skip_before_action :validate_token, only: [:index, :show]
     load_and_authorize_resource :district, parent: false
 
+    resource_description do
+      short 'List Districts.'
+      formats ['json']
+      error 401, "Unauthorized"
+      error 404, "Not Found"
+      error 422, "Validation Error"
+      error 500, "Internal Server Error"
+    end
+
+    def_param_group :district do
+      param :district, Hash, required: true do
+        param :name, String, desc: "Name of District"
+        param :territory_id, String, desc: "Id of territory to which district belongs."
+      end
+    end
+
+    api :GET, '/v1/districts', "List all districts"
+    param :ids, Array, of: Integer, desc: "Filter by district ids e.g. ids = [1,2,3,4]"
     def index
       if params[:ids].blank?
         render json: District.cached_json
@@ -12,6 +31,7 @@ module Api::V1
       render json: @districts, each_serializer: serializer
     end
 
+    api :GET, '/v1/district/1', "List a district"
     def show
       render json: @district, serializer: serializer
     end
---
+++ spec/controllers/api/v1/districts_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::DistrictsController, type: :controller do
+
+  let(:district) { create(:district) }
+  let(:serialized_district) { Api::V1::DistrictSerializer.new(district) }
+  let(:serialized_district_json) { JSON.parse( serialized_district.to_json ) }
+
+  let(:districts) { create_list(:district, 2) }
+
+  describe "GET district" do
+    it "returns 200" do
+      get :show, id: district.id
+      expect(response.status).to eq(200)
+    end
+
+    it "return serialized district", :show_in_doc do
+      get :show, id: district.id
+      body = JSON.parse(response.body)
+      expect( body ).to eq(serialized_district_json)
+    end
+  end
+
+  describe "GET districts" do
+    it "returns 200" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+
+    it "return serialized districts" do
+      get :index
+      body = JSON.parse(response.body)
+      expect( body['districts'].length ).to eq(District.count)
+    end
+  end
+
+end
