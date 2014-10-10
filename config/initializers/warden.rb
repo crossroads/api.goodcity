dbc7fd203b19bb4450e48b35bf442bc60749d753
Steve Kenworthy <steveyken@gmail.com>
2014-10-10 16:43:45 +0800

    Removed warden session setup as we're not using sessions

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -5,13 +5,4 @@ Rails.application.config.middleware.use Warden::Manager do |manager|
   manager.failure_app = UnauthorizedController
 end
 
-# Setup Session Serialization
-#Warden::Manager.serialize_into_session do |user|
-#  user.auth_tokens.first.try(:otp_auth_key)
-#end
-
-#Warden::Manager.serialize_from_session do |otp_key|
-#  user.auth_tokens.first.find(otp_key)
-#end
-
 Warden::Strategies.add(:pin, Warden::Strategies::PinStrategy)
