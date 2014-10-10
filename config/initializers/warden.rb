3bfa7a9d943db2ba6ea9183b87a47f91f7536a51
Steve Kenworthy <steveyken@gmail.com>
2014-10-10 16:35:39 +0800

    Move warden pin strategy to it's own module so we can test it.

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,35 +1,17 @@
+require 'warden/strategies/pin_strategy'
+
 Rails.application.config.middleware.use Warden::Manager do |manager|
   manager.default_strategies :pin
   manager.failure_app = UnauthorizedController
 end
 
 # Setup Session Serialization
-Warden::Manager.serialize_into_session do |user|
-  user.auth_tokens.first.try(:otp_secret_key)
-end
+#Warden::Manager.serialize_into_session do |user|
+#  user.auth_tokens.first.try(:otp_auth_key)
+#end
 
-Warden::Manager.serialize_from_session do |otp_key|
-  user.auth_tokens.first.find(otp_key)
-end
+#Warden::Manager.serialize_from_session do |otp_key|
+#  user.auth_tokens.first.find(otp_key)
+#end
 
-# Pin Strategy
-Warden::Strategies.add(:pin) do
-
-  def valid?
-    params["pin"].present? && params['mobile'].present?
-  end
-
-  def authenticate!
-    user = User.where(mobile: params['mobile']).first
-    if user && user.most_recent_token.authenticate_otp(params["pin"], { drift: otp_code_validity })
-      success! user
-    else
-      fail! user
-    end
-  end
-
-  def otp_code_validity
-    Rails.application.secrets.token['otp_code_validity']
-  end
-
-end
+Warden::Strategies.add(:pin, Warden::Strategies::PinStrategy)
---
+++ spec/lib/warden/strategies/pin_strategy_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+
+describe Warden::Strategies::PinStrategy, type: :controller do
+
+  let(:env) { {} }
+  let(:params) { {} }
+  let(:pin) { "1234" }
+  let(:otp_auth_key) { "zKER89Q/NRm0TXhqGII+Ww==" }
+  let(:request) { double( params: params) }
+  let(:strategy) { Warden::Strategies::PinStrategy.new(env) }
+  let(:auth_token) { build :auth_token }
+  let(:user) { build :user }
+
+  before { allow(strategy).to receive(:request).and_return(request) }
+
+  context 'valid?' do
+    context "with valid params" do
+      let(:params) { {'pin' => pin, 'otp_auth_key' => otp_auth_key} }
+      it { expect(strategy.valid?).to eql(true) }
+    end
+    context "with no params" do
+      it { expect(strategy.valid?).to eql(false) }
+    end
+    context "with blank params" do
+      let(:params) { {'pin' => '', 'otp_auth_key' => ''} }
+      it { expect(strategy.valid?).to eql(false) }
+    end
+    context "with just pin" do
+      let(:params) { {'pin' => pin} }
+      it { expect(strategy.valid?).to eql(false) }
+    end
+    context "with just otp_auth_key" do
+      let(:params) { {'otp_auth_key' => otp_auth_key} }
+      it { expect(strategy.valid?).to eql(false) }
+    end
+  end
+
+  context "authenticate" do
+
+    context "with valid auth_token" do
+      let(:params) { {'pin' => pin, 'otp_auth_key' => otp_auth_key} }
+      before { expect(AuthToken).to receive(:find_by_otp_auth_key).with(otp_auth_key).and_return(auth_token) }
+
+      context "and successful otp authentication" do
+        before { expect(auth_token).to receive(:authenticate_otp).with(pin, {drift: ENV['OTP_CODE_VALIDITY'].to_i}).and_return(true) }
+
+        context "and user found" do
+          before { expect(auth_token).to receive(:user).and_return(user) }
+          it "should be success!" do
+            expect(strategy).to receive(:success!).with(user)
+            strategy.authenticate!
+          end
+        end
+
+        context "and user not found" do
+          before { expect(auth_token).to receive(:user).and_return(nil) }
+          it "should fail!" do
+            expect(strategy).to receive(:fail!)
+            strategy.authenticate!
+          end
+        end
+
+      end
+
+      context "and unsuccessful otp authentication" do
+        before { expect(auth_token).to receive(:authenticate_otp).and_return(false) }
+        it "should fail!" do
+          expect(strategy).to receive(:fail!)
+          strategy.authenticate!
+        end
+      end
+
+    end
+
+    context "with invalid auth_token" do
+      before { expect(AuthToken).to receive(:find_by_otp_auth_key).and_return(nil) }
+      it "should fail!" do
+        expect(strategy).to receive(:fail!)
+        strategy.authenticate!
+      end
+
+    end
+
+  end
+
+end
