9e4ee6de771d6b046c6eb624d6120abce9ebccaf
Steve Kenworthy <steveyken@gmail.com>
2014-10-16 12:52:18 +0800

    Update API docs for addresses controller

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -4,7 +4,7 @@ module Api::V1
     load_and_authorize_resource :address, parent: false
 
     resource_description do
-      short 'List and create address.'
+      short 'List and create addresses for Contacts and Users'
       formats ['json']
       error 401, "Unauthorized"
       error 404, "Not Found"
@@ -14,13 +14,13 @@ module Api::V1
 
     def_param_group :address do
       param :address, Hash, required: true do
-        param :street, String, desc: "Street Name"
-        param :flat, String, desc: "Flat Name"
-        param :building, String, desc: "Building Name"
-        param :district_id, String, desc: "District"
-        param :address_type, String, desc: "Type of address ('collection'/'profile')"
-        param :addressable_type, String, desc: "Address belong to (Polymorphic)- e.g. 'Contact' or 'User'"
-        param :addressable_id, String, desc: "Id of the associated polymorphic record (contact_id or user_id)"
+        param :street, String, desc: "Street name"
+        param :flat, String, desc: "Flat name"
+        param :building, String, desc: "Building name"
+        param :district_id, String, desc: "Hong Kong district"
+        param :address_type, String, desc: "Type of address, usually 'Collection' or 'Profile'"
+        param :addressable_type, String, desc: "Object the address belongs to: 'Contact' or 'User' (Polymorphic)"
+        param :addressable_id, String, desc: "Object id of the associated polymorphic record. Use contact_id or user_id"
       end
     end
 
@@ -35,9 +35,9 @@ module Api::V1
       end
     end
 
-    api :GET, '/v1/address/1', "List an address"
+    api :GET, '/v1/address/1', "Show an address"
     def show
-     render json: @address, serializer: serializer
+      render json: @address, serializer: serializer
     end
 
     private
