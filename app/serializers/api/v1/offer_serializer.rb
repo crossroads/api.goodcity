bb880a4f3868e1881e2fd471f039264265b20f23
swatijadhav <swati.jadhav@kiprosh.com>
2014-11-01 18:53:00 +0530

    fixed offer-serializer issue: pass user refernce with offer

--- app/serializers/api/v1/offer_serializer.rb
+++ app/serializers/api/v1/offer_serializer.rb
@@ -4,14 +4,23 @@ module Api::V1
     embed :ids, include: true
 
     attributes :id, :language, :state, :origin, :stairs, :parking,
-               :estimated_size, :notes, :created_by_id, :created_at,
-               :updated_at, :submitted_at, :reviewed_by_id, :reviewed_at
+      :estimated_size, :notes, :created_by_id, :created_at,
+      :updated_at, :submitted_at, :reviewed_by_id, :reviewed_at,
+      :user_id, :delivery_id
 
     has_many :items, serializer: ItemSerializer
     has_many :messages, serializer: MessageSerializer
     has_one  :created_by, serializer: UserSerializer, root: :user
     has_one  :reviewed_by, serializer: UserSerializer, root: :user
     has_one  :delivery, serializer: DeliverySerializer
+
+    def user_id
+      object.created_by_id
+    end
+
+    def delivery_id
+      object.delivery.try(:id)
+    end
   end
 
 end
