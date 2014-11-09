36299a7b8c111c07946f237c2ee493ddfc27a945
Michael McDonald <mcm-ham@users.noreply.github.com>
2014-11-09 12:20:24 +0800

    remove for_new_offer from message serializer, value now determined in ember app

--- app/serializers/api/v1/message_serializer.rb
+++ app/serializers/api/v1/message_serializer.rb
@@ -5,12 +5,9 @@ module Api::V1
     embed :ids, include: true
 
     attributes :id, :body, :state, :is_private, :created_at,
-      :updated_at, :offer_id, :item_id, :for_new_offer
+      :updated_at, :offer_id, :item_id
 
     has_one :sender, serializer: UserSerializer, root: :user
 
-    def for_new_offer
-      object.try(:offer).try(:submitted?) || false
-    end
   end
 end
