d900966b48b0c1f4625a4f67fa95fa67d9d342db
Michael McDonald <mcm-ham@users.noreply.github.com>
2014-11-13 17:47:49 +0800

    Serialize only first & last name for users, and create auth/current_user_profile route to retrieve full user details

---
+++ app/serializers/api/v1/user_profile_serializer.rb
@@ -0,0 +1,17 @@
+module Api::V1
+
+  class UserProfileSerializer < ActiveModel::Serializer
+    embed :ids, include: true
+
+    attributes :id, :first_name, :last_name, :mobile
+
+    has_one :address, serializer: AddressSerializer
+    has_one :permission, serializer: PermissionSerializer
+
+    def mobile
+      object.try(:mobile) && object.mobile.slice(4..-1)
+    end
+
+  end
+
+end
