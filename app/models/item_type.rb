f54bb55811f117978505171f0e97646e59beb106
Shivani Thakur <shivanibhanwal@gmail.com>
2014-11-20 12:04:34 +0530

    Added create for package and itemType Added Apidoc details to the package and itemType

--- app/models/item_type.rb
+++ app/models/item_type.rb
@@ -5,7 +5,7 @@ class ItemType < ActiveRecord::Base
   has_many :items, inverse_of: :item_type
   has_many :packages, foreign_key: :package_type_id, inverse_of: :package_type
 
-  validates :name_en, presence: true
+  validates :name_en, presence: true, uniqueness: true
 
   translates :name
 
