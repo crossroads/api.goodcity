95aa63975362ed87f92e35b247a3cf9e5d0ba840
Steve Kenworthy <steveyken@gmail.com>
2014-11-21 15:45:40 +0800

    Added parent/child relationship and data to ItemType.

--- spec/factories/item_types.rb
+++ spec/factories/item_types.rb
@@ -6,7 +6,12 @@ FactoryGirl.define do
     name_en         { generate(:item_types)[code][:name_en] }
     #name_zh_tw      { generate(:item_types)[code][:name_zh_tw] }
     name_zh_tw      { generate(:random_chinese_string) }
-    parent_id       nil
+    parent_id       {
+      parent_code = generate(:item_types)[code][:parent_code]
+      if parent_code
+        create(:item_type, code: parent_code).id
+      end
+    }
     initialize_with { ItemType.find_or_initialize_by(code: code) }
   end
 end
