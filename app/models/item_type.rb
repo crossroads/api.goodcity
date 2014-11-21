e9154362b3792fcbbae7728097c6558a3c6f6c5e
Steve Kenworthy <steveyken@gmail.com>
2014-11-21 15:45:40 +0800

    Added parent/child relationship and data to ItemType.

--- app/models/item_type.rb
+++ app/models/item_type.rb
@@ -4,6 +4,7 @@ class ItemType < ActiveRecord::Base
 
   has_many :items, inverse_of: :item_type
   has_many :packages, foreign_key: :package_type_id, inverse_of: :package_type
+  belongs_to :parent, foreign_key: :parent_id, class_name: 'ItemType'
 
   validates :name_en, presence: true
 
