63ed17a1daedd5f506ac1e8f8fe0f0e9f6440b3c
swatijadhav <swati.jadhav@kiprosh.com>
2014-11-24 20:19:24 +0530

    refactored code at gogovan orders controller

--- app/controllers/api/v1/gogovan_orders_controller.rb
+++ app/controllers/api/v1/gogovan_orders_controller.rb
@@ -4,51 +4,48 @@ module Api::V1
     skip_authorization_check only: [:calculate_price, :confirm_order]
 
     def calculate_price
-      order_details = {
-        order: {
-          name: current_user.full_name,
-          phone_number: current_user.mobile,
-          pickup_time: params["pickupTime"],
-          vehicle: "van",
-          locations: [[22.312516,114.217874,"Seaview Center, 139 Hoi Bun Road, Hong Kong"],[22.3741183,113.9927744, "Crossroads Foundation"]].to_json,
-          extra_requirements: {
-            need_english: params["needEnglish"],
-            need_cart: params["needCart"],
-            need_carry: params["needCarry"]
-          }
-        }
-      }
-      order = GoGoVanApi::Order.new(params: order_details)
+      order = initiate_order(params)
       render json: order.price.to_json
     end
 
     def confirm_order
-      details = params["gogovan_order"]
-      order_details = {
-        order: {
-          name: current_user.full_name,
-          phone_number: current_user.mobile,
-          pickup_time: details["pickup_time"],
-          vehicle: "van",
-          locations: [[22.312516,114.217874,"Seaview Center, 139 Hoi Bun Road, Hong Kong"],[22.3741183,113.9927744, "Crossroads Foundation"]].to_json,
-          extra_requirements: {
-            need_english: details["need_english"],
-            need_cart: details["need_cart"],
-            need_carry: details["need_carry"]
-          }
-        }
-      }
-      order = GoGoVanApi::Order.new(params: order_details)
-      book_order = order.book
-      gogovan_order = GogovanOrder.create(status: "pending", booking_id: book_order['id'])
+      attributes = params_hash(params["gogovan_order"])
+      book_order = initiate_order(attributes).book
+      gogovan_order = GogovanOrder.save_booking(book_order['id'])
       render json: gogovan_order, serializer: serializer
     end
 
     private
 
+    def initiate_order(attributes)
+      order_details = order_attributes(attributes)
+      GoGoVanApi::Order.new(params: order_details)
+    end
+
+    # hash with keys in lower-camelcase form
+    def params_hash(params)
+      Hash[params.map{|(k,v)| [k.camelize(:lower),v]}]
+    end
+
     def serializer
       Api::V1::GogovanOrderSerializer
     end
 
+    def order_attributes(order)
+      {
+        order: {
+          name:           order["name"] || current_user.full_name,
+          phone_number:   order["mobile"] || current_user.mobile,
+          pickup_time:    order["pickupTime"],
+          vehicle:        "van",
+          locations:      District.location_json(order['districtId']),
+          extra_requirements: {
+            need_english: order["needEnglish"],
+            need_cart:    order["needCart"],
+            need_carry:   order["needCarry"]
+          }
+        }
+      }
+    end
   end
 end
