f514134f0601abd7d3fc69adbab8a1017ff6689c
Steve Kenworthy <steveyken@gmail.com>
2014-11-25 17:20:41 +0800

    Image factory data now points to real images in Cloudinary

--- spec/factories/sequences.rb
+++ spec/factories/sequences.rb
@@ -32,4 +32,21 @@ FactoryGirl.define do
     "+852" << (%w(5 6 9).sample) << (1..7).map{rand(9)}.join
   end
 
+  sequence :cloudinary_image_id do |n|
+    seq = %w(
+      1416897663/szfmfbmjeq6aphyfflmg.jpg
+      1416902230/mmguhm3zdkonc2nynjue.jpg
+      1416229451/jgpyo4oxrnfnjriofdjd.jpg
+      1416817641/auyhxsvppoohnubhtdvp.jpg
+      1416375331/zppxdm5fljlyv3tcfo3p.jpg
+      1415435713/i6lzfri9xpjzzznxgfnv.jpg
+      1415379081/hbqmdhzk47524ppxw7xs.jpg
+      1415015815/iijhzfxwjrkavecs8wjp.jpg
+      1414914035/ijip6ea6kjy6zn4mz81q.jpg
+      1414308170/kdc3pkmli7du4wcnyppz.jpg
+    )
+    i = n.modulo(seq.size)
+    seq[i]
+  end
+
 end
