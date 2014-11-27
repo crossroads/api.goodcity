10402ee425952f07082985ea983e8f9b73da78ca
Shivani Thakur <shivanibhanwal@gmail.com>
2014-11-27 22:21:03 +0530

    Added parent_id field to send to the APP

--- app/serializers/api/v1/item_type_serializer.rb
+++ app/serializers/api/v1/item_type_serializer.rb
@@ -2,7 +2,7 @@ module Api::V1
 
   class ItemTypeSerializer < ActiveModel::Serializer
     embed :ids, include: true
-    attributes :id, :name, :code
+    attributes :id, :name, :code, :parent_id
   end
 
 end
