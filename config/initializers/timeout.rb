3eb38ca02e2f1e943afa731dce7c9e6e14d47aa1
Michael McDonald <mcm-ham@users.noreply.github.com>
2014-12-10 10:40:55 +0800

    Prevent timeout errors during development since debugger (at least debase) doesn't pause timer Rubymine IDE requires .ruby-version to be prefixed with "ruby" Enable full error reports during development

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,2 +1,2 @@
-Rack::Timeout.timeout = 30 # Abort requests that take too long... Puma needs help doing this
+Rack::Timeout.timeout = Rails.env.development? ? 0 : 30 # Abort requests that take too long... Puma needs help doing this
 Rack::Timeout.unregister_state_change_observer(:logger) # Turn off the logging
