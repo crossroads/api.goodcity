9b81d857283cada56f1f6cc42b1086f1cff592e1
swatijadhav <swati.jadhav@kiprosh.com>
2014-12-16 16:45:50 +0530

    fixed hound warnings

--- app/controllers/api/v1/holidays_controller.rb
+++ app/controllers/api/v1/holidays_controller.rb
@@ -1,6 +1,5 @@
 module Api::V1
   class HolidaysController < Api::V1::ApiController
-
     load_and_authorize_resource :holiday, parent: false
     skip_before_action :validate_token, only: :available_dates
 
@@ -14,7 +13,8 @@ module Api::V1
     end
 
     api :GET, "/v1/holidays/available_dates", "List all available dates"
-    param :schedule_days, String, desc: "List next available dates excluding holidays, sundays and mondays", allow_nil: true
+    param :schedule_days, String, allow_nil: true,
+      desc: "List next available dates excluding holidays, sundays and mondays"
     def available_dates
       days_count = params[:schedule_days] || NEXT_AVAILABLE_DAYS_COUNT
       render json: DateSet.new(days_count).available_dates.to_json
--- lib/classes/date_set.rb
+++ lib/classes/date_set.rb
@@ -1,9 +1,8 @@
 class DateSet
-
   def initialize(period_in_days = NEXT_AVAILABLE_DAYS_COUNT)
     @days         = period_in_days.to_i
     @current_time = Time.zone.now
-    @offset       = (@days*2).days
+    @offset       = (@days * 2).days
     @start        = @current_time
     @last         = @current_time + @offset
     @dates        = []
@@ -11,7 +10,7 @@ class DateSet
   end
 
   def available_dates
-    @days.times do |n|
+    @days.times do
       get_dates_list
       break if @dates.count >= @days
       @last = @start + @offset
@@ -23,8 +22,8 @@ class DateSet
   private
 
   def get_dates_list
-    while(@start < @last) do
-      if [0,1].exclude?(@start.wday) # exclude all sundays and mondays
+    while (@start < @last)
+      if [0, 1].exclude?(@start.wday) # exclude all sundays and mondays
         start_time = beginning_of_day(@start)
         @dates << @start if @holidays.exclude?(start_time)
       end
@@ -35,5 +34,4 @@ class DateSet
   def beginning_of_day(current_date)
     current_date.to_datetime.in_time_zone(Time.zone).beginning_of_day
   end
-
 end
