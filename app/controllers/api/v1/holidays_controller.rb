dc410c7fb7e1dc31d5420ed00bd935af6932834a
swatijadhav <swati.jadhav@kiprosh.com>
2014-12-16 15:16:40 +0530

    specs and api-doc with examples for holidays-controller

--- app/controllers/api/v1/holidays_controller.rb
+++ app/controllers/api/v1/holidays_controller.rb
@@ -4,6 +4,17 @@ module Api::V1
     load_and_authorize_resource :holiday, parent: false
     skip_before_action :validate_token, only: :available_dates
 
+    resource_description do
+      short "List next available dates (excluding holidays)"
+      formats ["json"]
+      error 401, "Unauthorized"
+      error 404, "Not Found"
+      error 422, "Validation Error"
+      error 500, "Internal Server Error"
+    end
+
+    api :GET, "/v1/holidays/available_dates", "List all available dates"
+    param :schedule_days, String, desc: "List next available dates excluding holidays, sundays and mondays", allow_nil: true
     def available_dates
       days_count = params[:schedule_days] || NEXT_AVAILABLE_DAYS_COUNT
       render json: DateSet.new(days_count).available_dates.to_json
