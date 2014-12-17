a76f7824d340aa6fbbb7a167c8bb05a893ce5b04
swatijadhav <swati.jadhav@kiprosh.com>
2014-12-17 19:53:31 +0530

    remove time-slot-caching and updated api-doc for timeslot

--- app/controllers/api/v1/timeslots_controller.rb
+++ app/controllers/api/v1/timeslots_controller.rb
@@ -4,12 +4,17 @@ module Api::V1
     load_and_authorize_resource :timeslot, parent: false
     skip_before_action :validate_token, only: :index
 
+    resource_description do
+      short "List all time-slots."
+      formats ['json']
+      error 401, "Unauthorized"
+      error 404, "Not Found"
+      error 422, "Validation Error"
+      error 500, "Internal Server Error"
+    end
+
+    api :GET, '/v1/timeslots', "List all time-slots"
     def index
-      if params[:ids].blank?
-        render json: Timeslot.cached_json
-        return
-      end
-      @timeslots = @timeslots.find( params[:ids].split(",") ) if params[:ids].present?
       render json: @timeslots, each_serializer: serializer
     end
 
