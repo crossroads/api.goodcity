8701a8255146d735ec3a95b83b64853c0b7433e6
Michael McDonald <mcm-ham@users.noreply.github.com>
2014-12-20 15:55:10 +0800

    Add message_ids to item to fix ember relationships, it requires both item.message_ids and message.item_id

--- app/serializers/api/v1/item_serializer.rb
+++ app/serializers/api/v1/item_serializer.rb
@@ -5,11 +5,19 @@ module Api::V1
 
     attributes :id, :donor_description, :state, :offer_id, :reject_reason,
       :saleable, :created_at, :updated_at, :item_type_id,
-      :rejection_comments, :donor_condition_id, :rejection_reason_id
+      :rejection_comments, :donor_condition_id, :rejection_reason_id, :message_ids
 
     has_many :packages, serializer: PackageSerializer
     has_many :images,   serializer: ImageSerializer
     has_one :item_type, serializer: ItemTypeSerializer
+
+    def message_ids
+      object.messages.pluck(:id)
+    end
+
+    def message_ids__sql
+      "coalesce((select array_agg(messages.id) from messages where item_id = items.id), '{}'::int[])"
+    end
 
   end
 
