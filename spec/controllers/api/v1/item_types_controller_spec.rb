475d315589eae1a301195978b7b569e7739f0fbe
Shivani Thakur <shivanibhanwal@gmail.com>
2014-12-25 19:39:39 +0530

    Fixed hound error

--- spec/controllers/api/v1/item_types_controller_spec.rb
+++ spec/controllers/api/v1/item_types_controller_spec.rb
@@ -1,7 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Api::V1::ItemTypesController, type: :controller do
-
   let(:user) { create(:user_with_token, :reviewer) }
   let(:item_type) { create(:item_type) }
   let(:serialized_item_type) { Api::V1::ItemTypeSerializer.new(item_type).to_json }
@@ -12,9 +11,10 @@ RSpec.describe Api::V1::ItemTypesController, type: :controller do
 
     it "returns 200", show_in_doc: true  do
       3.times { create :item_type }
+
       get :index
       expect(response.status).to eq(200)
-      expect(subject['item_types'].length).to eq(3)
+      expect(subject["item_types"].length).to eq(3)
     end
   end
 end
