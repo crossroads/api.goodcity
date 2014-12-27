752a71ad7862e89571457f61a4f6c1c01e954be0
Steve Kenworthy <steveyken@gmail.com>
2014-12-27 16:14:52 +0800

    Switched Twilio to sidekiq job.

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -13,21 +13,11 @@ class TwilioService
     code = token.otp_code
     expiry = token.otp_code_expiry.strftime("%A %b %e %H:%M")
     body = I18n.t('twilio.sms_verification_pin', pin: code, expiry: expiry)
-    options = { from: twilio_conf['phone_number'], to: @user.mobile, body: body}
-    twilio_client.account.sms.messages.create(options)
+    options = { to: @user.mobile, body: body}
+    TwilioJob.perform_later(options)
   end
 
   private
-
-  def twilio_client
-    @twilio_client ||= begin
-      Twilio::REST::Client.new(twilio_conf['account_sid'], twilio_conf['auth_token'])
-    end
-  end
-
-  def twilio_conf
-    Rails.application.secrets.twilio
-  end
 
   def allowed_to_send?
     mobile = @user.mobile
