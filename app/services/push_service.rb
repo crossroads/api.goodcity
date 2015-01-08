02c1180a0b80babb79defb3c66703120f7101f14
Shivani Thakur <shivanibhanwal@gmail.com>
2015-01-08 16:50:04 +0530

    Fixed issue with the Notification messages. As it was getting converted to json twice and due to which app side code was failing

--- app/services/push_service.rb
+++ app/services/push_service.rb
@@ -16,7 +16,6 @@ class PushService
     %w(channel event data).each do |opt|
       raise PushServiceError, "'#{opt}' has not been set" if send(opt).blank?
     end
-
     [channel].flatten.in_groups_of(10, false) do |subarray_of_channels|
       PusherJob.perform_later(subarray_of_channels, event, data.to_json)
     end
@@ -30,7 +29,7 @@ class PushService
   def send_notification(text:, entity_type:, entity:, channel:)
     @channel = channel
     @event = "notification"
-    @data = {text: text, entity_type: entity_type, entity: entity, date: Time.now}.to_json
+    @data = {text: text, entity_type: entity_type, entity: entity, date: Time.now}
     notify
   end
 end
