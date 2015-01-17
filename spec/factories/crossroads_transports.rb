db80fef80333e3bf0a95eb55554645791a94dec3
swatijadhav <swati.jadhav@kiprosh.com>
2015-01-17 23:33:28 +0530

    fixed creation of multiple transport options

--- spec/factories/crossroads_transports.rb
+++ spec/factories/crossroads_transports.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :crossroads_transport do
     name_en    { generate(:crossroads_transports)[:name_en] }
     name_zh_tw { generate(:crossroads_transports)[:name_zh_tw] }
-    initialize_with { CrossroadsTransport.new(name_en: name_en) }
+    initialize_with { CrossroadsTransport.find_or_initialize_by(name_en: name_en) }
   end
 end
--- spec/factories/gogovan_transports.rb
+++ spec/factories/gogovan_transports.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :gogovan_transport do
     name_en    { generate(:gogovan_transports)[:name_en] }
     name_zh_tw { generate(:gogovan_transports)[:name_zh_tw] }
-    initialize_with { GogovanTransport.new(name_en: name_en) }
+    initialize_with { GogovanTransport.find_or_initialize_by(name_en: name_en) }
   end
 end
