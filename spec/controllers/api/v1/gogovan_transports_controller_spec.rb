d26365dce6a6de5cd71fe86dedaad2282000d54b
swatijadhav <swati.jadhav@kiprosh.com>
2015-01-18 00:19:11 +0530

    fixed gogovan transport and crossroads transport controller specs

--- spec/controllers/api/v1/gogovan_transports_controller_spec.rb
+++ spec/controllers/api/v1/gogovan_transports_controller_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Api::V1::GogovanTransportsController, type: :controller do
   let(:reviewer) { create(:user, :reviewer) }
 
   describe "GET gogovan_transport" do
-    let!(:gogovan_transports) { create_list(:gogovan_transport, 3) }
+    let!(:gogovan_transport1) { create(:gogovan_transport, name_en: "Van") }
+    let!(:gogovan_transport2) { create(:gogovan_transport, name_en: "5.5t Truck") }
     before { generate_and_set_token(reviewer) }
 
     it "returns 200", :show_in_doc do
@@ -15,7 +16,7 @@ RSpec.describe Api::V1::GogovanTransportsController, type: :controller do
     it "return serialized gogovan_transport" do
       get :index
       body = JSON.parse(response.body)
-      expect(body['gogovan_transports'].length).to eq(3)
+      expect(body['gogovan_transports'].length).to eq(2)
     end
   end
 end
