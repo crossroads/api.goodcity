3208881e4b26f613f80786261cc98b7ba648feff
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-01-20 18:06:33 +0800

    Replace Pusher with socket.io service

--- app/jobs/pusher_job.rb
+++ app/jobs/pusher_job.rb
@@ -1,7 +1,8 @@
 class PusherJob  < ActiveJob::Base
   queue_as :default
 
-  def perform(subarray_of_channels, event, data)
-    Pusher.trigger(subarray_of_channels, event, data)
+  def perform(channels, event, data)
+    url = Rails.application.secrets.socketio_service["url"]
+    Nestful.post url, {rooms:channels, event:event, args:JSON.parse(data)}, :format => :json
   end
 end
