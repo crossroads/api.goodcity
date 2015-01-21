397ea74b5cf2f32b256b2cc83e88a68ff8157dbd
swatijadhav <swati.jadhav@kiprosh.com>
2015-01-21 16:42:34 +0530

    specs for gogovan-transport changes

--- spec/models/gogovan_transport_spec.rb
+++ spec/models/gogovan_transport_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe GogovanTransport, type: :model do
   it { should have_db_column(:name_zh_tw).of_type(:string) }
   it { should validate_presence_of(:name_en) }
 
+  describe 'instance methods' do
+    it '#vehical_tag' do
+      expect(
+        (build :gogovan_transport, name_en: 'Van').vehical_tag
+      ).to eq('van')
+      expect(
+        (build :gogovan_transport, name_en: '5.5t Truck').vehical_tag
+      ).to eq('mudou')
+    end
+  end
 end
