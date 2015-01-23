5802ed800131c6544fa3c12e88b8df4b54b94a85
swatijadhav <swati.jadhav@kiprosh.com>
2015-01-23 19:17:32 +0530

    add truck-size and cost columns to crossroads_transports

--- spec/factories/gogovan_transports.rb
+++ spec/factories/gogovan_transports.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :gogovan_transport do
     name_en    { generate(:gogovan_transports).keys.sample }
     name_zh_tw { generate(:gogovan_transports)[name_en][:name_zh_tw] }
+    cost       { generate(:gogovan_transports)[name_en][:cost] }
+    truck_size { generate(:gogovan_transports)[name_en][:truck_size] }
     initialize_with { GogovanTransport.find_or_initialize_by(name_en: name_en) }
   end
 end
--- spec/models/crossroads_transport_spec.rb
+++ spec/models/crossroads_transport_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe CrossroadsTransport, type: :model do
 
   it { should have_db_column(:name_en).of_type(:string) }
   it { should have_db_column(:name_zh_tw).of_type(:string) }
+  it { should have_db_column(:cost).of_type(:integer) }
+  it { should have_db_column(:truck_size).of_type(:float) }
+
   it { should validate_presence_of(:name_en) }
 
 end
