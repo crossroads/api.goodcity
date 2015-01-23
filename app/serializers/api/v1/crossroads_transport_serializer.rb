7c9a87e7d7b6108baf99f5274830ea11d9ace266
swatijadhav <swati.jadhav@kiprosh.com>
2015-01-23 19:56:03 +0530

    update offer and crossroads_transport serializer

--- app/serializers/api/v1/crossroads_transport_serializer.rb
+++ app/serializers/api/v1/crossroads_transport_serializer.rb
@@ -1,7 +1,7 @@
 module Api::V1
 
   class CrossroadsTransportSerializer < ActiveModel::Serializer
-    attributes :id, :name
+    attributes :id, :name, :cost
 
     def name__sql
       "name_#{I18n.locale}"
