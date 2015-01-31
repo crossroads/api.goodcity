9007d75b94c540256d385f5fdf3dcf63c5a5b963
swatijadhav <swati.jadhav@kiprosh.com>
2015-01-31 16:15:50 +0530

    allow address and schedule records in push-updates

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,14 @@
 class Address < ActiveRecord::Base
+  include PushUpdates
   include Paranoid
 
   belongs_to :addressable, polymorphic: true
   belongs_to :district
+
+  #required by PusherUpdates module
+  def offer
+    if addressable_type == "Contact"
+      addressable.try(:delivery).try(:offer)
+    end
+  end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,10 @@
 class Schedule < ActiveRecord::Base
+  include PushUpdates
+
   has_many :deliveries
+
+  #required by PusherUpdates module
+  def offer
+    deliveries.last.try(:offer)
+  end
 end
