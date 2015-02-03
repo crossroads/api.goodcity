a758f4b10bbfca694472dbd38253010a3f9ce0c9
Steve Kenworthy <steveyken@gmail.com>
2015-02-03 11:54:18 +0800

    Ensure all GoGoVanTransport options are listed in APIPIE

--- spec/controllers/api/v1/gogovan_transports_controller_spec.rb
+++ spec/controllers/api/v1/gogovan_transports_controller_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe Api::V1::GogovanTransportsController, type: :controller do
   let(:reviewer) { create(:user, :reviewer) }
 
   describe "GET gogovan_transport" do
-    let!(:gogovan_transport1) { create(:gogovan_transport, name_en: "Van") }
-    let!(:gogovan_transport2) { create(:gogovan_transport, name_en: "5.5t Truck") }
+    let(:gogovan_transport) { create(:gogovan_transport, name_en: "Van") }
     before { generate_and_set_token(reviewer) }
 
     it "returns 200", :show_in_doc do
+      # Generate all transport options for listing in API docs
+      generate(:gogovan_transports).keys.each do |name_en|
+        create(:gogovan_transport, name_en: name_en)
+      end
       get :index
       expect(response.status).to eq(200)
     end
@@ -16,7 +19,8 @@ RSpec.describe Api::V1::GogovanTransportsController, type: :controller do
     it "return serialized gogovan_transport" do
       get :index
       body = JSON.parse(response.body)
-      expect(body['gogovan_transports'].length).to eq(2)
+      expect(body['gogovan_transports'].length).to eq(1)
+      expect(body['gogovan_transports']['name']).to eq(gogovan_transport.name)
     end
   end
 end
