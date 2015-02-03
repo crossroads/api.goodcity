419cde37bbf8b03eafc253e0bc0d04e2764c0afc
Steve Kenworthy <steveyken@gmail.com>
2015-02-03 12:06:02 +0800

    Updated documentation for donor_conditions

--- spec/controllers/api/v1/donor_conditions_controller_spec.rb
+++ spec/controllers/api/v1/donor_conditions_controller_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe Api::V1::DonorConditionsController, type: :controller do
   let(:serialized_donor_condition) { Api::V1::DonorConditionSerializer.new(donor_condition) }
   let(:serialized_donor_condition_json) { JSON.parse( serialized_donor_condition.to_json ) }
 
+  before { generate_and_set_token(user) }
+
   describe "GET donor_condition" do
-    before { generate_and_set_token(user) }
     it "returns 200" do
       get :show, id: donor_condition.id
       expect(response.status).to eq(200)
@@ -22,16 +23,15 @@ RSpec.describe Api::V1::DonorConditionsController, type: :controller do
   end
 
   describe "GET donor_conditions" do
-    let!(:donor_conditions) { create_list(:donor_condition, 4) }
-    before { generate_and_set_token(user) }
-
-    it "returns 200", :show_in_doc do
+    before do
+      # Generate all transport options for listing in API docs
+      generate(:donor_conditions).keys.each do |name_en|
+        create(:donor_condition, name_en: name_en)
+      end
+    end
+    it "return serialized donor_conditions", :show_in_doc do
       get :index
       expect(response.status).to eq(200)
-    end
-
-    it "return serialized donor_conditions" do
-      get :index
       body = JSON.parse(response.body)
       expect( body['donor_conditions'].length ).to eq(DonorCondition.count)
     end
