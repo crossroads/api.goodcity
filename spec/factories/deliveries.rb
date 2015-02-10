9c389c86b6b3815120468955b3ff5f232eb0fe80
swatijadhav <swati.jadhav@kiprosh.com>
2015-02-10 23:58:26 +0530

    add api-doc examples for all types of delivery types

--- spec/factories/deliveries.rb
+++ spec/factories/deliveries.rb
@@ -1,7 +1,24 @@
 FactoryGirl.define do
   factory :delivery do
     offer
-    contact
     delivery_type { ["Alternate", "Drop Off", "Gogovan"].sample }
+
+    factory :crossroads_delivery do
+      association :contact, factory: :contact_with_address
+      schedule
+      delivery_type { "Alternate" }
+    end
+
+    factory :gogovan_delivery do
+      association :contact, factory: :gogovan_contact
+      association :schedule, factory: :gogovan_schedule
+      gogovan_order
+      delivery_type { "Gogovan" }
+    end
+
+    factory :drop_off_delivery do
+      association :schedule, factory: :drop_off_schedule
+      delivery_type { "Drop Off" }
+    end
   end
 end
