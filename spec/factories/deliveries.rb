2ff25e15833e385853f7d9cf8e69d7cf22908b17
sunil-sharma <sunil.sharma@kiprosh.com>
2015-02-10 14:05:21 +0530

    added description for deliveries controller

--- spec/factories/deliveries.rb
+++ spec/factories/deliveries.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :delivery do
     offer
     contact
-    schedule
-    delivery_type { [ "Alternate", "Drop Off", "Gogovan"].sample }
+    delivery_type { ["Alternate", "Drop Off", "Gogovan"].sample }
   end
 end
