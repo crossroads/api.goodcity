4d93b3df3fd92281d00a473be8460e888eb071bd
Steve Kenworthy <steveyken@gmail.com>
2015-02-28 13:54:47 +0800

    New staging/production setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,2 @@
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
-
-server 'api.goodcity.hk', user: 'deployer', roles: %w{web app}
-#set :ssh_options, { :forward_agent => true }
+server 'goodcity-production.cloudapp.net', user: 'deployer', roles: %w{web app db}, primary: true
+server 'goodcity-production.cloudapp.net:62423', user: 'deployer', roles: %w{web app}
