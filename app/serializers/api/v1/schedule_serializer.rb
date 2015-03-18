99ea8ac61d738889161d5ba6747761190755d813
swatijadhav <swati.jadhav@kiprosh.com>
2015-03-18 13:48:47 +0530

    fixed time-zone format issue with serializer: fetch values in hk time-zone

--- app/serializers/api/v1/schedule_serializer.rb
+++ app/serializers/api/v1/schedule_serializer.rb
@@ -1,10 +1,15 @@
 module Api::V1
 
   class ScheduleSerializer < ApplicationSerializer
+
     embed :ids, include: true
     attributes :id, :resource, :scheduled_at, :slot, :slot_name, :zone
 
     # has_many :deliveries, serializer: DeliverySerializer
+
+    def scheduled_at__sql
+      " schedules.scheduled_at#{time_zone_query} "
+    end
   end
 
 end
