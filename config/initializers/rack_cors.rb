17967ada45072220b2d966d6891961cf86017e92
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-03-20 12:36:32 +0800

    Fix CORS header not being set for 500 responses This meant request was being aborted on client due to CORS security failure and then status code would be 0 instead of 500 which our error detection logic determines as internet connection failure

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use Rack::Cors do
+Rails.application.config.middleware.insert_after Rails::Rack::Logger, Rack::Cors, :logger => Rails.logger do
   allow do
     origins '*'
     resource '*', headers: :any,
