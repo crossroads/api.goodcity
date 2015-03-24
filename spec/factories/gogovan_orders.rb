d5452ad2cf8ea6714dde98636075d3688ed757e5
swatijadhav <swati.jadhav@kiprosh.com>
2015-03-24 23:43:50 +0530

    updated specs for gogovan-order model

--- spec/factories/gogovan_orders.rb
+++ spec/factories/gogovan_orders.rb
@@ -2,6 +2,12 @@ FactoryGirl.define do
   factory :gogovan_order do
     booking_id { rand(1000000..9999999) }
     status "pending"
+
+    trait :with_delivery do
+      after(:create) do |order|
+        create :gogovan_delivery, gogovan_order: order
+      end
+    end
   end
 
 end
--- spec/models/gogovan_order_spec.rb
+++ spec/models/gogovan_order_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe GogovanOrder, type: :model do
   describe 'Database columns' do
     it{ is_expected.to have_db_column(:booking_id).of_type(:integer)}
     it{ is_expected.to have_db_column(:status).of_type(:string)}
+    it{ is_expected.to have_db_column(:price).of_type(:float)}
+    it{ is_expected.to have_db_column(:driver_name).of_type(:string)}
+    it{ is_expected.to have_db_column(:driver_mobile).of_type(:string)}
+    it{ is_expected.to have_db_column(:driver_license).of_type(:string)}
   end
 
   describe 'save_booking' do
@@ -19,4 +23,52 @@ RSpec.describe GogovanOrder, type: :model do
       expect(GogovanOrder.last.booking_id).to eq(booking_id)
     end
   end
+
+  describe '#update_status' do
+    it 'should update status of order' do
+      order = create :gogovan_order
+      expect{
+        order.update_status("cancelled")
+      }.to change(order, :status).from('pending').to('cancelled')
+    end
+  end
+
+  describe '#need_polling?' do
+    it 'should return true if status is pending/active' do
+      order = build :gogovan_order, status: 'active'
+      expect(order.need_polling?).to eq(true)
+    end
+
+    it 'should return true if status is completed/cancelled' do
+      order = build :gogovan_order, status: 'completed'
+      expect(order.need_polling?).to eq(false)
+    end
+  end
+
+  describe '#is_cancelled?' do
+    let(:order) { build :gogovan_order }
+
+    it 'should return true if status changed to cancelled' do
+      order.status = 'cancelled'
+      expect(order.is_cancelled?).to eq(true)
+    end
+
+    it 'should return false if status changed to other than cancelled' do
+      order.status = 'active'
+      expect(order.is_cancelled?).to eq(false)
+    end
+  end
+
+  describe 'callbacks' do
+    describe 'push_back_offer_state' do
+      it 'should change offer state to reviewed' do
+        order = create :gogovan_order, :with_delivery
+        expect{
+          order.update_attributes(status: 'cancelled')
+          order.delivery.offer.reload
+        }.to change(order.delivery.offer, :state).to('reviewed')
+      end
+    end
+  end
+
 end
