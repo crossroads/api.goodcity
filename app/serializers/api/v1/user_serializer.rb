00744616b87236cda1bd9dda57ba6186a14658b0
swatijadhav <swati.jadhav@kiprosh.com>
2015-04-09 17:50:52 +0530

    allow admin to fetch all offers of donor

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -2,9 +2,11 @@ module Api::V1
 
   class UserSerializer < ApplicationSerializer
     embed :ids, include: true
-    attributes :id, :first_name, :last_name, :permission_id
+    attributes :id, :first_name, :last_name, :permission_id, :mobile,
+      :created_at
 
     has_one :image, serializer: ImageSerializer
+    has_one :address, serializer: AddressSerializer
   end
 
 end
