2d80d99a6253abdb5dca0561b505da9e8cf519eb
Steve Kenworthy <steveyken@gmail.com>
2015-04-25 23:02:16 +0800

    New capistrano version collapses server roles with same hostname irrespective of user/port

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server 'goodcity-production.cloudapp.net', user: 'deployer', roles: %w{web app db}, primary: true
+server 'api.goodcity.hk', user: 'deployer', roles: %w{web app db}, primary: true
 server 'goodcity-production.cloudapp.net:62423', user: 'deployer', roles: %w{web app}
 set :branch, :live
