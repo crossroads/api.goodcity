e2d0608e98c8592b8578381dec72f62753a291f9
swatijadhav <swati.jadhav@kiprosh.com>
2015-05-05 12:36:54 +0530

    last_connected and last_disconnected columns for user and update connect/disconnect time for user

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -3,15 +3,18 @@ module Api::V1
   class UserSerializer < ApplicationSerializer
     embed :ids, include: true
     attributes :id, :first_name, :last_name, :permission_id, :mobile,
-      :created_at
+      :created_at, :last_connected, :last_disconnected
 
     has_one :image, serializer: ImageSerializer
     has_one :address, serializer: AddressSerializer
 
-    def include_mobile?
+    def include_attribute?
       User.current_user.try(:permission).try(:present?)
     end
-    alias_method :include_address?, :include_mobile?
+    alias_method :include_address?, :include_attribute?
+    alias_method :include_mobile?, :include_attribute?
+    alias_method :include_last_connected?, :include_attribute?
+    alias_method :include_last_disconnected?, :include_attribute?
 
   end
 
