de68b95c841c5dd51fce5581b536b0ffafcf543f
swatijadhav <swati.jadhav@kiprosh.com>
2015-05-19 17:45:39 +0530

    rake task to migrate existing package-details

--- db/migrate/20150518123926_drop_item_types_table.rb
+++ db/migrate/20150518123926_drop_item_types_table.rb
@@ -1,5 +1,23 @@
 class DropItemTypesTable < ActiveRecord::Migration
-  def change
+  def up
+    Rake::Task["goodcity:update_packages"].invoke
+
+    rename_column :items, :item_type_id, :package_type_id
+
     drop_table :item_types
+  end
+
+  def down
+    rename_column :items, :package_type_id, :item_type_id
+
+    create_table :item_types do |t|
+      t.string  :name_en
+      t.string  :name_zh_tw
+      t.string  :code
+      t.integer :parent_id
+      t.boolean :is_item_type_node, null: false, default: false
+
+      t.timestamps
+    end
   end
 end
