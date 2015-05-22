3f6ea89f34bb8e8754eb7e46c0a3070e94b7ea29
swatijadhav <swati.jadhav@kiprosh.com>
2015-05-22 16:26:02 +0530

    in push-updates, send required details of sender and avoid associated details

--- app/models/concerns/push_updates.rb
+++ app/models/concerns/push_updates.rb
@@ -16,7 +16,6 @@ module PushUpdates
 
     exclude_relationships = {exclude: self.class.reflections.keys.map(&:to_sym)}
     serializer = "Api::V1::#{self.class}Serializer".constantize.new(self, exclude_relationships)
-    user = Api::V1::UserSerializer.new(current_user)
     type = self.class.name
     object = {}
 
@@ -37,6 +36,7 @@ module PushUpdates
     donor_channel = (offer.nil? || offer.try(:cancelled?)) ? [] :
       Channel.user_id(offer.created_by_id)
     channel = Channel.staff + donor_channel
+    user = Api::V1::UserSerializer.new(current_user, {user_summary: false})
     data = {item:object, sender:user, operation:operation}
     collapse_key = offer.nil? ? nil : "offer" + offer.id.to_s
     PushService.new.send_update_store(channel, data, collapse_key)
