d949f37a84521dc0267ce34c4e147fe78600cfa4
sunil sharma <sunil.sharma@kiprosh.com>
2015-05-25 16:36:08 +0530

    Added completed_at in gogovan_orders

--- app/serializers/api/v1/gogovan_order_serializer.rb
+++ app/serializers/api/v1/gogovan_order_serializer.rb
@@ -1,6 +1,6 @@
 module Api::V1
   class GogovanOrderSerializer < ApplicationSerializer
     attributes :id, :booking_id, :status, :price, :driver_name,
-      :driver_mobile, :driver_license, :ggv_uuid
+      :driver_mobile, :driver_license, :ggv_uuid, :completed_at
   end
 end
