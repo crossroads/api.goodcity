b38e7d209ba606e339d027d7ac10035829cdb60a
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-05-26 18:22:55 +0800

    Fix AddVanAllowedToCrossroadsTransport migration script for when creating a new db

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,6 +62,7 @@ gogovan_transports = YAML.load_file("#{Rails.root}/db/gogovan_transports.yml")
 gogovan_transports.each do |name, value|
   FactoryGirl.create :gogovan_transport, name_en: name, name_zh_tw: value[:name_zh_tw]
 end
+CrossroadsTransport.find_by(name_en: "Disable").update_column(:is_van_allowed, false)
 
 crossroads_transports = YAML.load_file("#{Rails.root}/db/crossroads_transports.yml")
 crossroads_transports.each do |name, value|
