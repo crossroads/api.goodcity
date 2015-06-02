9f92dcee4528ee76b5b39eee32c9ef784eb3487b
swatijadhav <swati.jadhav@kiprosh.com>
2015-06-02 19:00:43 +0530

    verify image referenced by multiple-items before deleting from cloudinary

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,7 +6,8 @@ class Image < ActiveRecord::Base
 
   has_one :user, inverse_of: :image
   belongs_to :item, inverse_of: :images
-  before_destroy :delete_image_from_cloudinary, unless: "Rails.env.test?"
+  before_destroy :delete_image_from_cloudinary,
+    unless: "Rails.env.test? || has_multiple_items"
 
   def public_image_id
     cloudinary_id.split("/").last.split(".").first rescue nil
@@ -23,6 +24,10 @@ class Image < ActiveRecord::Base
     image_id = public_image_id
     CloudinaryImageCleanupJob.perform_later(image_id) if image_id
     true
+  end
+
+  def has_multiple_items
+    Image.where(cloudinary_id: cloudinary_id).count > 1
   end
 
 end
