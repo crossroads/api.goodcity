c511fa64c5d9608b585adb9d7d69453300ebd442
swatijadhav <swati.jadhav@kiprosh.com>
2015-06-25 19:18:37 +0530

    Send notification for twilio calls - incoming call from donor to its active offer's private-thread subscribers - after call is accepted, notify to other offer's private-thread subscribers except call-receiver

--- app/jobs/accepted_call_notification_job.rb
+++ app/jobs/accepted_call_notification_job.rb
@@ -3,16 +3,16 @@ class AcceptedCallNotificationJob < ActiveJob::Base
 
   def perform(user_id, mobile)
     user     = User.find_by(id: user_id)
-    reviewer = User.user_exist?(mobile)
+    receiver = User.user_exist?(mobile)
     offer_id = user.try(:recent_active_offer_id)
     offer    = Offer.find_by(id: offer_id)
     text     = "Call from #{user.full_name} has been accepted \
-      by #{reviewer.full_name}."
+      by #{receiver.full_name}."
 
     PushService.new.send_notification(
       text:        text,
       entity_type: "offer",
       entity:      offer,
-      channel:     Channel.reviewer)
+      channel:     offer.call_notify_channels - ["user_#{receiver.id}"])
   end
 end
