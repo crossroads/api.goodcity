8481822105cb80fe2b9edb35968f892ae305699e
swatijadhav <swati.jadhav@kiprosh.com>
2015-07-09 21:03:24 +0530

    initial structure for package-categories

---
+++ app/models/package_sub_category.rb
@@ -0,0 +1,6 @@
+class PackageSubCategory < ActiveRecord::Base
+  belongs_to :package_type
+  belongs_to :package_category
+
+  validates :package_type_id, uniqueness: { scope: :package_category_id }
+end
---
+++ spec/factories/package_sub_categories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :package_sub_category do
+    package_type_id     1
+    package_category_id 1
+  end
+end
