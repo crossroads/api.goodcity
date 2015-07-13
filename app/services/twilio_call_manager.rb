74fecac8d064595b93319ae12738423b0b2df18b
swatijadhav <swati.jadhav@kiprosh.com>
2015-07-13 12:55:04 +0530

    moved redis-usage from twilio-outbound-controller to service

---
+++ app/services/twilio_call_manager.rb
@@ -0,0 +1,43 @@
+require "goodcity/redis"
+
+class TwilioCallManager
+
+  OutBoundCallPrefix = "twilio_outbound"
+
+  def initialize(options = {})
+    @call_to  = options[:to]
+    @offer_id = options[:offer_id]
+    @user_id  = options[:user_id]
+  end
+
+  def offer_id
+    data["offer_id"]
+  end
+
+  def user_id
+    data["user_id"]
+  end
+
+  def store
+    redis.mapped_hmset(key, { offer_id: @offer_id, user_id: @user_id })
+  end
+
+  def remove
+    redis.del(key)
+  end
+
+  private
+
+  def data
+    redis.hgetall key
+  end
+
+  def key
+    "#{OutBoundCallPrefix}_#{@call_to}"
+  end
+
+  def redis
+    @redis ||= Goodcity::Redis.new
+  end
+
+end
