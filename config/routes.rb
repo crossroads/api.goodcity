b53a7d8c0714c7ceb36ba824e1925bad11895c18
swatijadhav <swati.jadhav@kiprosh.com>
2015-07-17 12:25:32 +0530

    fetch items-list for browse app from browse controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace "api" do
     namespace "v1", defaults: { format: "json" } do
-      get "items/browse" => "items#browse"
+      get "browse/fetch_items" => "browse#fetch_items"
       resources :items, except: [:index]
       post "auth/signup" => "authentication#signup"
       post "auth/verify" => "authentication#verify"
