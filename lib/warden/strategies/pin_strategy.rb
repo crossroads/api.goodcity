2954ee7ec645a12264b264b00fcf5b9c28f389dd
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-07-18 16:08:27 +0800

    GCW-495 Add hard coded login credentials to live API for Apple reviewer

--- lib/warden/strategies/pin_strategy.rb
+++ lib/warden/strategies/pin_strategy.rb
@@ -10,6 +10,11 @@ module Warden
 
       def authenticate!
         auth_token = AuthToken.find_by_otp_auth_key(params['otp_auth_key'])
+
+        if appstore['number'] == auth_token.user.try(:mobile) && appstore['pin'] == params['pin']
+          return success!(auth_token.user)
+        end
+
         if auth_token && auth_token.authenticate_otp(params["pin"], { drift: otp_code_validity })
           if (user = auth_token.user)
             success!(user)
@@ -25,6 +30,10 @@ module Warden
 
       def otp_code_validity
         Rails.application.secrets.token['otp_code_validity']
+      end
+
+      def appstore
+        Rails.application.secrets.appstore_reviewer_login
       end
 
     end
