fc1fc6e00194b84c769de541de4743027b773740
swatijadhav <swati.jadhav@kiprosh.com>
2015-08-07 08:57:02 +0530

    Update seed.rb - remove creation of dummy offers

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,41 @@
 
 donor_conditions = YAML.load_file("#{Rails.root}/db/donor_conditions.yml")
 donor_conditions.each do |name, value|
-  FactoryGirl.create :donor_condition, name_en: name, name_zh_tw: value[:name_zh_tw]
+  FactoryGirl.create(:donor_condition,
+    name_en: name,
+    name_zh_tw: value[:name_zh_tw] )
 end
+
+rejection_reasons = YAML.load_file("#{Rails.root}/db/rejection_reasons.yml")
+rejection_reasons.each do |name_en, value|
+  FactoryGirl.create(:rejection_reason,
+    name_en: name_en,
+    name_zh_tw: value[:name_zh_tw] )
+end
+
+districts = YAML.load_file("#{Rails.root}/db/districts.yml")
+districts.each do |name_en, value|
+  # FactoryGirl creates the correct territory for us
+  FactoryGirl.create :district, name_en: name_en, latitude: value[:latitude], longitude: value[:longitude]
+end
+
+timeslots = [["9AM-11AM", "上午9時至上午11時"], ["11AM-1PM", "上午11時至下午1時"],
+  ["2PM-4PM", "下午2時至下午4時"]]
+timeslots.each do |name|
+  FactoryGirl.create :timeslot, name_en: name.first, name_zh_tw: name.last
+end
+
+gogovan_transports = YAML.load_file("#{Rails.root}/db/gogovan_transports.yml")
+gogovan_transports.each do |name, value|
+  FactoryGirl.create :gogovan_transport, name_en: name, name_zh_tw: value[:name_zh_tw]
+end
+
+crossroads_transports = YAML.load_file("#{Rails.root}/db/crossroads_transports.yml")
+crossroads_transports.each do |name, value|
+  FactoryGirl.create :crossroads_transport, name_en: name, name_zh_tw: value[:name_zh_tw], cost: value[:cost], truck_size: value[:truck_size]
+end
+
+CrossroadsTransport.find_by(name_en: "Disable").update_column(:is_van_allowed, false)
 
 package_types = YAML.load_file("#{Rails.root}/db/package_types.yml")
 package_types.each do |code, value|
@@ -40,34 +73,6 @@ package_types.each do |code, value|
     end
   end
 end
-
-rejection_reasons = YAML.load_file("#{Rails.root}/db/rejection_reasons.yml")
-rejection_reasons.each do |name_en, value|
-  FactoryGirl.create :rejection_reason, name_en: name_en, name_zh_tw: value[:name_zh_tw]
-end
-
-districts = YAML.load_file("#{Rails.root}/db/districts.yml")
-districts.each do |name_en, value|
-  # FactoryGirl creates the correct territory for us
-  FactoryGirl.create :district, name_en: name_en, latitude: value[:latitude], longitude: value[:longitude]
-end
-
-timeslots = [["9AM-11AM", "上午9時至上午11時"], ["11AM-1PM", "上午11時至下午1時"],
-  ["2PM-4PM", "下午2時至下午4時"]]
-timeslots.each do |name|
-  FactoryGirl.create :timeslot, name_en: name.first, name_zh_tw: name.last
-end
-
-gogovan_transports = YAML.load_file("#{Rails.root}/db/gogovan_transports.yml")
-gogovan_transports.each do |name, value|
-  FactoryGirl.create :gogovan_transport, name_en: name, name_zh_tw: value[:name_zh_tw]
-end
-
-crossroads_transports = YAML.load_file("#{Rails.root}/db/crossroads_transports.yml")
-crossroads_transports.each do |name, value|
-  FactoryGirl.create :crossroads_transport, name_en: name, name_zh_tw: value[:name_zh_tw], cost: value[:cost], truck_size: value[:truck_size]
-end
-CrossroadsTransport.find_by(name_en: "Disable").update_column(:is_van_allowed, false)
 
 # Create System User
 FactoryGirl.create(:user, :system)
@@ -105,7 +110,4 @@ unless ENV['LIVE'] == "true"
     { mobile: "+85291111114", first_name: "Scott", last_name: "Sandro94" },
   ]
   supervisor_attributes.each {|attr| FactoryGirl.create(:user, :supervisor, attr) }
-
-  10.times { FactoryGirl.create :offer, :with_items, :with_messages, created_by: User.donors.sample }
-
 end
