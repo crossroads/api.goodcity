d6db04369f9c31c608a139c3a1e8bd4aaa67afe5
swatijadhav <swati.jadhav@kiprosh.com>
2015-10-15 17:27:14 +0530

    fixed issue with sending updates to on cancelling offer

--- app/serializers/api/v1/package_serializer.rb
+++ app/serializers/api/v1/package_serializer.rb
@@ -3,6 +3,8 @@ module Api::V1
   class PackageSerializer < ApplicationSerializer
     embed :ids, include: true
 
+    has_one :package_type, serializer: PackageTypeSerializer
+
     attributes :id, :quantity, :length, :width, :height, :notes,
       :item_id, :state, :received_at, :rejected_at,
       :created_at, :updated_at, :package_type_id, :image_id, :offer_id
