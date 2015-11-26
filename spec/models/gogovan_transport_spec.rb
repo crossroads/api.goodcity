b2b53cd7f460625cca2467765a0e1c9f2f38ce85
swatijadhav <swati.jadhav@kiprosh.com>
2015-11-26 15:42:41 +0530

    disabled column for gogovan_transports

--- spec/models/gogovan_transport_spec.rb
+++ spec/models/gogovan_transport_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe GogovanTransport, type: :model do
 
   it { is_expected.to have_db_column(:name_en).of_type(:string) }
   it { is_expected.to have_db_column(:name_zh_tw).of_type(:string) }
+  it { is_expected.to have_db_column(:disabled).of_type(:boolean) }
   it { is_expected.to validate_presence_of(:name_en) }
 
   describe 'instance methods' do
