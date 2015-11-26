7a6c798ca88a9d73bdc19b8b7378a415377e6efd
swatijadhav <swati.jadhav@kiprosh.com>
2015-11-26 15:42:41 +0530

    disabled column for gogovan_transports

--- spec/factories/gogovan_transports.rb
+++ spec/factories/gogovan_transports.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :gogovan_transport do
     name_en    { generate(:gogovan_transports).keys.sample }
     name_zh_tw { generate(:gogovan_transports)[name_en][:name_zh_tw] }
+    disabled   { generate(:gogovan_transports)[name_en][:disabled] }
     initialize_with { GogovanTransport.find_or_initialize_by(name_en: name_en) }
   end
 end
