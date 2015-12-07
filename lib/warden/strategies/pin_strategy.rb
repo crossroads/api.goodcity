8e6adeced38cb4b28424b86a1c0f218bac98cc76
swatijadhav <swati.jadhav@kiprosh.com>
2015-12-07 11:52:21 +0530

    refactoring: update method-name and removed un-necessary transaction block

--- lib/warden/strategies/pin_strategy.rb
+++ lib/warden/strategies/pin_strategy.rb
@@ -10,7 +10,7 @@ module Warden
 
       def authenticate!
         auth_token = AuthToken.find_by_otp_auth_key(params['otp_auth_key'])
-        return success!(auth_token.user) if valid_credentials?(auth_token)
+        return success!(auth_token.user) if valid_app_store_credentials?(auth_token)
         user = auth_token.try(:user)
         has_valid_otp_code?(auth_token) && user ? success!(user) : fail
       end
@@ -21,7 +21,7 @@ module Warden
         auth_token && auth_token.authenticate_otp(params["pin"], { drift: otp_code_validity })
       end
 
-      def valid_credentials?(auth_token)
+      def valid_app_store_credentials?(auth_token)
         appstore.try(:[], 'number').present? &&
         appstore.try(:[], 'pin').present? &&
         appstore['pin'] == params['pin'] &&
