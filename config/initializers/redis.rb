60e498c35dea48d24bbc02f0c8b321ea19dc6807
Steve Kenworthy <steveyken@gmail.com>
2015-12-16 15:16:47 +0800

    Remove REDIS_NAMESPACE as this is no longer recommended in SideKiq. Will need to re-add GGV jobs.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,4 @@
 redis_options = { namespace: 'goodcity' }
-redis_options.merge!(namespace: ENV['REDIS_NAMESPACE']) if ENV['REDIS_NAMESPACE'].present?
 redis_options.merge!(password: ENV['REDIS_PASSWORD']) if ENV['REDIS_PASSWORD'].present?
 redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379'
 
