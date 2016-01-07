710202121e477f7a32d3ded56daae4d3b841de38
swatijadhav <swati.jadhav@kiprosh.com>
2016-01-07 18:27:24 +0530

    updated test cases for location

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :location do
-    building    { FFaker::Lorem.word }
-    area        { FFaker::Lorem.word }
-    stockit_id  { rand(99) }
+    building   { [10..45].sample }
+    area       { FFaker::Lorem.characters(1) }
+    stockit_id { rand(99) }
   end
 
 end
