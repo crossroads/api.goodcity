1d223cc358127765f1d366ced2c867382dd46a80
swatijadhav <swati.jadhav@kiprosh.com>
2016-01-25 16:30:39 +0530

    external api-request authentication

--- spec/controllers/concerns/token_validatable_spec.rb
+++ spec/controllers/concerns/token_validatable_spec.rb
@@ -7,15 +7,24 @@ end
 describe TokenValidatableFakeController do
 
   let(:token) { Token.new }
-  before { expect(Token).to receive(:new).and_return(token) }
+  before do
+    expect(Token).to receive(:new).and_return(token)
+    User.current_user = create :user, disabled: true
+  end
 
-  it "should successfully validate a good token" do
+  it "should successfully validate a good token and valid user" do
+    User.current_user = create :user
     expect(token).to receive(:valid?).and_return(true)
     subject.send(:validate_token)
   end
 
   it "should throw unauthorized error if token validation fails" do
     expect(token).to receive(:valid?).and_return(false)
+    expect{ subject.send(:validate_token) }.to throw_symbol(:warden)
+  end
+
+  it "should throw unauthorized error if user is disabled" do
+    expect(token).to receive(:valid?).and_return(true)
     expect{ subject.send(:validate_token) }.to throw_symbol(:warden)
   end
 
