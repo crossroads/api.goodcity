b4b84c08b6469f52ab611c127c34f6189fbeb51e
swatijadhav <swati.jadhav@kiprosh.com>
2016-01-27 13:05:15 +0530

    update cancellation_reasons model

--- spec/factories/cancellation_reasons.rb
+++ spec/factories/cancellation_reasons.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :cancellation_reason do
     name_en         { generate(:cancellation_reasons).keys.sample }
     name_zh_tw      { generate(:cancellation_reasons)[name_en][:name_zh_tw] }
+    visible_to_admin { generate(:cancellation_reasons)[name_en][:visible_to_admin] }
     initialize_with { CancellationReason.find_or_initialize_by(name_en: name_en) }
   end
 end
