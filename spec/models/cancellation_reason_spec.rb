6c50e824c37076a3607be249b48b46b7cf73c30b
swatijadhav <swati.jadhav@kiprosh.com>
2016-01-28 12:48:35 +0530

    fixed cancallation_reason model spec

--- spec/models/cancellation_reason_spec.rb
+++ spec/models/cancellation_reason_spec.rb
@@ -8,11 +8,10 @@ RSpec.describe CancellationReason, type: :model do
   it { is_expected.to validate_presence_of(:name_en) }
 
   describe "scope: visible" do
-    let(:visible_scope) { CancellationReason.visible }
-
     it "return records having visible_to_admin as true" do
       visible_reason = create :cancellation_reason, visible_to_admin: true
       invisible_reason = create :cancellation_reason, visible_to_admin: false
+      visible_scope = CancellationReason.visible
       expect(visible_scope).to include(visible_reason)
       expect(visible_scope).to_not include(invisible_reason)
     end
