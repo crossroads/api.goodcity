28f9fffc525888997920389c9dd5c78648b4a896
swatijadhav <swati.jadhav@kiprosh.com>
2016-02-16 20:27:40 +0530

    GCW-885 braintree integration

---
+++ app/controllers/api/v1/braintree_controller.rb
@@ -0,0 +1,28 @@
+module Api::V1
+  class BraintreeController < Api::V1::ApiController
+
+    before_action :authorize_user, :braintree_object
+
+    api :GET, '/v1/braintree/generate_token', "Get braintree token"
+    def generate_token
+      token = @braintree.client_token
+      render json: { braintree_token: token }.to_json
+    end
+
+    def make_transaction
+      response = @braintree.create_transaction(params["amount"], params["payment_method_nonce"])
+      render json: { response: response }.to_json
+    end
+
+    private
+
+    def authorize_user
+      authorize!(:current_user_profile, User)
+    end
+
+    def braintree_object
+      @braintree = BraintreeService.new(User.current_user)
+    end
+
+  end
+end
