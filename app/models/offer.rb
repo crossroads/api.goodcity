8a4cb39aad1da9139a9fbcaf69549cbfdf85d57c
swatijadhav <swati.jadhav@kiprosh.com>
2016-04-27 12:16:17 +0530

    GCW-1003 send new-offer-notification to supervisors

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -235,7 +235,7 @@ class Offer < ActiveRecord::Base
   end
 
   def send_new_offer_notification
-    PushService.new.send_notification Channel.reviewers, true, {
+    PushService.new.send_notification Channel.staff, true, {
       category:   'new_offer',
       message:    I18n.t("notification.new_offer", name: created_by.full_name),
       offer_id:   id,
