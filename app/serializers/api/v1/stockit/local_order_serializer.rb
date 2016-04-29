d4e46c034f89e002c66ca8711def2e6a344f09b9
Michael McDonald <mcm-ham@users.noreply.github.com>
2016-04-29 16:50:22 +0800

    Ensure HKID is not exposed

--- app/serializers/api/v1/stockit/local_order_serializer.rb
+++ app/serializers/api/v1/stockit/local_order_serializer.rb
@@ -1,5 +1,20 @@
 module Api::V1::Stockit
   class LocalOrderSerializer < ApplicationSerializer
     attributes :id, :client_name, :hkid_number
+
+    def hkid_number
+      if object.hkid_number.blank?
+        ""
+      else
+        "****#{object.hkid_number[-6..-1]}"
+      end
+    end
+
+    def hkid_number__sql
+      "case when hkid_number is null or hkid_number = '' \
+        then '*****' || substr(hkid_number, 5) \
+        else '' \
+      end"
+    end
   end
 end
