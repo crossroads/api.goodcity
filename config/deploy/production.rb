9d5c1b82a2e37839b41a39eaf365e62e4932f0d7
Steve Kenworthy <steveyken@gmail.com>
2016-05-20 15:11:23 +0800

    Updated external ssh port

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server 'api.goodcity.hk', user: 'deployer', roles: %w{web app db}, primary: true
+server 'api.goodcity.hk:62422', user: 'deployer', roles: %w{web app db}, primary: true
 server 'goodcity-production.cloudapp.net:62423', user: 'deployer', roles: %w{web app}
 set :branch, :live
