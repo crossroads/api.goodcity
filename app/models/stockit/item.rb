63b4336db4e38b63d26801e3b2e7d0e925048106
swatijadhav <swati.jadhav@kiprosh.com>
2016-05-27 16:59:38 +0530

    GCW-1044 search items from stockit-DB

--- app/models/stockit/item.rb
+++ app/models/stockit/item.rb
@@ -2,4 +2,14 @@ class Stockit::Item < Stockit::BaseModel
   belongs_to :designation
   belongs_to :image
   belongs_to :location
+
+  scope :latest, -> { order('id desc') }
+  scope :exclude_designated, ->(designation_id) {
+    where("designation_id <> ?", designation_id)
+  }
+
+  def self.search(search_text)
+    where("inventory_number LIKE :query", query: "%#{search_text}%")
+  end
+
 end
