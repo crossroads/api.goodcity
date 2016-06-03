6c61b2202485d50d0fe66eed9cd19674d099f63f
Michael McDonald <mcm-ham@users.noreply.github.com>
2016-06-03 17:31:31 +0800

    Add group tag for reviewers and supervisors on azure notifications

--- app/services/push_service.rb
+++ app/services/push_service.rb
@@ -1,19 +1,15 @@
 class PushService
   def send_update_store(channel, is_admin_app, data)
-    channels = [channel].flatten
-    channels = Channel.add_admin_app_prefix(channels) if is_admin_app
-    SocketioSendJob.perform_later(channels, "update_store", data.to_json, true)
+    channel = Channel.add_admin_app_suffix(channel) if is_admin_app
+    SocketioSendJob.perform_later(channel, "update_store", data.to_json, true)
   end
 
   def send_notification(channel, is_admin_app, data)
     data[:message] = ActionView::Base.full_sanitizer.sanitize(data[:message])
     data[:date] = Time.now.to_json.tr('"','')
-    channels = [channel].flatten
-    channels = Channel.add_admin_app_prefix(channels) if is_admin_app
-    SocketioSendJob.perform_later(channels, "notification", data.to_json)
+    channel = Channel.add_admin_app_suffix(channel) if is_admin_app
 
-    if Channel.user_channel?(channels)
-      AzureNotifyJob.perform_later(channels, data, is_admin_app)
-    end
+    SocketioSendJob.perform_later(channel, "notification", data.to_json)
+    AzureNotifyJob.perform_later(channel, data, is_admin_app)
   end
 end
