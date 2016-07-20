d91a9263e2fb5c79be57796ebecda6c8aab5f741
swatijadhav <swati.jadhav@kiprosh.com>
2016-07-20 14:06:48 +0530

    update package-inventory-number index migration

--- db/migrate/20160719095224_add_index_for_packages.rb
+++ db/migrate/20160719095224_add_index_for_packages.rb
@@ -2,7 +2,9 @@ class AddIndexForPackages < ActiveRecord::Migration
   def up
     execute "CREATE EXTENSION IF NOT EXISTS btree_gin;"
     execute "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
-    execute "CREATE INDEX inventory_numbers_search_idx ON packages USING gin (inventory_number gin_trgm_ops);"
+    unless index_exists?(:packages, :inventory_number, name: "inventory_numbers_search_idx")
+      execute "CREATE INDEX inventory_numbers_search_idx ON packages USING gin (inventory_number gin_trgm_ops);"
+    end
   end
 
   def down
