41a5ea20f86cde578ff030b5074c67abccc62552
swatijadhav <swati.jadhav@kiprosh.com>
2016-07-26 13:30:55 +0530

    removed bypassing filter at syncing stockit records

--- app/controllers/api/v1/stockit_contacts_controller.rb
+++ app/controllers/api/v1/stockit_contacts_controller.rb
@@ -1,7 +1,6 @@
 module Api::V1
   class StockitContactsController < Api::V1::ApiController
 
-    skip_before_action :validate_token, only: [:create]
     load_and_authorize_resource :stockit_contact, parent: false
 
     resource_description do
