1457d54395f9fab04fd0c637d049203b0dec5045
swatijadhav <swati.jadhav@kiprosh.com>
2016-08-31 16:43:45 +0530

    GCW-1233 update logic to display browse-items

--- app/controllers/api/v1/browse_controller.rb
+++ app/controllers/api/v1/browse_controller.rb
@@ -1,8 +1,8 @@
 module Api::V1
   class BrowseController < Api::V1::ApiController
 
-    load_and_authorize_resource :item, parent: false
-    skip_before_action :validate_token, unless: -> { Rails.env.production? }
+    load_and_authorize_resource :package, parent: false
+    skip_before_action :validate_token
 
     resource_description do
       short 'Get items list.'
@@ -13,15 +13,16 @@ module Api::V1
       error 500, "Internal Server Error"
     end
 
-    api :GET, '/v1/browse/fetch_items', "List all items"
-    def fetch_items
-      render json: @items.accepted, each_serializer: serializer, is_browse_request: true, root: "items"
+    api :GET, '/v1/browse/fetch_packages', "List all packages"
+    def fetch_packages
+      packages = @packages.browse_inventorized.union(@packages.browse_non_inventorized)
+      render json: packages, each_serializer: serializer, root: "package"
     end
 
     private
 
     def serializer
-      Api::V1::ItemSerializer
+      Api::V1::BrowsePackageSerializer
     end
   end
 end
