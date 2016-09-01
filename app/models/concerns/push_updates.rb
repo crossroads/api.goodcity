129d441621194756e8749755d3544f36e82e9679
swatijadhav <swati.jadhav@kiprosh.com>
2016-09-01 13:53:34 +0530

    update push-update logic to send updates for custom serialized columns

--- app/models/concerns/push_updates.rb
+++ app/models/concerns/push_updates.rb
@@ -49,6 +49,7 @@ module PushUpdates
       .find_all{|i| serializer.respond_to?(i) || serializer.respond_to?(i.sub('_id', ''))}
       .map{|i| i.to_sym}
       .each{|i| object[type][i] = self[i]}
+    object.values.first.merge!(serialized_object(object))
     object
   end
 
@@ -56,6 +57,12 @@ module PushUpdates
     name = self.class
     exclude_relationships = {exclude: name.reflections.keys.map(&:to_sym)}
     "Api::V1::#{name}Serializer".constantize.new(self, exclude_relationships)
+  end
+
+  def serialized_object(object)
+    serializer_name = "Api::V1::#{self.class}Serializer".constantize
+    object_key = object.keys[0].downcase.to_sym
+    serializer_name.new(serializer.object).as_json[object_key]
   end
 
   def service
