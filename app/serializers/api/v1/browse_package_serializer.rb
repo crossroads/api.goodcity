3c5375a21627b5bfee4e258a56fd27f8f6344603
swatijadhav <swati.jadhav@kiprosh.com>
2016-09-06 20:37:22 +0530

    GCW-1248 update browse package serializer

--- app/serializers/api/v1/browse_package_serializer.rb
+++ app/serializers/api/v1/browse_package_serializer.rb
@@ -5,19 +5,11 @@ module Api::V1
 
     has_one :package_type, serializer: PackageTypeSerializer
     has_one :item, serializer: BrowseItemSerializer
-    has_one :favourite_image, serializer: ImageSerializer, root: :image
+    has_many :images, serializer: StockitImageSerializer
 
     attributes :id, :quantity, :length, :width, :height, :notes, :item_id,
-      :created_at, :updated_at, :package_type_id, :favourite_image_id, :grade,
-      :donor_condition_id, :image_id
-
-    def image_id
-      object.favourite_image_id
-    end
-
-    def image_id__sql
-      "packages.favourite_image_id"
-    end
+      :created_at, :updated_at, :package_type_id, :grade,
+      :donor_condition_id
 
     def item_id
       object.set_item_id.present?
