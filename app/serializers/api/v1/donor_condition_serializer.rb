b2c9516a723b4a256eab5722422893317b848a98
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2016-09-14 17:17:41 +0530

    GCW-1234 Changes for donor conditions

--- app/serializers/api/v1/donor_condition_serializer.rb
+++ app/serializers/api/v1/donor_condition_serializer.rb
@@ -4,8 +4,30 @@ module Api::V1
     attributes :id, :name
 
     def name__sql
-      "name_#{current_language}"
+      if @options[:include_stock_condition]
+        "(CASE WHEN donor_conditions.id=1 THEN 'New'
+              WHEN donor_conditions.id=2 THEN 'Mixed'
+              WHEN donor_conditions.id=3 THEN 'Used'
+              WHEN donor_conditions.id=4 THEN 'Broken'
+        END)"
+      else
+        "name_#{current_language}"
+      end
     end
+
+    def name
+      if @options[:include_stock_condition]
+        case object.id
+          when 1 then 'New'
+          when 2 then 'Mixed'
+          when 3 then 'Used'
+          when 4 then 'Broken'
+        end
+      else
+        object.name
+      end
+    end
+
   end
 
 end
