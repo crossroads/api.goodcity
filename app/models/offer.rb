4f2a91d7edc2d75b1453af733c7bc081abb3fd00
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2016-09-22 13:20:54 +0530

    Fixed offer selection for admin

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -49,6 +49,7 @@ class Offer < ActiveRecord::Base
     states.push(*Offer.nondraft_states) if states.delete('nondraft')
     states.push(*Offer.active_states) if states.delete('active')
     states.push(*Offer.donor_valid_states) if states.delete('for_donor')
+    states.push(*Offer.donor_states) if states.delete('donor_non_draft')
     where(state: states.uniq)
   }
 
@@ -182,6 +183,9 @@ class Offer < ActiveRecord::Base
       valid_states - not_active_states
     end
     def nondraft_states
+      active_states - ["draft"]
+    end
+    def donor_states
       valid_states - ["draft"]
     end
   end
