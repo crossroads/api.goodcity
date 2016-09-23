ee4593e8b25c5194b736c6f49df38bd243b2c918
Steve Kenworthy <steveyken@gmail.com>
2016-09-23 16:46:49 +0800

    Removed donor description from the API

--- app/serializers/api/v1/browse_item_serializer.rb
+++ app/serializers/api/v1/browse_item_serializer.rb
@@ -3,7 +3,7 @@ module Api::V1
   class BrowseItemSerializer < ApplicationSerializer
     embed :ids, include: true
 
-    attributes :id, :donor_description, :created_at, :updated_at,
+    attributes :id, :created_at, :updated_at,
       :package_type_id, :donor_condition_id
 
     has_one  :package_type, serializer: PackageTypeSerializer
