b353011b842c0cea062bd2904eab4a524d82b82b
swatijadhav <swati.jadhav@kiprosh.com>
2016-09-25 19:50:24 +0530

    update order-serializer

--- app/serializers/api/v1/order_transport_serializer.rb
+++ app/serializers/api/v1/order_transport_serializer.rb
@@ -4,7 +4,7 @@ module Api::V1
 
     embed :ids, include: true
     attributes :id, :order_id, :scheduled_at, :timeslot, :gogovan_transport_id,
-      :transport_type
+      :transport_type, :need_english, :need_cart, :need_carry
 
     has_one :contact, serializer: ContactSerializer
     has_one :gogovan_order, serializer: GogovanOrderSerializer
