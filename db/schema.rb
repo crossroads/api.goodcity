f8b78a9953117a6ffd11bdb9f504200ea2fffa00
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2016-12-07 18:33:06 +0530

    GCW-1258-populate orders_packages with designated and dispatched packages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20161208115855) do
+=======
+ActiveRecord::Schema.define(version: 20161207064453) do
+>>>>>>> GCW-1258-populate orders_packages with designated and dispatched packages
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
