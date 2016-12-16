db626c3dbf6fb7b6d391eabcd0b58984b1128d6a
namrataukirde <namrata.ukirde@kiprosh.com>
2016-12-16 17:40:17 +0530

    fix existing single location should work as previously it was

--- app/serializers/api/v1/packages_location_serializer.rb
+++ app/serializers/api/v1/packages_location_serializer.rb
@@ -2,8 +2,15 @@ module Api::V1
 
   class PackagesLocationSerializer < ApplicationSerializer
     embed :ids, include: true
-    attributes :id, :package_id, :location_id, :quantity
+    attributes :id, :package_id, :location_id, :quantity, :item_id
 
     has_one :location, serializer: LocationSerializer
+
+    def item_id
+    end
+
+    def item_id__sql
+      'package_id'
+    end
   end
 end
