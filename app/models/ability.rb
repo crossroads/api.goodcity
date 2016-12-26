39ca712983ca5c77efeb9d23f2d2db1bd991848a
namrataukirde <namrata.ukirde@kiprosh.com>
2016-12-26 21:26:00 +0530

    partial move calculations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -157,9 +157,10 @@ class Ability
     if staff?
       can [:index, :show, :create, :update, :destroy, :print_barcode,
         :search_stockit_items, :designate_stockit_item, :remove_from_set,
-        :undesignate_stockit_item, :designate_partial_item, :update_partial_quantity_of_same_designation,:undesignate_partial_item, :dispatch_stockit_item, :move_stockit_item,
-        :print_inventory_label, :undispatch_stockit_item,
-        :stockit_item_details], Package
+        :undesignate_stockit_item, :designate_partial_item, :update_partial_quantity_of_same_designation,
+        :undesignate_partial_item, :dispatch_stockit_item, :move_stockit_item,
+        :move_partial_quantity, :print_inventory_label,
+        :undispatch_stockit_item, :stockit_item_details], Package
     else
       can [:index, :show, :create, :update], Package, Package.donor_packages(@user_id) do |record|
         record.item ? record.item.offer.created_by_id == @user_id : false
