d2d373201c981f9fd7f56939d9711ac1bc326167
namrataukirde <namrata.ukirde@kiprosh.com>
2016-12-29 21:19:28 +0530

    add papertrail for packages_location and specs for packages_location model

--- spec/models/packages_location_spec.rb
+++ spec/models/packages_location_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe PackagesLocation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { is_expected.to belong_to :location }
+    it { is_expected.to belong_to :package }
+  end
+
+  describe 'Database columns' do
+    it{ is_expected.to have_db_column(:package_id).of_type(:integer)}
+    it{ is_expected.to have_db_column(:location_id).of_type(:integer)}
+    it{ is_expected.to have_db_column(:quantity).of_type(:integer)}
+  end
 end
