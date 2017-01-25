4ab2bdf87fd32277fdbba27c6e198e7342f2ef81
trojanh <rajan.tiwari@kiprosh.com>
2017-01-25 15:54:17 +0530

    refactored factories and demo rake

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
   factory :order do
     status   ["draft", "submitted", "processing", "closed", "cancelled"].sample
-    state   ["draft", "submitted", "processing", "closed", "cancelled"].sample
+    state    ["draft", "submitted", "processing", "closed", "cancelled"].sample
 
-    code            "#{(65 + Random.rand(26)).chr}#{Random.rand(5)}"
-    detail_type     ["CarryOut", "Shipment", "StockitLocalOrder", "Goodcity"].sample
+    code          generate(:code)
+    detail_type   ["CarryOut", "Shipment", "StockitLocalOrder", "Goodcity"].sample
 
     description     FFaker::Lorem.sentence
     purpose_description FFaker::Lorem.sentence
-    stockit_id      1
+    stockit_id      nil
     association     :detail, factory: :stockit_local_order  , strategy: :create
     association     :stockit_organisation
     association     :stockit_activity
--- spec/factories/packages.rb
+++ spec/factories/packages.rb
@@ -2,13 +2,14 @@
 
 FactoryGirl.define do
   factory :package do
-    quantity    { rand(5) + 1 }
-    length      { rand(199) + 1 }
-    width       { rand(199) + 1 }
-    height      { rand(199) + 1 }
-    notes       { FFaker::Lorem.paragraph }
-    state       'expecting'
-    received_quantity 1
+    quantity              { rand(5) + 1 }
+    length                { rand(199) + 1 }
+    width                 { rand(199) + 1 }
+    height                { rand(199) + 1 }
+    notes                 { FFaker::Lorem.paragraph }
+    inventory_number      { generate(:inventory_number) }
+    state                 'expecting'
+    received_quantity     1
 
     received_at nil
     rejected_at nil
@@ -21,13 +22,10 @@ FactoryGirl.define do
     end
 
     trait :stockit_package do
-      inventory_number {rand(1000000).to_s.rjust(6,'0')}
       sequence(:stockit_id) { |n| n }
     end
 
     trait :with_set_item do
-      inventory_number {rand(1000000).to_s.rjust(6,'0')}
-
       sequence(:stockit_id) { |n| n }
       item
       set_item_id { item.id }
@@ -37,7 +35,6 @@ FactoryGirl.define do
     trait :received do
       state "received"
       received_at { Time.now }
-      inventory_number {rand(1000000).to_s.rjust(6,'0')}
       sequence(:stockit_id) { |n| n }
     end
   end
