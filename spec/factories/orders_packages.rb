63a928ac286ea4cdd37a969c74eb9ea84c29e811
trojanh <rajan.tiwari@kiprosh.com>
2017-02-08 17:32:18 +0530

    changed the orders_packages attribute

--- spec/factories/orders_packages.rb
+++ spec/factories/orders_packages.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :orders_package do
     association  :order
     association  :package
-    association :reviewed_by, factory: :user, strategy: :build
+    association :updated_by, factory: :user, strategy: :build
     state         ["requested", "cancelled", "designated", "received", "dispatched"].sample
     quantity      Random.rand(5)
   end
