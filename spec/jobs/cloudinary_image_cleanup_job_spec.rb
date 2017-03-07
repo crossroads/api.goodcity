b5442155f90af6b09fefb80f3d3275c40323382c
Steve Kenworthy <steveyken@gmail.com>
2017-03-07 14:43:12 +0800

    Fixed broken cloudinary test.

--- spec/jobs/cloudinary_image_cleanup_job_spec.rb
+++ spec/jobs/cloudinary_image_cleanup_job_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe CloudinaryImageCleanupJob, type: :job do
   let!(:cloudinary_id) { image.public_image_id }
 
   it "should call CloudinaryImageCleanupJob with image" do
+    allow(Rails).to receive(:env).and_return("staging")
     expect(Cloudinary::Api).to receive(:delete_resources).
       with([cloudinary_id]).and_return(true)
     CloudinaryImageCleanupJob.new.perform(cloudinary_id)
