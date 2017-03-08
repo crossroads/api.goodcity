5db565ac6c23c5d7cb2f1a3fc51e3593d697f948
trojanh <rajan.tiwari@kiprosh.com>
2017-03-08 17:45:09 +0530

    Checked country on stockit and created if not present

--- app/services/stockit/country_sync.rb
+++ app/services/stockit/country_sync.rb
@@ -5,8 +5,19 @@ module Stockit
 
     include Stockit::Base
 
+    def initialize(country_name_en , country_name_zh , country_code, country_region)
+      @name_en = country_name_en
+      @name_zh = country_name_zh
+      @code = country_code
+      @region_en = country_region
+    end
+
     def self.index
       new.index
+    end
+
+    def self.create(country_name_en, country_name_zh, country_code, country_region)
+      new(country_name_en, country_name_zh, country_code, country_region).create
     end
 
     def index
@@ -14,5 +25,15 @@ module Stockit
       get(url)
     end
 
+    def create
+      url = url_for("/api/v1/countries")
+      stockit_params = {
+        name_en: @name_en,
+        name_zh: @name_zh,
+        code: @code,
+        region_en: @region_en
+      }
+      post(url, stockit_params)
+    end
   end
 end
