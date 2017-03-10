c9a92e2d2dc56d73226c3d6eccb54dfd200080ac
trojanh <rajan.tiwari@kiprosh.com>
2017-03-10 12:33:57 +0530

    Removed country job and country sync changes suggested by Steve

--- app/services/stockit/country_sync.rb
+++ app/services/stockit/country_sync.rb
@@ -5,19 +5,8 @@ module Stockit
 
     include Stockit::Base
 
-    def initialize(country_name_en , country_name_zh , country_code, country_region)
-      @name_en = country_name_en
-      @name_zh = country_name_zh
-      @code = country_code
-      @region_en = country_region
-    end
-
     def self.index
       new.index
-    end
-
-    def self.create(country_name_en, country_name_zh, country_code, country_region)
-      new(country_name_en, country_name_zh, country_code, country_region).create
     end
 
     def index
@@ -25,15 +14,5 @@ module Stockit
       get(url)
     end
 
-    def create
-      url = url_for("/api/v1/countries")
-      stockit_params = {
-        name_en: @name_en,
-        name_zh: @name_zh,
-        code: @code,
-        region_en: @region_en
-      }
-      post(url, stockit_params)
-    end
   end
 end
