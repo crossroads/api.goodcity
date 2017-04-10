45d3b9bdccbf3865ff177c22e1e53a7301334fcd
trojanh <rajan.tiwari@kiprosh.com>
2017-04-10 18:20:11 +0530

    Refactoring: Made stub request multiline

--- spec/models/orders_package_spec.rb
+++ spec/models/orders_package_spec.rb
@@ -195,8 +195,12 @@ RSpec.describe OrdersPackage, type: :model do
   describe '#delete_unwanted_cancelled_packages' do
     before do
       stub_request(:put, "http://www.example.com/api/v1/items/destroy").
-         with(:body => "{\"gc_orders_package_id\":#{orders_package.id}}",
-              :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'Token'=>'jchahjfsjfvacterr6e87dfbdsbqvh3v4brrb', 'User-Agent'=>'Ruby'}).
+        with(:body => "{\"gc_orders_package_id\":#{orders_package.id}}",
+          :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'
+          gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Type'=>'application/json',
+          'Token'=>'jchahjfsjfvacterr6e87dfbdsbqvh3v4brrb',
+          'User-Agent'=>'Ruby'}).
          to_return(:status => 200, :body => "", :headers => {})
 
     end
