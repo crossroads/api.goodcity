ad563a420f727f7aa3e51233a68be95c150c4dd5
trojanh <rajan.tiwari@kiprosh.com>
2017-05-04 13:18:55 +0530

    removed repeated code from index method

--- app/controllers/api/v1/rejection_reasons_controller.rb
+++ app/controllers/api/v1/rejection_reasons_controller.rb
@@ -4,12 +4,7 @@ module Api::V1
     load_and_authorize_resource :rejection_reason, parent: false
 
     def index
-      if params[:ids].blank?
-        render json: RejectionReason.cached_json
-        return
-      end
-      @rejection_reasons = @rejection_reasons.find( params[:ids].split(",") ) if params[:ids].present?
-      render json: @rejection_reasons, each_serializer: serializer
+      render_object(@rejection_reasons, RejectionReason, params)
     end
 
     def show
