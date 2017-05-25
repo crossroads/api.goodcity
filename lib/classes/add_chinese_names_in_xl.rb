404dfee29d330c59a49126e51188e158f2727ed9
trojanh <rajan.tiwari@kiprosh.com>
2017-05-25 11:49:41 +0530

    GCW-1552-Refactoring

--- lib/classes/add_chinese_names_in_xl.rb
+++ lib/classes/add_chinese_names_in_xl.rb
@@ -3,29 +3,30 @@ class AddChineseNamesInXl
   def initialize(path)
     @file_path = File.expand_path(path)
     @workbook = nil
+    @worksheet = nil
   end
 
-  def update_cell_values(worksheet, row_num, en_name, zh_name)
-    worksheet[row_num][1].change_contents(en_name)
-    worksheet.add_cell(row_num, 3, zh_name)
+  def update_cell_values(row_num, en_name, zh_name)
+    @worksheet[row_num][1].change_contents(en_name)
+    @worksheet.add_cell(row_num, 3, zh_name)
   end
 
-  def check_zh_name_and_update_cells(worksheet, cell_value, row_num)
+  def check_zh_name_and_update_cells(cell_value, row_num)
     separate_names = ChineseNameSeparator.new(cell_value)
     if (separate_names.find_zh_name_index)
       puts "#{row_num+1}\t cell_value before update: \t#{cell_value}"
       en_name = separate_names.get_en_name
       zh_name = separate_names.get_zh_name
-      update_cell_values(worksheet, row_num, en_name, zh_name)
+      update_cell_values(row_num, en_name, zh_name)
     end
   end
 
   def find_chinese_names_and_update_cells
-    worksheet = open_xl_workbook
+    open_xl_workbook
     row_num = 0
-    while (worksheet[row_num]&&worksheet[row_num][1].value)
-      cell_value = worksheet[row_num][1].value
-      check_zh_name_and_update_cells(worksheet, cell_value, row_num)
+    while (@worksheet[row_num]&&@worksheet[row_num][1].value)
+      cell_value = @worksheet[row_num][1].value
+      check_zh_name_and_update_cells(cell_value, row_num)
       row_num += 1
     end
     save_workbook
@@ -36,7 +37,7 @@ class AddChineseNamesInXl
   def open_xl_workbook
     @workbook = RubyXL::Parser.parse(@file_path)
     @workbook.stream
-    @workbook[0]
+    @worksheet = @workbook[0]
   end
 
   def save_workbook
