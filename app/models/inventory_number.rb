825ae1b27df369d0e449062110c1b8b29ab815f9
trojanh <rajan.tiwari@kiprosh.com>
2017-06-22 18:30:19 +0530

    Added code to return missing codes

--- app/models/inventory_number.rb
+++ app/models/inventory_number.rb
@@ -1,5 +1,6 @@
 class InventoryNumber < ActiveRecord::Base
-
+  validates :code, presence: true
+  validates :code, uniqueness: true
   def self.all_codes
     select("CAST(code AS integer) AS number").map(&:number)
   end
@@ -10,7 +11,12 @@ class InventoryNumber < ActiveRecord::Base
   end
 
   def self.available_code
-    code = ((1..latest_code).to_a - all_codes).first || (latest_code + 1)
+    code  = missing_code.present || where("CAST(code as INTEGER) <= ?", count).order("code").last.try(:code).try(:to_i)+1 || (latest_code + 1)
     code.to_s.rjust(6, "0")
+  end
+
+  def self.missing_code
+    codes = ActiveRecord::Base.connection.exec_query("SELECT s.i AS missing_cmd FROM generate_series(1,#{code}) s(i) WHERE NOT EXISTS (SELECT 1 FROM inventory_numbers where CAST(code AS INTEGER) = s.i)").rows
+    codes.flatten
   end
 end
