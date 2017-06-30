89d1f24aa3b1931d3dd0aad0f2091074524c2ada
trojanh <rajan.tiwari@kiprosh.com>
2017-06-30 16:45:15 +0530

    removed quantity condition from packages_locations

--- app/models/concerns/package_quantity_validator.rb
+++ app/models/concerns/package_quantity_validator.rb
@@ -1,21 +1,13 @@
 class PackageQuantityValidator < ActiveModel::Validator
   def validate(record)
     package = record.package
-    if(package.present? && is_valid_total_quantity?(record))
+    if(package.present? && is_invalid_total_quantity?(record))
       record.errors.add(:quantity, "cannot be greater than package quantity")
     end
   end
 
-  def is_valid_total_quantity?(record)
+  def is_invalid_total_quantity?(record)
     total = record.package.try(record.class.table_name).where.not(id: record.id).pluck(:quantity).sum + record.quantity
-    total > get_package_quantity(record)
-  end
-
-  def get_package_quantity(record)
-    if(record.class === "OrdersPackage")
-      record.package.recived_quantity
-    else #(record.class === "PackagesLocation")
-      record.package.quantity
-    end
+    total > record.package.received_quantity
   end
 end
