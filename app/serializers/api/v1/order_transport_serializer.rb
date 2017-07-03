97a21cc600fb9a89ce4cf5755a1f726000b6ddfa
trojanh <rajan.tiwari@kiprosh.com>
2017-07-03 18:57:24 +0530

    Added serializer for designation_id

--- app/serializers/api/v1/order_transport_serializer.rb
+++ app/serializers/api/v1/order_transport_serializer.rb
@@ -4,10 +4,18 @@ module Api::V1
 
     embed :ids, include: true
     attributes :id, :order_id, :scheduled_at, :timeslot, :gogovan_transport_id,
-      :transport_type, :need_english, :need_cart, :need_carry
+      :transport_type, :need_english, :need_cart, :need_carry, :designation_id
 
     has_one :contact, serializer: ContactSerializer
     has_one :gogovan_order, serializer: GogovanOrderSerializer
+
+    def designation_id
+      object.order_id
+    end
+
+    def designation_id__sql
+      "order_id"
+    end
 
   end
 end
