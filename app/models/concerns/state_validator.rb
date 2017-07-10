159bcf4395414d19246ac621242bd2ff61fb22b9
namrataukirde <namrata.ukirde@kiprosh.com>
2017-07-10 12:54:47 +0530

    state vaildation for partial quantity

--- app/models/concerns/state_validator.rb
+++ app/models/concerns/state_validator.rb
@@ -1,6 +1,7 @@
 class StateValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if record.state_was == value && record.package.received_quantity == record.quantity
+    received_quantity = record.package.received_quantity
+    if record.state_was == value && received_quantity == record.quantity && record.quantity_was == received_quantity
       record.errors.add(:state, 'same state')
     end
   end
