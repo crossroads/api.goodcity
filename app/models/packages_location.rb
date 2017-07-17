3f457f352c00278b68e82e46ac3d51509c93d406
trojanh <rajan.tiwari@kiprosh.com>
2017-07-17 13:59:37 +0530

    Added model level validations for referenced tables in OrdersPackage and PackagesLocation

--- app/models/packages_location.rb
+++ app/models/packages_location.rb
@@ -4,6 +4,7 @@ class PackagesLocation < ActiveRecord::Base
 
   has_paper_trail class_name: 'Version'
 
+  validates :package_id, :location_id presence: true
   validates :quantity,  numericality: { greater_than_or_equal_to: 0 }
   validates_with PackageQuantityValidator
 
