7bfee0479e3054081fc6892e94ac65b42c5bc713
trojanh <rajan.tiwari@kiprosh.com>
2017-07-25 19:25:10 +0530

    Added specs and fixed failing spec

--- spec/models/packages_location_spec.rb
+++ spec/models/packages_location_spec.rb
@@ -13,10 +13,63 @@ RSpec.describe PackagesLocation, type: :model do
   end
 
   describe 'Validations' do
-    it 'validates quantity' do
-      is_expected.to_not allow_value(-1).for(:quantity)
-      is_expected.to allow_value(rand(1..99999999)).for(:quantity)
+
+
+    context 'validates quantity: ' do
+      let!(:package) { create :package, :received }
+      let!(:packages_location) { package.packages_locations.first}
+
+      it 'quantity can not be less than 0' do
+        packages_location.quantity = -1
+        expect(packages_location.valid?).to be(false)
+      end
+
+      it 'quantity can not be greater than package.received_quantity' do
+        packages_location.quantity = package.quantity+rand(1..10)
+        expect(packages_location.valid?).to be(false)
+      end
+
+      it  'quantity can be less than package.received_quantity' do
+        packages_location.quantity = package.quantity-rand(1..3)
+        expect(packages_location.valid?).to be(true)
+      end
+
+      it  'quantity can be equal package.received_quantity' do
+        packages_location.quantity = package.received_quantity
+        expect(packages_location.valid?).to be(true)
+      end
     end
+
+    context 'validates package ' do
+      let!(:package) { create :package, :with_inventory_number, state: "received" }
+
+      it "should have inventory_number" do
+        package.packages_locations.build(quantity: 5)
+        expect(package.save).to be(true)
+      end
+
+      it "inventory_number cannot be nil" do
+        package.update(inventory_number: nil)
+        package.packages_locations.build(quantity: 5)
+        expect(package.save).to be(false)
+      end
+    end
+
+    context 'validates package ' do
+      let!(:package) { create :package, :with_inventory_number, state: "received" }
+
+      it "should be in received state" do
+        package.packages_locations.build(quantity: 5)
+        expect(package.save).to be(true)
+      end
+
+      it "is not in received state" do
+        package.update(state: "missing")
+        package.packages_locations.build(quantity: 5)
+        expect(package.save).to be(false)
+      end
+    end
+
   end
 
   describe '#update_quantity' do
