bd518ee0f439e17a9644c8c25318ed8c36f1e3cb
trojanh <rajan.tiwari@kiprosh.com>
2017-08-10 16:06:37 +0530

    GCW-1661 Remove 'Dispatched' and 'Multiple' from recently used locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -30,7 +30,6 @@ class Location < ActiveRecord::Base
       (object_changes ->> 'location_id') IS NOT NULL AND
       CAST(whodunnit AS integer) = ? AND
       (object_changes ->> 'created_at') >= (?)", ['create', 'update'], user_id, 15.days.ago).
-    order("locations.id, recently_used_at DESC")
+    order("locations.id, recently_used_at DESC").where("building NOT IN (?)", ['Dispatched', 'Multiple'])
   end
 end
-
