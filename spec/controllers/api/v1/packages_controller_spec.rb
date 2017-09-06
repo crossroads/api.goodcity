6c472725e354ebf84b63aa64e6d5270d4296c146
namrataukirde <namrata.ukirde@kiprosh.com>
2017-09-06 13:34:12 +0530

    fix package_controller_spec

--- spec/controllers/api/v1/packages_controller_spec.rb
+++ spec/controllers/api/v1/packages_controller_spec.rb
@@ -240,10 +240,10 @@ RSpec.describe Api::V1::PackagesController, type: :controller do
         }
 
         it 'dispatches orders_package if exists with same designation' do
-          package = create :package, :stockit_package, designation_name: 'abc'
+          package = create :package, :stockit_package, stockit_id: 15, designation_name: 'abc'
           orders_package = create :orders_package, package: package, order: order, state: 'designated'
           stockit_params_with_sent_on_and_designation[:stockit_id] = package.reload.stockit_id
-           expect{
+          expect{
             post :create, format: :json, package: stockit_params_with_sent_on_and_designation
           }.to change(OrdersPackage, :count).by(0)
           test_package_changes(package, response.status, order.code)
