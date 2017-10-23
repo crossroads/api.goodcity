022d0d7dc14df5a265dde18c5390f2d558e01b76
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-10-23 12:40:40 +0530

    Users added to organisation serialiser

--- app/serializers/api/v1/organisation_serializer.rb
+++ app/serializers/api/v1/organisation_serializer.rb
@@ -1,6 +1,7 @@
 module Api::V1
   class OrganisationSerializer < ApplicationSerializer
     embed :ids, include: true
+    has_many :users, serializer: UserSerializer
 
     attributes :id, :name_en, :name_zh_tw, :description_en, :description_zh_tw, :registration, :website
   end
