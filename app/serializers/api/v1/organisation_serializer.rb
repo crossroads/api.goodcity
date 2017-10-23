ca78e30f0780461ae014a1129e98800f5de6f9ef
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-10-23 13:12:04 +0530

    User relationship removed from organisation serializer

--- app/serializers/api/v1/organisation_serializer.rb
+++ app/serializers/api/v1/organisation_serializer.rb
@@ -1,7 +1,6 @@
 module Api::V1
   class OrganisationSerializer < ApplicationSerializer
     embed :ids, include: true
-    has_many :user_profiles, serializer: UserProfileSerializer
 
     attributes :id, :name_en, :name_zh_tw, :description_en, :description_zh_tw, :registration, :website
   end
