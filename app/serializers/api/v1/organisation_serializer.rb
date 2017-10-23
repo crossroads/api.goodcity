3ff2fa10fbb8e4f56af1a66d587196069aad50b5
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-10-23 12:48:24 +0530

    User serializer changed to user_profile

--- app/serializers/api/v1/organisation_serializer.rb
+++ app/serializers/api/v1/organisation_serializer.rb
@@ -1,7 +1,7 @@
 module Api::V1
   class OrganisationSerializer < ApplicationSerializer
     embed :ids, include: true
-    has_many :users, serializer: UserSerializer
+    has_many :user_profiles, serializer: UserProfileSerializer
 
     attributes :id, :name_en, :name_zh_tw, :description_en, :description_zh_tw, :registration, :website
   end
