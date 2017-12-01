bfecd68d4559ba3fdf418ff01cbfbc175ee223b1
namrataukirde <namrata.ukirde@kiprosh.com>
2017-12-01 20:08:05 +0530

    added show action for organisation

--- app/controllers/api/v1/organisations_controller.rb
+++ app/controllers/api/v1/organisations_controller.rb
@@ -11,11 +11,16 @@ module Api::V1
       error 500, "Internal Server Error"
     end
 
-    api :GET, '/v1/orders', "List all orders"
+    api :GET, '/v1/organisations', "List all organisations"
     def index
       records = @organisations.search(params["searchText"]).page(params["page"]).per(params["per_page"])
       @organisations = ActiveModel::ArraySerializer.new(records, each_serializer: serializer, root: "organisations").to_json
       render json: @organisations.chop + ",\"meta\":{\"total_pages\": #{records.total_pages}, \"search\": \"#{params['searchText']}\"}}"
+    end
+
+    api :GET, '/v1/organisations/1', "Details of a package"
+    def show
+      render json: @organisation, serializer: serializer
     end
 
     private
