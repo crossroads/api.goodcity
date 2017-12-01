5648a7a1b448439fb743501898073598b0930a3e
namrataukirde <namrata.ukirde@kiprosh.com>
2017-12-01 17:01:48 +0530

    added meta data in response and api doc changes

--- app/controllers/api/v1/organisations_controller.rb
+++ app/controllers/api/v1/organisations_controller.rb
@@ -2,10 +2,20 @@ module Api::V1
   class OrganisationsController < Api::V1::ApiController
     load_and_authorize_resource :organisation, parent: false
 
+    resource_description do
+      short "list, show organisations"
+      formats ["json"]
+      error 401, "Unauthorized"
+      error 404, "Not Found"
+      error 422, "Validation Error"
+      error 500, "Internal Server Error"
+    end
+
     api :GET, '/v1/orders', "List all orders"
     def index
-      @organisations = @organisations.search(params["searchText"])
-      render json: @organisations, each_serializer: serializer
+      records = @organisations.search(params["searchText"]).page(params["page"]).per(params["per_page"])
+      @organisations = ActiveModel::ArraySerializer.new(records, each_serializer: serializer, root: "organisations").to_json
+      render json: @organisations.chop + ",\"meta\":{\"total_pages\": #{records.total_pages}, \"search\": \"#{params['searchText']}\"}}"
     end
 
     private
