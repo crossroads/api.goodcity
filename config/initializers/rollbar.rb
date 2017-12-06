112e66c88e851e3fb59b3cae1383451485f8e22d
viveksriram1 <vivek.sriram@kiprosh.com>
2017-12-06 13:39:30 +0530

    remove rollbar access token from secrete yml

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,7 +2,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = Rails.application.secrets.rollbar['rollbar_access_token']
+  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
   # Here we'll disable in 'test' and 'development':
   config.enabled = Rails.env.production? || Rails.env.staging?
