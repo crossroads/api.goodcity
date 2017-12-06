468aeb37cd2bb89ee2e1ec9bb4c4389ff9ecc72f
namrataukirde <namrata.ukirde@kiprosh.com>
2017-12-06 21:12:06 +0530

    fix pg duplicate aliase error

--- app/serializers/api/v1/organisations_user_serializer.rb
+++ app/serializers/api/v1/organisations_user_serializer.rb
@@ -2,7 +2,6 @@ module Api::V1
   class OrganisationsUserSerializer < ActiveModel::Serializer
     embed :ids, include: true
     attributes :id, :user_id, :organisation_id, :role
-    has_one :organisation, serializer: OrganisationSerializer
     has_one :user, serializer: UserSerializer
   end
 end
