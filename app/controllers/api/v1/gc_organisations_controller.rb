cf2899ce1ebef9458754d1144991b0376bf9010c
namrataukirde <namrata.ukirde@kiprosh.com>
2017-12-13 20:59:12 +0530

    add gc_organisations controller instead of organisations controller

--- app/controllers/api/v1/gc_organisations_controller.rb
+++ app/controllers/api/v1/gc_organisations_controller.rb
@@ -1,5 +1,5 @@
 module Api::V1
-  class OrganisationsController < Api::V1::ApiController
+  class GcOrganisationsController < Api::V1::ApiController
     load_and_authorize_resource :organisation, parent: false
 
     resource_description do
@@ -14,14 +14,15 @@ module Api::V1
     api :GET, '/v1/organisations', "List all organisations"
     def index
       records = @organisations.search(params["searchText"]).page(params["page"]).per(params["per_page"])
-      @organisations = ActiveModel::ArraySerializer.new(records, each_serializer: serializer, root: "organisations").to_json
+      @organisations = ActiveModel::ArraySerializer.new(records, each_serializer: serializer, root: "gc_organisations").to_json
       render json: @organisations.chop + ",\"meta\":{\"total_pages\": #{records.total_pages}, \"search\": \"#{params['searchText']}\"}}"
     end
 
     api :GET, '/v1/organisations/1', "Details of a package"
     def show
       # render json: @organisation, serializer: OrganisationSerializer
-      render json: Api::V1::OrganisationSerializer.new(@organisation)
+      @organisation =  Api::V1::OrganisationSerializer.new(@organisation, root: "gc_organisations")
+      render json: @organisation
     end
 
     private
