5e900b95ad7da1e6701d37559602ab9cd2ddb808
viveksriram1 <vivek.sriram@kiprosh.com>
2017-12-14 17:39:25 +0530

    add browse controller test cases

---
+++ spec/controllers/api/v1/browse_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::BrowseController, type: :controller do
+
+  let(:user) { create(:user_with_token) }
+
+  describe "GET fetch_packages" do
+    before { generate_and_set_token(user) }
+      it "list all the packages" do
+        get :fetch_packages
+        expect(response.status).to eq(200)
+      end
+  end
+end
