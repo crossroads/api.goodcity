f32c586450c80f92ec25d73789088f83427774a4
namrataukirde <namrata.ukirde@kiprosh.com>
2017-12-18 21:28:31 +0530

    use nested attributes for organisation user creation

--- app/controllers/api/v1/organisations_users_controller.rb
+++ app/controllers/api/v1/organisations_users_controller.rb
@@ -10,26 +10,28 @@ module Api::V1
       error 500, "Internal Server Error"
     end
 
+    def_param_group :organisations_user do
+      param :organisations_user, Hash, required: true do
+        param :organisation_id, lambda { |val| [String, Fixnum].include? val.class }, desc: "Organisation of User", allow_nil: true
+      end
+    end
+
     api :POST, "/v1/organisations_user", "Create a package"
     # param_group :organisations_user
     def create
-      if organisations_user_record
-        render json: @organisations_user, serializer: serializer
-      else
-        render json: { errors: @organisations_user.errors.full_messages }.to_json , status: 422
-      end
+      @organisations_user.user.permission = Permission.charity
+      save_and_render_object(@organisations_user)
     end
 
     private
 
-    def serializer
-      Api::V1::OrganisationsUserSerializer
+    def organisations_user_params
+      params.require(:organisations_user).permit(:organisation_id, :position, user_attributes: [:first_name,
+        :last_name, :mobile, :email])
     end
 
-    def organisations_user_record
-      @organisations_user.user = User.build_new_user(params["oranisations_users"])
-      @organisations_user.organisation_id = params["oranisations_users"]["organisation_id"]
-      @organisations_user.save
+    def serializer
+      Api::V1::OrganisationsUserSerializer
     end
   end
 end
