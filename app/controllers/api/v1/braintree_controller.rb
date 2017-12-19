ee2a8cfa35b36bfd03242aca254ee51b8ca6f4d3
namrataukirde <namrata.ukirde@kiprosh.com>
2017-12-19 21:42:50 +0530

    fix code climate module issues

--- app/controllers/api/v1/braintree_controller.rb
+++ app/controllers/api/v1/braintree_controller.rb
@@ -1,34 +1,36 @@
-module Api::V1
-  class BraintreeController < Api::V1::ApiController
-    before_action :authorize_user, :braintree_object
+module Api
+  module V1
+    class BraintreeController < Api::V1::ApiController
+      before_action :authorize_user, :braintree_object
 
-    api :GET, '/v1/braintree/generate_token', "Get braintree token"
-    def generate_token
-      token = @braintree.client_token
-      render json: { braintree_token: token }.to_json
-    end
+      api :GET, '/v1/braintree/generate_token', "Get braintree token"
+      def generate_token
+        token = @braintree.client_token
+        render json: { braintree_token: token }.to_json
+      end
 
-    api :POST, '/v1/braintree/make_transaction', "Get braintree token"
-    param :amount, String, desc: "The donation amount donor wants to donate."
-    param :payment_method_nonce, String, desc: "The token(nonce) returned by braintree server."
-    def make_transaction
-      response = @braintree.create_transaction(params["amount"], params["payment_method_nonce"])
-      render json: { response: response.success?,
-        error: response_error_message(response) }.to_json
-    end
+      api :POST, '/v1/braintree/make_transaction', "Get braintree token"
+      param :amount, String, desc: "The donation amount donor wants to donate."
+      param :payment_method_nonce, String, desc: "The token(nonce) returned by braintree server."
+      def make_transaction
+        response = @braintree.create_transaction(params["amount"], params["payment_method_nonce"])
+        render json: { response: response.success?,
+          error: response_error_message(response) }.to_json
+      end
 
-    private
+      private
 
-    def authorize_user
-      authorize!(:current_user_profile, User)
-    end
+      def authorize_user
+        authorize!(:current_user_profile, User)
+      end
 
-    def braintree_object
-      @braintree = BraintreeService.new(User.current_user)
-    end
+      def braintree_object
+        @braintree = BraintreeService.new(User.current_user)
+      end
 
-    def response_error_message(response)
-      response.try(:errors).try(:first).try(:message) || response.try(:transaction).try(:processor_response_text)
+      def response_error_message(response)
+        response.try(:errors).try(:first).try(:message) || response.try(:transaction).try(:processor_response_text)
+      end
     end
   end
 end
