9683b6a75e4490e7d495c70674e1231496da32a0
namrataukirde <namrata.ukirde@kiprosh.com>
2017-12-19 21:42:50 +0530

    fix code climate module issues

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -1,48 +1,50 @@
-module Api::V1
-  class AddressesController < Api::V1::ApiController
-    load_and_authorize_resource :address, parent: false
+module Api
+  module V1
+    class AddressesController < Api::V1::ApiController
+      load_and_authorize_resource :address, parent: false
 
-    resource_description do
-      short 'List and create addresses for Contacts and Users'
-      formats ['json']
-      error 401, "Unauthorized"
-      error 404, "Not Found"
-      error 422, "Validation Error"
-      error 500, "Internal Server Error"
-    end
-
-    def_param_group :address do
-      param :address, Hash, required: true do
-        param :street, String, desc: "Street name", allow_nil: true
-        param :flat, String, desc: "Flat name", allow_nil: true
-        param :building, String, desc: "Building name", allow_nil: true
-        param :district_id, String, desc: "Hong Kong district"
-        param :address_type, String, desc: "Type of address, usually 'Collection' or 'Profile'"
-        param :addressable_type, String, desc: "Object the address belongs to: 'Contact' or 'User' (Polymorphic)"
-        param :addressable_id, String, desc: "Object id of the associated polymorphic record. Use contact_id or user_id"
+      resource_description do
+        short 'List and create addresses for Contacts and Users'
+        formats ['json']
+        error 401, "Unauthorized"
+        error 404, "Not Found"
+        error 422, "Validation Error"
+        error 500, "Internal Server Error"
       end
-    end
 
-    api :POST, '/v1/addresses', "Create an address"
-    param_group :address
-    def create
-      save_and_render_object(@address)
-    end
+      def_param_group :address do
+        param :address, Hash, required: true do
+          param :street, String, desc: "Street name", allow_nil: true
+          param :flat, String, desc: "Flat name", allow_nil: true
+          param :building, String, desc: "Building name", allow_nil: true
+          param :district_id, String, desc: "Hong Kong district"
+          param :address_type, String, desc: "Type of address, usually 'Collection' or 'Profile'"
+          param :addressable_type, String, desc: "Object the address belongs to: 'Contact' or 'User' (Polymorphic)"
+          param :addressable_id, String, desc: "Object id of the associated polymorphic record. Use contact_id or user_id"
+        end
+      end
 
-    api :GET, '/v1/address/1', "Show an address"
-    def show
-      render json: @address, serializer: serializer
-    end
+      api :POST, '/v1/addresses', "Create an address"
+      param_group :address
+      def create
+        save_and_render_object(@address)
+      end
 
-    private
+      api :GET, '/v1/address/1', "Show an address"
+      def show
+        render json: @address, serializer: serializer
+      end
 
-    def serializer
-      Api::V1::AddressSerializer
-    end
+      private
 
-    def address_params
-      params.require(:address).permit(:street, :flat, :building,
-        :district_id, :address_type, :addressable_id, :addressable_type)
+      def serializer
+        Api::V1::AddressSerializer
+      end
+
+      def address_params
+        params.require(:address).permit(:street, :flat, :building,
+          :district_id, :address_type, :addressable_id, :addressable_type)
+      end
     end
   end
 end
--- app/controllers/api/v1/donor_conditions_controller.rb
+++ app/controllers/api/v1/donor_conditions_controller.rb
@@ -1,32 +1,34 @@
-module Api::V1
-  class DonorConditionsController < Api::V1::ApiController
-    load_and_authorize_resource :donor_condition, parent: false
-    skip_before_action :validate_token, only: :index
+module Api
+  module V1
+    class DonorConditionsController < Api::V1::ApiController
+      load_and_authorize_resource :donor_condition, parent: false
+      skip_before_action :validate_token, only: :index
 
-    resource_description do
-      short 'Categorise the state a donation item is in. E.g. New / Used / Broken'
-      formats ['json']
-      error 401, "Unauthorized"
-      error 404, "Not Found"
-      error 422, "Validation Error"
-      error 500, "Internal Server Error"
-    end
+      resource_description do
+        short 'Categorise the state a donation item is in. E.g. New / Used / Broken'
+        formats ['json']
+        error 401, "Unauthorized"
+        error 404, "Not Found"
+        error 422, "Validation Error"
+        error 500, "Internal Server Error"
+      end
 
-    api :GET, '/v1/donor_conditions', "List all donor conditions."
-    param :ids, Array, of: Integer, desc: "Filter by donor condition ids e.g. ids = [1,2,3,4]"
-    def index
-      render_object_with_cache(@donor_conditions, params[:ids])
-    end
+      api :GET, '/v1/donor_conditions', "List all donor conditions."
+      param :ids, Array, of: Integer, desc: "Filter by donor condition ids e.g. ids = [1,2,3,4]"
+      def index
+        render_object_with_cache(@donor_conditions, params[:ids])
+      end
 
-    api :GET, '/v1/donor_conditions/1', "List a Donor-Condition"
-    def show
-      render json: @donor_condition, serializer: serializer
-    end
+      api :GET, '/v1/donor_conditions/1', "List a Donor-Condition"
+      def show
+        render json: @donor_condition, serializer: serializer
+      end
 
-    private
+      private
 
-    def serializer
-      Api::V1::DonorConditionSerializer
+      def serializer
+        Api::V1::DonorConditionSerializer
+      end
     end
   end
 end
