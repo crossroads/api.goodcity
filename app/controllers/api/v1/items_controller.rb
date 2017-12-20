fb03355fcf41e40bbef81acd8484e1782f82cc3a
namrataukirde <namrata.ukirde@kiprosh.com>
2017-12-20 13:08:53 +0530

    fix module issue

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,131 +1,133 @@
-module Api::V1
-  class ItemsController < Api::V1::ApiController
-    load_and_authorize_resource :item, parent: false
+module Api
+  module V1
+    class ItemsController < Api::V1::ApiController
+      load_and_authorize_resource :item, parent: false
 
-    resource_description do
-      short 'Get, create, update and delete items.'
-      description <<-EOS
-        == Item states
-        [link:/doc/item_state.png]
-      EOS
-      formats ['json']
-      error 401, "Unauthorized"
-      error 404, "Not Found"
-      error 422, "Validation Error"
-      error 500, "Internal Server Error"
-    end
-
-    def_param_group :item do
-      param :item, Hash, required: true do
-        param :donor_description, String, allow_nil: true, desc: "Description/Details of item given by Item-Donor"
-        param :donor_condition_id, String, desc: "Describes the item's condition "<< DonorCondition.pluck(:id, :name_en).map{ |x| "#{x.first} - #{x.last}" }.join("; "), allow_nil: true
-        param :state_event, Item.valid_events, allow_nil: true, desc: "Fires the state transition (if allowed) for this item. 'submit' is for when the donor has completed creating the item and will change the state from draft to submitted. Items in draft state should be hidden from offer view and if donor clicks to add an item the draft item should be loaded allowing them to continue creating the item."
-        param :offer_id, String, desc: "Id of Offer to which item belongs."
-        param :package_type_id, String, allow_nil: true, desc: "Not yet used"
-        param :rejection_reason_id, String, desc: "A categorisation describing the reason the item was rejected "<< RejectionReason.pluck(:id, :name_en).map{ |x| "#{x.first} - #{x.last}" }.join("; "), allow_nil: true
-        param :reject_reason, String, allow_nil: true, desc: "Reviewer description of why the item was rejected"
-        param :rejection_comments, String, allow_nil: true, desc: "Reviewer description of why the item was rejected given to Donor."
-      end
-    end
-
-    api :POST, '/v1/items', "Create an item"
-    param_group :item
-    def create
-      save_and_render_object(@item)
-    end
-
-    api :GET, '/v1/item/1', "Get an item"
-    def show
-      render json: @item, serializer: serializer
-    end
-
-    api :DELETE, '/v1/items/1', "Delete an item"
-    description "If this item's offer is in draft state it will be destroyed. Any other state and it will be marked as deleted but remain recoverable."
-    def destroy
-      offer = @item.offer
-      return single_item_error if offer.has_single_item?
-      @item.remove
-      update_offer_state(offer)
-      render json: {}
-    end
-
-    api :PUT, '/v1/items/1', "Update an item"
-    param_group :item
-    def update
-      offer = @item.offer
-
-      # if rejecting last accepted item but gogovan is booked return error
-      if rejecting_last_item_having_confirmed_ggv(offer)
-        return render_require_ggv_cancel_error
+      resource_description do
+        short 'Get, create, update and delete items.'
+        description <<-EOS
+          == Item states
+          [link:/doc/item_state.png]
+        EOS
+        formats ['json']
+        error 401, "Unauthorized"
+        error 404, "Not Found"
+        error 422, "Validation Error"
+        error 500, "Internal Server Error"
       end
 
-      if @item.update_attributes(item_params)
-        update_offer_state(offer)
+      def_param_group :item do
+        param :item, Hash, required: true do
+          param :donor_description, String, allow_nil: true, desc: "Description/Details of item given by Item-Donor"
+          param :donor_condition_id, String, desc: "Describes the item's condition "<< DonorCondition.pluck(:id, :name_en).map{ |x| "#{x.first} - #{x.last}" }.join("; "), allow_nil: true
+          param :state_event, Item.valid_events, allow_nil: true, desc: "Fires the state transition (if allowed) for this item. 'submit' is for when the donor has completed creating the item and will change the state from draft to submitted. Items in draft state should be hidden from offer view and if donor clicks to add an item the draft item should be loaded allowing them to continue creating the item."
+          param :offer_id, String, desc: "Id of Offer to which item belongs."
+          param :package_type_id, String, allow_nil: true, desc: "Not yet used"
+          param :rejection_reason_id, String, desc: "A categorisation describing the reason the item was rejected "<< RejectionReason.pluck(:id, :name_en).map{ |x| "#{x.first} - #{x.last}" }.join("; "), allow_nil: true
+          param :reject_reason, String, allow_nil: true, desc: "Reviewer description of why the item was rejected"
+          param :rejection_comments, String, allow_nil: true, desc: "Reviewer description of why the item was rejected given to Donor."
+        end
+      end
+
+      api :POST, '/v1/items', "Create an item"
+      param_group :item
+      def create
+        save_and_render_object(@item)
+      end
+
+      api :GET, '/v1/item/1', "Get an item"
+      def show
         render json: @item, serializer: serializer
-      else
-        render json: @item.errors.to_json, status: 422
       end
-    end
 
-    def designate_stockit_item_set
-      @item.designate_set_to_stockit_order(params[:package])
-      render json: @item, serializer: stockit_serializer
-    end
-
-    def update_designation_of_set
-      @item.update_designation(params[:package])
-      render json: @item, serializer: stockit_serializer
-    end
-
-    def dispatch_stockit_item_set
-      @item.dispatch_set_to_stockit_order(params[:package])
-      render json: @item, serializer: stockit_serializer
-    end
-
-    def move_stockit_item_set
-      @item.move_set_to_location(params["location_id"])
-      render json: @item, serializer: stockit_serializer
-    end
-
-    private
-
-    def stockit_serializer
-      Api::V1::StockitSetItemSerializer
-    end
-
-    def update_offer_state(offer)
-      offer.items.reload
-      if [:reviewed, :scheduled].include?(offer.state_name) && offer.items.all?{|i| i.state_name == :rejected}
-        offer.re_review!
+      api :DELETE, '/v1/items/1', "Delete an item"
+      description "If this item's offer is in draft state it will be destroyed. Any other state and it will be marked as deleted but remain recoverable."
+      def destroy
+        offer = @item.offer
+        return single_item_error if offer.has_single_item?
+        @item.remove
+        update_offer_state(offer)
+        render json: {}
       end
-    end
 
-    def item_params
-      params.require(:item).permit(:donor_description, :donor_condition_id,
-        :state_event, :offer_id, :package_type_id, :rejection_reason_id,
-        :reject_reason, :rejection_comments)
-    end
+      api :PUT, '/v1/items/1', "Update an item"
+      param_group :item
+      def update
+        offer = @item.offer
 
-    def serializer
-      Api::V1::ItemSerializer
-    end
+        # if rejecting last accepted item but gogovan is booked return error
+        if rejecting_last_item_having_confirmed_ggv(offer)
+          return render_require_ggv_cancel_error
+        end
 
-    def single_item_error
-      render json:
-        { errors: 'Cannot delete the last item of a submitted offer' }.to_json,
-        status: 422
-    end
+        if @item.update_attributes(item_params)
+          update_offer_state(offer)
+          render json: @item, serializer: serializer
+        else
+          render json: @item.errors.to_json, status: 422
+        end
+      end
 
-    def rejecting_last_item_having_confirmed_ggv(offer)
-      item_params[:state_event] == 'reject' &&
-      offer.items.all?{ |i| i.state_name == :rejected || i.id == @item.id } &&
-      offer.gogovan_order && offer.scheduled? && !offer.can_cancel?
-    end
+      def designate_stockit_item_set
+        @item.designate_set_to_stockit_order(params[:package])
+        render json: @item, serializer: stockit_serializer
+      end
 
-    def render_require_ggv_cancel_error
-      render json: { errors: [{ requires_gogovan_cancellation:
-        'Cannot reject last item if there\'s a confirmed gogovan booking' }]
-        }.to_json, status: 422
+      def update_designation_of_set
+        @item.update_designation(params[:package])
+        render json: @item, serializer: stockit_serializer
+      end
+
+      def dispatch_stockit_item_set
+        @item.dispatch_set_to_stockit_order(params[:package])
+        render json: @item, serializer: stockit_serializer
+      end
+
+      def move_stockit_item_set
+        @item.move_set_to_location(params["location_id"])
+        render json: @item, serializer: stockit_serializer
+      end
+
+      private
+
+      def stockit_serializer
+        Api::V1::StockitSetItemSerializer
+      end
+
+      def update_offer_state(offer)
+        offer.items.reload
+        if [:reviewed, :scheduled].include?(offer.state_name) && offer.items.all?{|i| i.state_name == :rejected}
+          offer.re_review!
+        end
+      end
+
+      def item_params
+        params.require(:item).permit(:donor_description, :donor_condition_id,
+          :state_event, :offer_id, :package_type_id, :rejection_reason_id,
+          :reject_reason, :rejection_comments)
+      end
+
+      def serializer
+        Api::V1::ItemSerializer
+      end
+
+      def single_item_error
+        render json:
+          { errors: 'Cannot delete the last item of a submitted offer' }.to_json,
+          status: 422
+      end
+
+      def rejecting_last_item_having_confirmed_ggv(offer)
+        item_params[:state_event] == 'reject' &&
+        offer.items.all?{ |i| i.state_name == :rejected || i.id == @item.id } &&
+        offer.gogovan_order && offer.scheduled? && !offer.can_cancel?
+      end
+
+      def render_require_ggv_cancel_error
+        render json: { errors: [{ requires_gogovan_cancellation:
+          'Cannot reject last item if there\'s a confirmed gogovan booking' }]
+          }.to_json, status: 422
+      end
     end
   end
 end
--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,81 +1,83 @@
-module Api::V1
-  class LocationsController < Api::V1::ApiController
-    load_and_authorize_resource :location, parent: false
+module Api
+  module V1
+    class LocationsController < Api::V1::ApiController
+      load_and_authorize_resource :location, parent: false
 
-    resource_description do
-      formats ['json']
-      error 401, "Unauthorized"
-      error 404, "Not Found"
-      error 422, "Validation Error"
-      error 500, "Internal Server Error"
-    end
-
-    def_param_group :location do
-      param :location, Hash, required: true do
-        param :building, String, desc: "Name of building"
-        param :area, String, desc: "Name of area"
-        param :stockit_id, String, desc: "stockit location record id"
+      resource_description do
+        formats ['json']
+        error 401, "Unauthorized"
+        error 404, "Not Found"
+        error 422, "Validation Error"
+        error 500, "Internal Server Error"
       end
-    end
 
-    api :GET, '/v1/locations', "List all locations"
-    param :ids, Array, of: Integer, desc: "Filter by location ids e.g. ids = [1,2,3,4]"
-    def index
-      return search if params['searchText'].present?
-      return recent_locations if params['recently_used'].present?
-      if params[:ids].blank?
-        render json: Location.cached_json
-        return
+      def_param_group :location do
+        param :location, Hash, required: true do
+          param :building, String, desc: "Name of building"
+          param :area, String, desc: "Name of area"
+          param :stockit_id, String, desc: "stockit location record id"
+        end
       end
-      @locations = @locations.with_eager_load
-      @locations = @locations.find(params[:ids].split(",")) if params[:ids].present?
-      render json: @locations, each_serializer: serializer
-    end
 
-    api :POST, "/v1/locations", "Create or Update a location"
-    param_group :location
-    def create
-      if location_record.save
-        render json: @location, serializer: serializer, status: 201
-      else
-        render json: @location.errors.to_json, status: 422
+      api :GET, '/v1/locations', "List all locations"
+      param :ids, Array, of: Integer, desc: "Filter by location ids e.g. ids = [1,2,3,4]"
+      def index
+        return search if params['searchText'].present?
+        return recent_locations if params['recently_used'].present?
+        if params[:ids].blank?
+          render json: Location.cached_json
+          return
+        end
+        @locations = @locations.with_eager_load
+        @locations = @locations.find(params[:ids].split(",")) if params[:ids].present?
+        render json: @locations, each_serializer: serializer
       end
-    end
 
-    api :DELETE, "/v1/locations/1", "Delete Location"
-    description "If request comes from stockit, find record by matching it with stockit_id."
-    def destroy
-      @location = Location.find_by(stockit_id: params[:id]) if is_stockit_request
-      @location.try(:destroy)
-      render json: {}
-    end
+      api :POST, "/v1/locations", "Create or Update a location"
+      param_group :location
+      def create
+        if location_record.save
+          render json: @location, serializer: serializer, status: 201
+        else
+          render json: @location.errors.to_json, status: 422
+        end
+      end
 
-    def search
-      records = @locations.search(params['searchText']).
-        page(params["page"]).per(params["per_page"])
-      locations = ActiveModel::ArraySerializer.new(records, each_serializer: serializer, root: "locations").to_json
-      render json: locations.chop + ",\"meta\":{\"total_pages\": #{records.total_pages}, \"search\": \"#{params['searchText']}\"}}"
-    end
+      api :DELETE, "/v1/locations/1", "Delete Location"
+      description "If request comes from stockit, find record by matching it with stockit_id."
+      def destroy
+        @location = Location.find_by(stockit_id: params[:id]) if is_stockit_request
+        @location.try(:destroy)
+        render json: {}
+      end
 
-    def recent_locations
-      @locations = Location.recently_used(User.current_user.id)
-      render json: @locations, each_serializer: serializer
-    end
+      def search
+        records = @locations.search(params['searchText']).
+          page(params["page"]).per(params["per_page"])
+        locations = ActiveModel::ArraySerializer.new(records, each_serializer: serializer, root: "locations").to_json
+        render json: locations.chop + ",\"meta\":{\"total_pages\": #{records.total_pages}, \"search\": \"#{params['searchText']}\"}}"
+      end
 
-    private
+      def recent_locations
+        @locations = Location.recently_used(User.current_user.id)
+        render json: @locations, each_serializer: serializer
+      end
 
-    def location_record
-      @location = Location.where(stockit_id: location_params[:stockit_id]).first_or_initialize
-      @location.assign_attributes(location_params)
-      @location
-    end
+      private
 
-    def location_params
-      params.require(:location).permit(:stockit_id, :building, :area)
-    end
+      def location_record
+        @location = Location.where(stockit_id: location_params[:stockit_id]).first_or_initialize
+        @location.assign_attributes(location_params)
+        @location
+      end
 
-    def serializer
-      Api::V1::LocationSerializer
+      def location_params
+        params.require(:location).permit(:stockit_id, :building, :area)
+      end
+
+      def serializer
+        Api::V1::LocationSerializer
+      end
     end
   end
 end
