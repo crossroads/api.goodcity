af5f89e2c9289f9c2ed5afda2a6c4e0f1ca7ba77
namrataukirde <namrata.ukirde@kiprosh.com>
2017-12-28 13:26:34 +0530

    organisation serializer changes

--- app/serializers/api/v1/organisation_serializer.rb
+++ app/serializers/api/v1/organisation_serializer.rb
@@ -3,17 +3,10 @@ module Api::V1
     embed :ids, include: true
 
     attributes :id, :name_en, :name_zh_tw, :description_en, :description_zh_tw, :registration,
-      :website, :organisation_type_id, :district_id, :country_id, :order_ids, :created_at,
+      :website, :organisation_type_id, :district_id, :country_id, :created_at,
       :updated_at
 
     has_many :organisations_users, serializer: OrganisationsUserSerializer
-    has_many :orders, serializer: OrderSerializer
-
-    def order_ids
-    end
-
-    def order_ids__sql
-      'order_ids'
-    end
+    has_many :orders, serializer: OrderSerializer, root: :designations
   end
 end
