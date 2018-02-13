7c244086986f2267293f14f086ab07796f0af6f8
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-02-13 12:48:54 +0530

    OrdersPurposeSerializer specs added

--- spec/serializers/api/v1/purpose_serializer_spec.rb
+++ spec/serializers/api/v1/purpose_serializer_spec.rb
@@ -6,7 +6,6 @@ describe Api::V1::PurposeSerializer do
   let(:serializer) { Api::V1::PurposeSerializer.new(purpose) }
   let(:json)       { JSON.parse( serializer.to_json ) }
 
-
   it "creates JSON" do
     expect(json['purpose']['name_en']).to eql(purpose.name_en)
     expect(json['purpose']['name_zh_tw']).to eql(purpose.name_zh_tw)
