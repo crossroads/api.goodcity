3a8a6ec8e69d70b4e0121cf1906e93d9cfde4310
namrataukirde <namrata.ukirde@kiprosh.com>
2018-02-14 19:29:44 +0530

    define and modify various role methods on user model

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 class Role < ActiveRecord::Base
   has_many :user_roles
   has_many :users, through: :user_roles
+  has_many :role_permissions
+  has_many :permissions, through: :role_permissions
 end
