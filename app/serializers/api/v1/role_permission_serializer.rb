6e0e9bc1ebe1e9215263e3e17de8fd37d095749c
namrataukirde <namrata.ukirde@kiprosh.com>
2018-02-20 19:39:59 +0530

    serializer changes

--- app/serializers/api/v1/role_permission_serializer.rb
+++ app/serializers/api/v1/role_permission_serializer.rb
@@ -2,7 +2,8 @@ module Api::V1
   class RolePermissionSerializer < ApplicationSerializer
     embed :ids, include: true
 
-    has_one :role, serializer: RoleSerializer
+    attributes :id, :role_id
+
     has_one :permission, serializer: PermissionSerializer
   end
 end
