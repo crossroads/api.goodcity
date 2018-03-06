f9ac410b32d7084fb22b63a88acd1eba0ce065bb
namrataukirde <namrata.ukirde@kiprosh.com>
2018-03-06 20:26:01 +0530

    fix packages controller failing specs

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -214,6 +214,12 @@ FactoryGirl.define do
       end
     end
 
+    trait :with_can_manage_orders_permission do
+      after(:create) do |role|
+        role.permissions << (create :permission, name: 'can_manage_orders')
+      end
+    end
+
     trait :with_can_manage_orders_packages_permission do
       after(:create) do |role|
         role.permissions << (create :permission, name: 'can_manage_orders_packages')
