5ceaedb5e1a99bf9a9e5323a811845e092c2cd6a
namrataukirde <namrata.ukirde@kiprosh.com>
2018-03-07 16:34:45 +0530

    changes to fetch all roles on admin app instead of permissions

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,8 +1,11 @@
 class Role < ActiveRecord::Base
+  include CacheableJson
+
   has_many :user_roles
   has_many :users, through: :user_roles
-  has_many :role_permissions
+  has_many :role_permissions, dependent: :destroy
   has_many :permissions, through: :role_permissions
 
   scope :charity, ->{ where(name: 'Charity').first }
+  scope :visible,    ->{ where.not(name: 'api-write') }
 end
