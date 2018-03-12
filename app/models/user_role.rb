d5788ab8269fde8e6fa335974e0c9eba101d601e
namrataukirde <namrata.ukirde@kiprosh.com>
2018-03-12 16:36:33 +0530

    code refactoring

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -3,6 +3,6 @@ class UserRole < ActiveRecord::Base
   belongs_to :role
 
   def self.create_user_role(user_id, role_id)
-    UserRole.create(user_id: user_id, role_id: role_id)
+    create(user_id: user_id, role_id: role_id)
   end
 end
