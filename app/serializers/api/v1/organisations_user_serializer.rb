2a9aa9f8f9e4ed26e9a61121a77bb059e5a63fe4
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-03-16 15:16:14 +0530

    GCW-1835 Show Organisation on order detail

--- app/serializers/api/v1/organisations_user_serializer.rb
+++ app/serializers/api/v1/organisations_user_serializer.rb
@@ -3,5 +3,6 @@ module Api::V1
     embed :ids, include: true
     attributes :id, :user_id, :organisation_id, :position
     has_one :user, serializer: UserSerializer
+    # has_one :organisation, serializer: OrganisationSerializer
   end
 end
