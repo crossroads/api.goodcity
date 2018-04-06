58fe601aed5956bc1a3a48d22353757182d27a6f
namrataukirde <namrata.ukirde@kiprosh.com>
2018-04-06 16:43:15 +0530

    change ip for production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,2 @@
-ip = ENV["CI"] ? "api.goodcity.hk:62422" : "10.25.0.5:22"
-server ip, user: 'deployer', roles: %w{web app db}, primary: true
-# server 'goodcity-production.cloudapp.net:62423', user: 'deployer', roles: %w{web app}
+server "api.goodcity.hk:62422", user: 'deployer', roles: %w{web app db}, primary: true
 set :branch, :live
