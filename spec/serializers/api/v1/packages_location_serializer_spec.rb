26688935446f7b2f6058446c6537db6c8603ea16
namrataukirde <namrata.ukirde@kiprosh.com>
2018-05-07 16:17:43 +0530

    packages_location serializer specs

---
+++ spec/serializers/api/v1/packages_location_serializer_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Api::V1::PackagesLocationSerializer do
+  let(:packages_location) { build :packages_location }
+  let(:serializer) { Api::V1::PackagesLocationSerializer.new(packages_location) }
+  let(:json)       { JSON.parse( serializer.to_json ) }
+
+
+  it 'creates JSON' do
+    expect(json['packages_location']['quantity']).to eql(packages_location.quantity)
+    expect(json['packages_location']['package_id']).to eql(packages_location.package_id)
+    expect(json['packages_location']['item_id']).to eql(packages_location.package_id)
+    expect(json['packages_location']['location_id']).to eql(packages_location.location_id)
+  end
+end
