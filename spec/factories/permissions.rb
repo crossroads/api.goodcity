46873c11bd574a7cf0d9fac43e93dd93ddcdb567
namrataukirde <namrata.ukirde@kiprosh.com>
2018-05-23 21:26:34 +0530

    added permissions controller specs

--- spec/factories/permissions.rb
+++ spec/factories/permissions.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :permission do
     name            { %w( can_manange_orders can_manage_offers can_manage_packages ).sample }
     initialize_with { Permission.find_or_initialize_by(name: name) } # limits us to our sample of permissions
+
+    trait :api_write do
+      name 'api-write'
+    end
   end
 end
