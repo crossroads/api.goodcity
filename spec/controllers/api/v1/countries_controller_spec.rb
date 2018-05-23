9dee2593056480829ec625889b7d5de53485b9b1
AbulAsar <abul.a@kiprosh.com>
2018-05-23 10:55:50 +0530

    stockit_id cases added

--- spec/controllers/api/v1/countries_controller_spec.rb
+++ spec/controllers/api/v1/countries_controller_spec.rb
@@ -9,6 +9,22 @@ RSpec.describe Api::V1::CountriesController, type: :controller do
   describe "POST countries" do
     before { generate_and_set_token(user) }
     it 'create countries' do
+      expect {
+        post :create, country: country_params
+        }.to change(Country, :count).by(1)
+      expect(response.status).to eq(201)
+    end
+
+    it "when stockit_id is present" do
+      expect{
+        post :create, format: :json, country: country_params
+        }.to change(Country, :count).by(1)
+
+      expect(response.status).to eq(201)
+    end
+
+    it "when stockit_id is not present" do
+      country_params[:stockit_id] = nil
       post :create, format: :json, country: country_params
       expect(response.status).to eq(201)
     end
