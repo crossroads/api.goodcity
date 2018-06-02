7fe7c980e56ec57312160a1c239d9b4909d60e1b
AbulAsar <abul.a@kiprosh.com>
2018-06-02 16:06:10 +0800

    orders_package delete method added

--- app/models/orders_package.rb
+++ app/models/orders_package.rb
@@ -9,6 +9,7 @@ class OrdersPackage < ActiveRecord::Base
   after_create -> { recalculate_quantity("create") }
   after_update -> { recalculate_quantity("update") }
   before_destroy -> { destroy_stockit_record("destroy") }
+  after_destroy :delete_order_if_no_package
 
   scope :get_records_associated_with_order_id, ->(order_id) { where(order_id: order_id) }
   scope :get_designated_and_dispatched_packages, ->(package_id) { where("package_id = (?) and state IN (?)", package_id, ['designated', 'dispatched']) }
@@ -24,6 +25,10 @@ class OrdersPackage < ActiveRecord::Base
 
   def set_initial_state
     self.state ||= :requested
+  end
+
+  def delete_order_if_no_package
+    order.destroy unless order.orders_packages.exists?
   end
 
   state_machine :state, initial: :requested do
