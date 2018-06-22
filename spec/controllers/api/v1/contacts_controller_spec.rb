31f892899997a1d7db6b481ef7e9fc61742746b4
Steve Kenworthy <steveyken@gmail.com>
2018-06-22 17:31:06 +0800

    Fixed JSON tests for new Oj gem version.

--- spec/controllers/api/v1/contacts_controller_spec.rb
+++ spec/controllers/api/v1/contacts_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Api::V1::ContactsController, type: :controller do
 
   let(:user) { create(:user_with_token) }
   let(:contact_params) { FactoryGirl.attributes_for(:contact) }
+  let(:parsed_body) { JSON.parse(response.body )}
 
   describe "POST contact/1" do
     before { generate_and_set_token(user) }
@@ -12,6 +13,8 @@ RSpec.describe Api::V1::ContactsController, type: :controller do
         post :create, contact: contact_params
         }.to change(Contact, :count).by(1)
       expect(response.status).to eq(201)
+      expect(parsed_body['contact']['name']).to eql(contact_params[:name])
+      expect(parsed_body['contact']['mobile']).to eql(contact_params[:mobile])
     end
   end
 
