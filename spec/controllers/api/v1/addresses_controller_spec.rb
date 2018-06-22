09f653a909e7d63a44682ee0fc61652de5be4453
Steve Kenworthy <steveyken@gmail.com>
2018-06-22 16:45:55 +0800

    Fixed addresses controller spec with new Oj JSON requirements.

--- spec/controllers/api/v1/addresses_controller_spec.rb
+++ spec/controllers/api/v1/addresses_controller_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe Api::V1::AddressesController, type: :controller do
 
   let(:user) { create(:user_with_token) }
   let(:address) { create(:profile_address, addressable: user) }
-  let(:serialized_address) { Api::V1::AddressSerializer.new(address) }
+  let(:serialized_address) { Api::V1::AddressSerializer.new(address).as_json }
   let(:serialized_address_json) { JSON.parse( serialized_address.to_json ) }
   let(:address_params) { FactoryGirl.attributes_for(:profile_address).merge({ addressable: user }) }
+  let(:parsed_body) { JSON.parse(response.body) }
 
   describe "POST address/1" do
     before { generate_and_set_token(user) }
@@ -19,16 +20,15 @@ RSpec.describe Api::V1::AddressesController, type: :controller do
   end
 
   describe "GET address" do
-    before { generate_and_set_token(user) }
-    it "returns 200" do
+    before do
+      generate_and_set_token(user)
       get :show, id: address.id
+    end
+    it "returns 200" do
       expect(response.status).to eq(200)
     end
-
     it "return serialized address", :show_in_doc do
-      get :show, id: address.id
-      body = JSON.parse(response.body)
-      expect( body ).to eq(serialized_address_json)
+      expect(parsed_body).to eq(serialized_address_json)
     end
   end
 
