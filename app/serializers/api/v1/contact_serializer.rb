75c5252925e5f06b8c24823a7f5818fb91a449cb
AbulAsar <abul.a@kiprosh.com>
2018-07-25 15:28:52 +0530

    mobile phone number field added

--- app/serializers/api/v1/contact_serializer.rb
+++ app/serializers/api/v1/contact_serializer.rb
@@ -1,7 +1,15 @@
 module Api::V1
   class ContactSerializer < ApplicationSerializer
     embed :ids, include: true
-    attributes :id, :name, :mobile
+    attributes :id, :name, :mobile, :mobile_phone_number
     has_one :address, serializer: AddressSerializer
+
+    def mobile_phone_number
+      object.mobile
+    end
+
+    def mobile_phone_number__sql
+      'mobile'
+    end
   end
 end
