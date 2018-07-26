8c419a5cd7bcd26b5f968d6eece288a1c904351f
namrataukirde <namrata.ukirde@kiprosh.com>
2018-07-26 21:45:33 +0530

    change schedular time to test swd rake on staging

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, 'log/cron_log.log'
 set :environment, ENV['RAILS_ENV']
 
-every :thursday, at: '11 pm' do
+every :friday, at: '12:45 am' do
   rake 'goodcity:import_swd_organisations'
 end
