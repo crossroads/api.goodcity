7502bab1dbc499071e236aaeb78b87c9d224c03c
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-07-31 15:49:33 +0530

    GCW-2086 Address detail for GGV trasnport on Order detail page

---
+++ app/serializers/api/v1/territory_without_district_serializer.rb
@@ -0,0 +1,10 @@
+module Api::V1
+  class TerritoryWithoutDistrictSerializer < ApplicationSerializer
+    embed :ids, include: true
+    attributes :id, :name
+
+    def name__sql
+      "name_#{current_language}"
+    end
+  end
+end
