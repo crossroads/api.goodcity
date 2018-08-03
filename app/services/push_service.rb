ebfc702450f7856409ad03bb37f4582c40855dd7
Steve Kenworthy <steveyken@gmail.com>
2018-08-03 12:58:51 +0800

    Replace add_admin_app_suffix with add_app_name_suffix.

--- app/services/push_service.rb
+++ app/services/push_service.rb
@@ -1,13 +1,13 @@
 class PushService
   def send_update_store(channel, is_admin_app, data)
-    channel = Channel.add_admin_app_suffix(channel) if is_admin_app
+    channel = Channel.add_app_name_suffix(channel, ADMIN_APP) if is_admin_app
     SocketioSendJob.perform_later(channel, "update_store", data.to_json, true)
   end
 
   def send_notification(channel, is_admin_app, data)
     data[:message] = ActionView::Base.full_sanitizer.sanitize(data[:message])
     data[:date] = Time.now.to_json.tr('"','')
-    channel = Channel.add_admin_app_suffix(channel) if is_admin_app
+    channel = Channel.add_app_name_suffix(channel, ADMIN_APP) if is_admin_app
 
     SocketioSendJob.perform_later(channel, "notification", data.to_json)
     AzureNotifyJob.perform_later(channel, data, is_admin_app)
