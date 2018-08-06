e881de80d4994792545f24429a0ba996750ce6dd
AbulAsar <abul.a@kiprosh.com>
2018-08-06 11:16:18 +0530

    order_id field added in notification payload

--- app/services/azure_notifications_service.rb
+++ app/services/azure_notifications_service.rb
@@ -57,7 +57,7 @@ class AzureNotificationsService
   end
 
   def payload
-    '"category":"$(category)", "offer_id":"$(offer_id)", "item_id":"$(item_id)", "author_id":"$(author_id)", "is_private":"$(is_private)", "message_id": "$(message_id)"'
+    '"category":"$(category)", "offer_id":"$(offer_id)", "order_id":"$(order_id)", "item_id":"$(item_id)", "author_id":"$(author_id)", "is_private":"$(is_private)", "message_id": "$(message_id)"'
   end
 
   def update_data(data)
