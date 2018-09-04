11118ff249501e5cb81c7a93c3e6942c84515d08
Steve Kenworthy <steveyken@gmail.com>
2018-09-04 11:57:15 +0800

    Enhancements to user builder.

--- app/models/organisations_user.rb
+++ app/models/organisations_user.rb
@@ -1,35 +1,9 @@
 class OrganisationsUser < ActiveRecord::Base
   belongs_to :organisation
   belongs_to :user
+  # position
 
-  accepts_nested_attributes_for :user, allow_destroy: true, limit: 1
+  validates :organisation_id, :user_id, presence: true
 
-  after_create :send_welcome_msg, :create_user_role
-
-  def create_or_update_existing_organisation_user
-    existing_user = User.find_by_mobile(user.mobile)
-    if existing_user && already_exist_in_same_organisation?(existing_user)
-      existing_user.first_name = user.first_name
-      existing_user.last_name = user.last_name
-      existing_user.email = user.email
-      existing_user.save
-      self.user_id = existing_user.id
-    end
-    self
-  end
-
-  def already_exist_in_same_organisation?(existing_user)
-    !existing_user.organisations.include?(organisation)
-  end
-
-  private
-
-  def send_welcome_msg
-    TwilioService.new(user).send_welcome_msg
-  end
-
-  def create_user_role
-    UserRole.create_user_role(user.id, Role.charity.id)
-  end
 end
 
