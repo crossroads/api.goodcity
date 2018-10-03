313960aae955604296b24551a3496483b1e0454c
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2018-10-03 11:51:32 +0800

    GCW-2139 Remove unneeded test (managed by rake task)

--- spec/models/identity_type_spec.rb
+++ spec/models/identity_type_spec.rb
@@ -6,15 +6,4 @@ RSpec.describe IdentityType, type: :model do
     it { is_expected.to have_db_column(:name).of_type(:string) }
   end
 
-  describe 'Values' do
-    it "should be initialized with values" do
-      expectedTypes = [
-        "HKID",
-        "ASRF"
-      ]
-      expect(IdentityType.count).to eq(expectedTypes.count)
-      expectedTypes.each { |type| expect(IdentityType.find_by(name: type)).not_to be_nil }
-    end
-  end
-
 end
