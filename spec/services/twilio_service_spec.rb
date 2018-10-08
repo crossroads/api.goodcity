523e83b824b865559d9a31c63ac1833ff0bf2e9b
namrataukirde <namrata.ukirde@kiprosh.com>
2018-10-08 18:45:59 +0530

    fix failing specs

--- spec/services/twilio_service_spec.rb
+++ spec/services/twilio_service_spec.rb
@@ -22,7 +22,7 @@ describe TwilioService do
       allow(user).to receive_message_chain(:most_recent_token, :otp_code).and_return(otp_code)
       body = "Single-use pin is #{otp_code}. GoodCity.HK welcomes you! Enjoy donating\nyour quality goods. (If you didn't request this message, please ignore)\n"
       expect(TwilioJob).to receive(:perform_later).with(to: user.mobile, body: body)
-      twilio.sms_verification_pin
+      twilio.sms_verification_pin(DONOR_APP)
     end
   end
 
