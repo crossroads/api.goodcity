06f4b36b71cc76ed71536185f359cf14908a81cb
AbulAsar <abul.a@kiprosh.com>
2018-10-26 20:42:48 +0530

    fix stack overflow error for cancelling order from browse

--- app/models/orders_package.rb
+++ app/models/orders_package.rb
@@ -9,7 +9,7 @@ class OrdersPackage < ActiveRecord::Base
   after_create -> { recalculate_quantity("create") }
   after_update -> { recalculate_quantity("update") }
   before_destroy -> { destroy_stockit_record("destroy") }
-  after_destroy -> { order.delete_if_no_orders_packages }
+  #after_destroy -> { order.delete_if_no_orders_packages }
 
   scope :get_records_associated_with_order_id, ->(order_id) { where(order_id: order_id) }
   scope :get_designated_and_dispatched_packages, ->(package_id) { where("package_id = (?) and state IN (?)", package_id, ['designated', 'dispatched']) }
@@ -97,6 +97,10 @@ class OrdersPackage < ActiveRecord::Base
     else
       update(quantity: total_quantity)
     end
+  end
+
+  def delete_orders_package
+    self.destroy and order.delete_if_no_orders_packages
   end
 
   def update_quantity_based_on_dispatch_state(total_quantity)
