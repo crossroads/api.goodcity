882e5a7fe341aabae509cc609a5f6350bd59e147
namrataukirde <namrata.ukirde@kiprosh.com>
2018-11-19 20:01:08 +0530

    fix slack sms text

--- app/services/slack_pin_service.rb
+++ app/services/slack_pin_service.rb
@@ -6,11 +6,21 @@ class SlackPinService
     @user = user
   end
 
-  def send_otp
+  def send_otp(app_name)
     channel = ENV['SLACK_PIN_CHANNEL']
     token = @user.most_recent_token
-    message = "[#{Rails.env}] " + I18n.t('twilio.sms_verification_pin', pin: token.otp_code)
+    
+    message = "[#{Rails.env}] " + message_text(app_name, token)
     SlackMessageJob.perform_later(message, channel)
   end
 
+  private
+
+  def message_text(app_name, token)
+    if app_name == BROWSE_APP
+      I18n.t('twilio.browse_sms_verification_pin', pin: token.otp_code)
+    else
+      I18n.t('twilio.sms_verification_pin', pin: token.otp_code)
+    end
+  end
 end
