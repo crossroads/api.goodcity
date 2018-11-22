90d51786ac6b6e97b49b652aa879f49300a767ac
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-11-22 14:37:42 +0530

    GCW-2212 Singleton designation fix

--- app/services/stockit/item_sync.rb
+++ app/services/stockit/item_sync.rb
@@ -109,8 +109,8 @@ module Stockit
 
     def item_params
       {
-        # quantity: package.singleton_package? ? package.received_quantity : package.quantity,
-        quantity: package.quantity,
+        quantity: package.singleton_package? ? package.received_quantity : package.quantity,
+        # quantity: package.quantity,
         code_id: package.package_type.try(:stockit_id),
         inventory_number: add_stockit_prefix(package.inventory_number),
         case_number: package.case_number.blank? ? nil : package.case_number,
