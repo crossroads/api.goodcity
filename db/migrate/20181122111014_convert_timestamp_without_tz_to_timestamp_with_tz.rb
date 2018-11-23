c4a98b9754bf97151e5131b273d6595d8f112ea7
Steve Kenworthy <steveyken@gmail.com>
2018-11-23 13:21:11 +0800

    Constantize model names

--- db/migrate/20181122111014_convert_timestamp_without_tz_to_timestamp_with_tz.rb
+++ db/migrate/20181122111014_convert_timestamp_without_tz_to_timestamp_with_tz.rb
@@ -1,7 +1,6 @@
 class ConvertTimestampWithoutTzToTimestampWithTz < ActiveRecord::Migration
-  def up
 
-    model_names = ['address', 'auth_token', 'beneficiary', 'booking_type', 'box',
+  MODEL_NAMES = ['address', 'auth_token', 'beneficiary', 'booking_type', 'box',
     'braintree_transaction', 'cancellation_reason', 'contact', 'country',
     'crossroads_transport', 'delivery', 'district', 'donor_condition', 'gogovan_order',
     'gogovan_transport', 'goodcity_request', 'holiday', 'identity_type', 'image',
@@ -13,7 +12,9 @@ class ConvertTimestampWithoutTzToTimestampWithTz < ActiveRecord::Migration
     'stockit_contact', 'stockit_local_order', 'stockit_organisation', 'subpackage_type',
     'subscription', 'territory', 'timeslot', 'user', 'user_role', 'version'].freeze
 
-    model_names.each do |model_name|
+  def up
+
+    MODEL_NAMES.each do |model_name|
       model = model_name.camelize.constantize
       model.columns.each do |column|
         if column.sql_type == "timestamp without time zone"
@@ -26,19 +27,7 @@ class ConvertTimestampWithoutTzToTimestampWithTz < ActiveRecord::Migration
 
   def down
 
-    model_names = ['address', 'auth_token', 'beneficiary', 'booking_type', 'box',
-    'braintree_transaction', 'cancellation_reason', 'contact', 'country',
-    'crossroads_transport', 'delivery', 'district', 'donor_condition', 'gogovan_order',
-    'gogovan_transport', 'goodcity_request', 'holiday', 'identity_type', 'image',
-    'inventory_number', 'item', 'location', 'message', 'offer', 'order', 'orders_package',
-    'orders_purpose', 'order_transport', 'organisation', 'organisations_user',
-    'organisation_type', 'package_categories_package_type', 'package_category',
-    'package', 'packages_location', 'package_type', 'pallet', 'permission', 'purpose',
-    'rejection_reason', 'role_permission', 'role', 'schedule', 'stockit_activity',
-    'stockit_contact', 'stockit_local_order', 'stockit_organisation', 'subpackage_type',
-    'subscription', 'territory', 'timeslot', 'user', 'user_role', 'version'].freeze
-
-    model_names.each do |model_name|
+    MODEL_NAMES.each do |model_name|
       model = model_name.camelize.constantize
       model_name.camelize.constantize.columns.each do |column|
         if column.sql_type == "timestamp with time zone"
