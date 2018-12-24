c0e029fa29a74978bfa674bf1e254db4345a5e89
namrataukirde <namrata.ukirde@kiprosh.com>
2018-12-24 18:50:03 +0530

    add identifier to purpose

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181224103318) do
+ActiveRecord::Schema.define(version: 20181224131147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -593,6 +593,7 @@ ActiveRecord::Schema.define(version: 20181224103318) do
     t.string   "name_zh_tw"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "identifier"
   end
 
   create_table "rejection_reasons", force: :cascade do |t|
--- spec/factories/purpose.rb
+++ spec/factories/purpose.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :purpose do
     name_en { FFaker::Lorem.word }
     name_zh_tw { FFaker::Lorem.word }
+    identifier { FFaker::Lorem.word }
   end
 end
