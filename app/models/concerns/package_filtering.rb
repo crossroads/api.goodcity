b8055d6d7cfa7d6338baa8133c7c366ba49b29e3
Abhinav Garg <abhinav.g@kiprosh.com>
2019-01-02 17:33:44 +0530

    Package filtering

--- app/models/concerns/package_filtering.rb
+++ app/models/concerns/package_filtering.rb
@@ -3,10 +3,41 @@ module PackageFiltering
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def filter(states: [], location: nil)
-      res = where("state IN (?)", states) unless states.empty?
-      res = res.filter_by_location(location) if location
+    # Filter based on states, location, publish status and images
+
+    def filter(states: [], location: nil, published: nil, images: nil)
+      res = where(nil)
+      res = res.where_states(states) unless states.empty?
+      res = res.filter_by_location(location) unless location.blank?
       res
+    end
+
+    def where_states(states)
+      states = states.select { |t| respond_to?("#{t}_sql") }
+      return none if states.empty?
+
+      queries = states.map do |t|
+        method = "#{t}_sql"
+        "(#{send(method)})"
+      end
+      states_sql = queries.compact.join(" OR ")
+      join_order_packages.where(states_sql)
+    end
+
+    def join_order_packages
+      joins("LEFT OUTER JOIN orders_packages ON orders_packages.package_id = packages.id")
+    end
+
+    def in_stock_sql
+      "packages.state = 'received' and packages.quantity > 0"
+    end
+
+    def designated_sql
+      "orders_packages.state = 'designated'"
+    end
+
+    def dispatched_sql
+      "orders_packages.state = 'dispatched'"
     end
 
     def filter_by_location(location)
