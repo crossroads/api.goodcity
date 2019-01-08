73ba4bbfe72f97ecdf0cae53ebc9a9facdb52a4a
Abhinav Garg <abhinav.g@kiprosh.com>
2019-01-08 17:28:54 +0530

    location filter spec

--- spec/models/concerns/package_filtering_spec.rb
+++ spec/models/concerns/package_filtering_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 describe Package do
   # testing dispatched packages
   context 'in_stock packages' do
-    before do
+    before(:each) do
       create :package, inventory_number: "111000", state: 'received', quantity: 1
       create :package, inventory_number: "111001", state: 'received', allow_web_publish: true, quantity: 1
       create :package, inventory_number: "111002", state: 'received', quantity: 0
@@ -57,7 +57,7 @@ describe Package do
 
   # testing designated packages
   context 'designated packages' do
-    before do
+    before(:each) do
       @order_package = create(:orders_package, :with_state_designated, quantity: 1)
       create(:package, :with_images, inventory_number: "111003", state: 'received', quantity: 1)
       create(:package, :with_images, inventory_number: "111005", allow_web_publish: true, state: 'received', quantity: 1)
@@ -77,7 +77,7 @@ describe Package do
 
   # testing dispatched packages
   context 'dispatched packages' do
-    before do
+    before(:each) do
       @order_package = create(:orders_package, :with_state_dispatched, quantity: 1)
       create(:package, :with_images, inventory_number: "111003", state: 'received', quantity: 1)
       create(:package, :with_images, inventory_number: "111005", allow_web_publish: true, state: 'received', quantity: 1)
@@ -93,5 +93,27 @@ describe Package do
       package_inventory = @order_package.package.update(inventory_number: '111007')
       expect(Package.where("inventory_number ILIKE '%111%'").filter(states: ['dispatched']).count).to eq(1)
     end
+  end
+
+  context 'filter based on location' do
+    before(:each) do
+      @package_with_location = create(:package, :package_with_locations, inventory_number: "111003", state: 'received', quantity: 1)
+      create(:package, :with_images, inventory_number: "111005", allow_web_publish: true, state: 'received', quantity: 1)
+      create(:package, :with_images, inventory_number: "111006", allow_web_publish: true, state: 'received', quantity: 1)
+    end
+
+    it 'Should not filter out anything if no explicit arguments are provided/selected' do
+      expect(Package.count).to eq(3)
+      expect(Package.where("inventory_number ILIKE '%111%'").count).to eq(3)
+      expect(Package.where("inventory_number ILIKE '%111%'").filter().count).to eq(3)
+    end
+
+    # it 'should filter out item based on location' do
+    #   expect(Package.count).to eq(3)
+    #   pkg_location_name = @package_with_location.locations.pluck(:building, :area).first.join('-')
+    #   expect(Package.where("inventory_number ILIKE '%111%'")
+    #     .filter(states: ['in_stock'], location: pkg_location_name).count)
+    #     .to eq(1)
+    # end
   end
 end
