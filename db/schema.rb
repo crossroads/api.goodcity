4f4ad6699ff52ac768609e1b6602157accd3dabb
Abhinav Garg <abhinav.g@kiprosh.com>
2019-01-18 12:47:38 +0530

    add order_id to messages and subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -288,10 +288,12 @@ ActiveRecord::Schema.define(version: 20190218025723) do
     t.datetime "deleted_at"
     t.integer  "offer_id"
     t.integer  "item_id"
+    t.integer  "order_id"
   end
 
   add_index "messages", ["item_id"], name: "index_messages_on_item_id", using: :btree
   add_index "messages", ["offer_id"], name: "index_messages_on_offer_id", using: :btree
+  add_index "messages", ["order_id"], name: "index_messages_on_order_id", using: :btree
   add_index "messages", ["sender_id"], name: "index_messages_on_sender_id", using: :btree
 
   create_table "offers", force: :cascade do |t|
@@ -390,8 +392,8 @@ ActiveRecord::Schema.define(version: 20190218025723) do
     t.integer  "address_id"
     t.integer  "district_id"
     t.text     "cancellation_reason"
-    t.integer  "authorised_by_id"
     t.integer  "booking_type_id"
+    t.integer  "authorised_by_id"
     t.string   "staff_note",              default: ""
   end
 
@@ -548,6 +550,7 @@ ActiveRecord::Schema.define(version: 20190218025723) do
     t.string   "case_number"
     t.boolean  "allow_web_publish"
     t.integer  "received_quantity"
+    t.boolean  "last_allow_web_published"
   end
 
   add_index "packages", ["allow_web_publish"], name: "index_packages_on_allow_web_publish", using: :btree
@@ -702,11 +705,13 @@ ActiveRecord::Schema.define(version: 20190218025723) do
     t.integer "user_id"
     t.integer "message_id"
     t.string  "state"
+    t.integer "order_id"
   end
 
   add_index "subscriptions", ["message_id"], name: "index_subscriptions_on_message_id", using: :btree
   add_index "subscriptions", ["offer_id", "user_id", "message_id"], name: "offer_user_message", unique: true, using: :btree
   add_index "subscriptions", ["offer_id"], name: "index_subscriptions_on_offer_id", using: :btree
+  add_index "subscriptions", ["order_id"], name: "index_subscriptions_on_order_id", using: :btree
   add_index "subscriptions", ["state"], name: "index_subscriptions_on_state", using: :btree
   add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
 
@@ -778,9 +783,11 @@ ActiveRecord::Schema.define(version: 20190218025723) do
   add_foreign_key "beneficiaries", "identity_types"
   add_foreign_key "goodcity_requests", "orders"
   add_foreign_key "goodcity_requests", "package_types"
+  add_foreign_key "messages", "orders"
   add_foreign_key "organisations", "countries"
   add_foreign_key "organisations", "districts"
   add_foreign_key "organisations", "organisation_types"
   add_foreign_key "organisations_users", "organisations"
   add_foreign_key "organisations_users", "users"
+  add_foreign_key "subscriptions", "orders"
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-
+   describe "Associations" do
+    it { is_expected.to belong_to :order }
+  end
 end
