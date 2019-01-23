e5f35e8ffb1f00adc70d818918094e11eb147120
Steve Kenworthy <steveyken@gmail.com>
2019-01-23 17:30:26 +0800

    Moved message subscription to it's own module and refactored the logic.

---
+++ app/models/concerns/message_subscription.rb
@@ -0,0 +1,53 @@
+#
+# Subscription logic for messages is extracted here to avoid cluttering the model class
+module MessageSubscription
+  extend ActiveSupport::Concern
+
+  #
+  # Who gets notified of a new message
+  # If you are subscribed here, you get a new 'unread' message in your inbox
+  #   (or a 'read' message if you are the sender).
+  def subscribe_users_to_message
+    obj = offer || order
+    klass = obj.class.name.underscore
+    user_ids = []
+
+    # Add the following users
+    #   - Donor / Charity user - if offer/order is not cancelled
+    #   - Message sender
+    #   - Anyone who has previously replied to offer/order
+    #   - Admin users processing the offer/order
+    user_ids << obj.try(:created_by_id) unless obj.try('cancelled?')
+    user_ids << sender_id
+    user_ids << Subscription.where(:"#{klass}_id" => obj.id).pluck(:user_id)
+    admin_user_fields.each{|field| user_ids << obj.try(:field)}
+
+    # Remove the following users
+    #   - SystemUser and StockitUser
+    #   - donor/charity user if the message is private (supervisor channel)
+    user_ids -= [User.system_user.try(:id), User.stockit_user.try(:id)]
+    user_ids -= obj.try(:created_by_id) if is_private
+
+    user_ids.uniq.compact.each do |user_id|
+      state = (user_id == self.sender_id) ? "read" : "unread" # mark as read for sender
+      add_subscription(state, user_id)
+    end
+  end
+
+  private
+
+  def admin_user_fields
+    [:reviewed_by_id, :processed_by_id, :process_completed_by_id,  :cancelled_by_id, :process_completed_by,
+      :dispatch_started_by, :closed_by, :submitted_by]
+  end
+
+  def add_subscription(state, user_id)
+    subscriptions.create(
+      state: state,
+      message_id: self.id,
+      offer_id: self.offer_id,
+      order_id: self.order_id,
+      user_id: user_id)
+  end
+
+end
