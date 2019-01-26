31bb10dcb294c69272a0cbbd510e4ddd8d43afb0
Steve Kenworthy <steveyken@gmail.com>
2019-01-26 14:16:25 +0800

    Initial refactoring work

--- app/services/subscriptions_reminder.rb
+++ app/services/subscriptions_reminder.rb
@@ -1,26 +1,46 @@
 class SubscriptionsReminder
 
   def generate
-    sms_url = "#{Rails.application.secrets.base_urls['app']}/offers"
-    donor_unread_subscriptions(donor_users).group_by(&:user_id).each do |user_id, subscriptions|
-      begin
-        Subscription.where(id: subscriptions.map(&:id)).update_all(sms_reminder_sent_at: Time.zone.now)
-        TwilioService.new(User.find(user_id)).send_unread_message_reminder(sms_url)
-        Rails.logger.info("\n Message sent to user: #{user_id}")
-      rescue Exception => e
-        Rails.logger.error("\nError: #{e.message}")
-      end
+    users_to_remind.each do |user_id, subscriptions|
+      user = User.find(user_id)
+      user.update(sms_reminder_sent_at: Time.zone.now)
+      send_sms_reminder(user)
     end
   end
 
   private
 
-  def donor_users
-    Offer.distinct.pluck(:created_by_id)
+  # Get list of subscriptions distinct by user where user is donor and hasn't received reminder since
+
+  def users_to_remind
+    donor_ids = Offer.distinct.pluck(:created_by_id).compact
+
+    User.joins(subscriptions: [:messages]).where(
+     'id IN (?) AND
+     subscriptions.state = (?) AND
+     MIN(messages.created_at) > users.sms_reminder_sent_at AND
+     MIN(messages.created_at) > (?)',
+     donor_ids, 'unread', delta
+    ).distinct('id')
+
+    # Subscription.
+    #   joins(:message, :user).
+    #   where('users.id IN (?) AND 
+    #     subscriptions.state = (?) AND
+    #     MIN(messages.created_at) > users.sms_reminder_sent_at AND
+    #     MIN(messages.created_at) > (?)',
+    #     donor_ids, 'unread', delta)
   end
 
-  def donor_unread_subscriptions(users)
-    Subscription.where(state: 'unread', user_id: users).
-      where("sms_reminder_sent_at IS NULL OR sms_reminder_sent_at < ?", SUBSCRIPTION_REMINDER_TIME_DELTA.ago)
+  def send_sms_reminder(user)
+    sms_url = "#{Rails.application.secrets.base_urls['app']}/offers"
+    TwilioService.new(user).send_unread_message_reminder(sms_url)
+    Rails.logger.info("SMS reminder sent to user #{user.id}")
   end
+
+  # E.g. 4.hours.ago
+  def delta
+    SUBSCRIPTION_REMINDER_TIME_DELTA.ago
+  end
+
 end
--- spec/services/subscriptions_reminder_spec.rb
+++ spec/services/subscriptions_reminder_spec.rb
@@ -1,31 +1,70 @@
 require 'rails_helper'
 
 describe SubscriptionsReminder do
-  let(:offer) { create :offer }
-  let(:user) { offer.created_by }
-  let!(:subscription) { create(:subscription, user: user, state:'unread' ) }
-  let!(:subscription1) { create(:subscription, user: user, offer: offer, state:'unread', sms_reminder_sent_at: ((SUBSCRIPTION_REMINDER_TIME_DELTA+1).ago) ) }
-  let!(:subscription2) { create(:subscription, user: user, offer: offer, state:'unread', sms_reminder_sent_at: ((SUBSCRIPTION_REMINDER_TIME_DELTA-2).ago) ) }
-  let(:subscription_reminder) { SubscriptionsReminder.new }
-  let(:time) { Time.now }
+  let(:user)          { create :user }
+  let(:delta)         { SUBSCRIPTION_REMINDER_TIME_DELTA }
+  let(:delta_plus_2)  { delta + 2.hours }
+  let(:delta_minus_2) { delta - 2.hours }
+  let(:time)          { Time.now }
 
-  before(:each) do
-    allow(Time).to receive(:now).and_return(time)
-  end
+  before(:each){ allow(Time).to receive(:now).and_return(time) }
+
+  subject { SubscriptionsReminder.new }
 
   context "generate" do
-    it "sends sms of unread messages if sms_reminder_sent_at is nil" do
-      subscription_reminder.generate
-      expect(subscription.reload.sms_reminder_sent_at.to_s(:db)).to eq(time.to_s(:db))
-    end
-
-    it "sends sms of unread messages if sms_reminder_sent_at gap time is more than 4 hours" do
-      subscription_reminder.generate
-      expect(subscription1.reload.sms_reminder_sent_at.to_s(:db)).to eq(time.to_s(:db))
-    end
-
-    it "do not sends sms of unread messages if sms_reminder_sent_at gap time is less than 4 hours" do
-      expect{ subscription_reminder.generate}.to_not change(Subscription.find(subscription2.id), :sms_reminder_sent_at)
+    context "sends reminder when" do
+      it "new unread message created in last X hours" do
+        subscription = create(:subscription, user: user, state: 'unread')
+        subject.generate
+        expect(subscription.reload.sms_reminder_sent_at.to_s(:db)).to eq(time.to_s(:db))
+        # TwilioService.new(User.find(user_id)).send_unread_message_reminder(sms_url)
+        expect(TwilioService).to receive(:new)#.with(user).and_return(TwilioService.new(user))
+      end
+      it "2 new unread message created in last 4 hours only sends one reminder" do
+      end
     end
   end
+
+  context "doesn't send reminder when" do
+    it "other reminders have been sent to same user within 4 hours"
+    it "new message created in last 4 hours has already been read"
+    it "reminder has already been sent for same message"
+    it "it was written by the user themselves"
+  end
+
 end
+
+
+# Send a message if
+#   recipient is donor
+#   a NEW message has been created in last 4 hours
+#   AND it is still unread
+#   AND no other reminders have been sent in last 4 hours
+
+# Don't send message if
+#   no new message has been created
+#   OR new message has been created but it's not been at least 4 hours since the last reminder
+#   OR the new message has been read already
+#   OR it was written by the user
+
+# Only ever alert for a message once
+#   set sms_reminder_sent_at time on all messages when the reminder is sent
+
+# If there is more than one new message, only send one SMS reminder.
+
+
+#   context "generate" do
+#     it "sends sms of unread messages if sms_reminder_sent_at is nil" do
+#       subscription_reminder.generate
+#       expect(subscription.reload.sms_reminder_sent_at.to_s(:db)).to eq(time.to_s(:db))
+#     end
+
+#     it "sends sms of unread messages if sms_reminder_sent_at gap time is more than 4 hours" do
+#       subscription_reminder.generate
+#       expect(subscription1.reload.sms_reminder_sent_at.to_s(:db)).to eq(time.to_s(:db))
+#     end
+
+#     it "do not sends sms of unread messages if sms_reminder_sent_at gap time is less than 4 hours" do
+#       expect{ subscription_reminder.generate}.to_not change(Subscription.find(subscription2.id), :sms_reminder_sent_at)
+#     end
+#   end
