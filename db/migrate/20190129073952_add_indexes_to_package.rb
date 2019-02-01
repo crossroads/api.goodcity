8033a0c9b8069ec4b6ef413cd4c209ffdfdf6b6e
Steve Kenworthy <steveyken@gmail.com>
2019-02-01 12:11:21 +0800

    Tests and extra indexes

--- db/migrate/20190129073952_add_indexes_to_package.rb
+++ db/migrate/20190129073952_add_indexes_to_package.rb
@@ -7,9 +7,13 @@ class AddIndexesToPackage < ActiveRecord::Migration
     execute "CREATE INDEX index_packages_on_notes ON packages USING gin (notes gin_trgm_ops)"
     execute "CREATE INDEX index_packages_on_case_number ON packages USING gin (case_number gin_trgm_ops)"
     execute "CREATE INDEX index_packages_on_state ON packages USING gin (state gin_trgm_ops)"
+    add_index :packages, :quantity, name: 'partial_index_quantity_greater_than_zero', where: "quantity > 0"
+    add_index :packages, :allow_web_publish
   end
 
   def down
+    remove_index :packages, :allow_web_publish
+    remove_index :packages, name: "partial_index_quantity_greater_than_zero"
     execute "DROP INDEX index_packages_on_designation_name"
     execute "DROP INDEX index_packages_on_notes"
     execute "DROP INDEX index_packages_on_case_number"
