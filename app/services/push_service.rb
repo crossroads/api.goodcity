868e249bd2344523814c6429b7e46d6dc1714da8
Abhinav Garg <abhinav.g@kiprosh.com>
2019-02-13 15:17:14 +0530

    remove unwanted condition as it is handled in channel.rb

--- app/services/push_service.rb
+++ app/services/push_service.rb
@@ -1,17 +1,13 @@
 class PushService
   def send_update_store(channel, app_name, data)
-    if [ADMIN_APP, BROWSE_APP, STOCK_APP].include?(app_name)
-      channel = Channel.add_app_name_suffix(channel, app_name)
-    end
+    channel = Channel.add_app_name_suffix(channel, app_name)
     SocketioSendJob.perform_later(channel, "update_store", data.to_json, true)
   end
 
   def send_notification(channel, app_name, data)
     data[:message] = ActionView::Base.full_sanitizer.sanitize(data[:message])
     data[:date] = Time.now.to_json.tr('"','')
-    if [ADMIN_APP, BROWSE_APP, STOCK_APP].include?(app_name)
-      channel = Channel.add_app_name_suffix(channel, app_name)
-    end
+    channel = Channel.add_app_name_suffix(channel, app_name)
     SocketioSendJob.perform_later(channel, "notification", data.to_json)
     AzureNotifyJob.perform_later(channel, data, app_name)
   end
