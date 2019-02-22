e093f2b9015d678c6e00b5b05536775de658e178
Steve Kenworthy <steveyken@gmail.com>
2019-02-22 17:03:49 +0800

    Wrote specs for push_service.

--- spec/services/push_service_spec.rb
+++ spec/services/push_service_spec.rb
@@ -3,34 +3,57 @@ require "rails_helper"
 describe PushService do
 
   let(:service) { PushService.new }
-  let(:channel) { ['user_1'] }
+  let(:channels) { ['user_1'] }
   let(:admin_channel) { 'user_1_admin' }
   let(:data)    { {message: message} }
   let(:message) { "New message" }
   let(:time)     { Time.now }
   
+  
   before { allow(Time).to receive(:now).and_return(time) }
 
   context "send_update_store" do
-    pending "should send updates"
+    context "should send updates" do
+      it "with single channel string" do
+        expect(SocketioSendJob).to receive(:perform_later).with(['user_1'], 'update_store', data.to_json, true)
+        service.send_update_store('user_1', data)
+      end
+      it "with single channel array" do
+        expect(SocketioSendJob).to receive(:perform_later).with(['user_1'], 'update_store', data.to_json, true)
+        service.send_update_store(['user_1'], data)
+      end
+      it "with mixed channel array" do
+        expect(SocketioSendJob).to receive(:perform_later).with(['user_1', 'user_2', 'user_3', 'user_4'], 'update_store', data.to_json, true)
+        service.send_update_store(['user_1', ['user_2', 'user_3'], 'user_4'], data)
+      end
+    end
+    context "should not send updates" do
+      it "if channels are nil" do
+        expect(SocketioSendJob).to_not receive(:perform_later)
+        service.send_update_store(nil, data)
+      end
+      it "if channels are []" do
+        expect(SocketioSendJob).to_not receive(:perform_later)
+        service.send_update_store([], data)
+      end
+    end
   end
 
   context "send_notification" do
+    let(:payload) { data.merge(date: time).to_json }
     
     context "donor app" do
       let(:app_name) { DONOR_APP }
       it do
-        payload = data.merge(date: time).to_json
-        expect(SocketioSendJob).to receive(:perform_later).with(channel, "notification", payload)
-        expect(AzureNotifyJob).to receive(:perform_later).with(channel, data, app_name)
-        service.send_notification(channel, app_name, data)
+        expect(SocketioSendJob).to receive(:perform_later).with(channels, "notification", payload)
+        expect(AzureNotifyJob).to receive(:perform_later).with(channels, data, app_name)
+        service.send_notification(channels, app_name, data)
       end
     end
 
     context "admin app" do
       let(:app_name) { ADMIN_APP }
       it do
-        payload = data.merge(date: time).to_json
         expect(SocketioSendJob).to receive(:perform_later).with([admin_channel], "notification", payload)
         expect(AzureNotifyJob).to receive(:perform_later).with([admin_channel], data, app_name)
         service.send_notification(admin_channel, app_name, data)
