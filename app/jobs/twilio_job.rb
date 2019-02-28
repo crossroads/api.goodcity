ddb6c822af4b635f5049d6c947fee3d747ad6c47
namrataukirde <namrata.ukirde@kiprosh.com>
2019-02-28 14:04:26 +0530

    send sms only if to mobile number is present

--- app/jobs/twilio_job.rb
+++ app/jobs/twilio_job.rb
@@ -3,8 +3,10 @@ class TwilioJob < ActiveJob::Base
 
   # e.g. options = { to: @user.mobile, body: body }
   def perform(options)
-    twilio_conf = Rails.application.secrets.twilio
-    client = Twilio::REST::Client.new(twilio_conf['account_sid'], twilio_conf['auth_token'])
-    client.account.messages.create({ from: twilio_conf['phone_number'] }.merge(options))
+    if options[:to].present?
+      twilio_conf = Rails.application.secrets.twilio
+      client = Twilio::REST::Client.new(twilio_conf['account_sid'], twilio_conf['auth_token'])
+      client.account.messages.create({ from: twilio_conf['phone_number'] }.merge(options))
+    end
   end
 end
