55dff633c1afef6a9ddfcd56f767c0f75a6c4eb7
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-03-07 10:23:55 +0800

    offers summary -> use shallow=true instead

--- spec/controllers/api/v1/offers_controller_spec.rb
+++ spec/controllers/api/v1/offers_controller_spec.rb
@@ -128,37 +128,28 @@ RSpec.describe Api::V1::OffersController, type: :controller do
     end
   end
 
-  describe "GET offers/summary" do
+  describe "GET offers without associations (shallow=true)" do
     context "as an admin" do
       before { generate_and_set_token(supervisor) }
 
       it "should return a 200" do
-        get :summary
+        get :index, shallow: 'true'
         expect(response.status).to eq(200)
       end
 
       it "should return the orders with the users associations" do
         2.times{ create :offer }
-        get :summary
+        get :index, shallow: 'true'
         body = JSON.parse(response.body)
         expect( body['offers'].length ).to eq(2)
         expect( body['user'] ).not_to be_nil
       end
 
       it "should not return items or messages (to avoid a large payload)" do
-        get :summary
+        get :index, shallow: 'true'
         body = JSON.parse(response.body)
         expect(body).not_to include('items')
         expect(body).not_to include('messages')
-      end
-    end
-
-    context "as a user" do
-      before { generate_and_set_token(user) }
-
-      it "should return a 403" do
-        get :summary
-        expect(response.status).to eq(403)
       end
     end
   end
