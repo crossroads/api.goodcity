df725b4db27e7c899bbfc683f79f259c5a036b11
Patrick R <patrick.rabier@gmail.com>
2019-03-08 12:26:08 +0800

    Offers summary serializer (#668)
    Offer summary serializer

--- spec/controllers/api/v1/offers_controller_spec.rb
+++ spec/controllers/api/v1/offers_controller_spec.rb
@@ -128,6 +128,32 @@ RSpec.describe Api::V1::OffersController, type: :controller do
     end
   end
 
+  describe "GET offers without associations (summarize=true)" do
+    context "as an admin" do
+      before { generate_and_set_token(supervisor) }
+
+      it "should return a 200" do
+        get :index, summarize: 'true'
+        expect(response.status).to eq(200)
+      end
+
+      it "should return the orders with the users associations" do
+        2.times{ create :offer }
+        get :index, summarize: 'true'
+        body = JSON.parse(response.body)
+        expect( body['offers'].length ).to eq(2)
+        expect( body['user'] ).not_to be_nil
+      end
+
+      it "should not return items or messages (to avoid a large payload)" do
+        get :index, summarize: 'true'
+        body = JSON.parse(response.body)
+        expect(body).not_to include('items')
+        expect(body).not_to include('messages')
+      end
+    end
+  end
+
   describe "GET offer/1" do
     before { generate_and_set_token(user) }
     it "returns 200" do
