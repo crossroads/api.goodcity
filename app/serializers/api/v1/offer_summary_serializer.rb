4f00037476e4e7282d613fd400da1ff6f17f521d
Steve Kenworthy <steveyken@gmail.com>
2019-03-09 17:35:50 +0800

    Cut back on number of attributes in offer summary serializer.

--- app/serializers/api/v1/offer_summary_serializer.rb
+++ app/serializers/api/v1/offer_summary_serializer.rb
@@ -2,12 +2,9 @@ module Api::V1
   class OfferSummarySerializer < ApplicationSerializer
     embed :ids, include: true
 
-    attributes :id, :language, :state, :origin, :stairs, :parking, :saleable,
-      :estimated_size, :notes, :created_by_id, :created_at, :inactive_at,
-      :updated_at, :submitted_at, :reviewed_at, :gogovan_transport_id,
-      :crossroads_transport_id, :review_completed_at, :received_at,
-      :delivered_by, :closed_by_id, :cancelled_at, :received_by_id,
-      :start_receiving_at, :cancellation_reason_id, :cancel_reason
+    attributes :id, :state, :created_at, :inactive_at,
+      :updated_at, :submitted_at, :reviewed_at, :review_completed_at,
+      :received_at, :cancelled_at, :start_receiving_at
 
     has_one  :closed_by, serializer: UserSummarySerializer, root: :user
     has_one  :created_by, serializer: UserSummarySerializer, root: :user
