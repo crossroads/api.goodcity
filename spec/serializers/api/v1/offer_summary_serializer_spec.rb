d526db748e7705e0ffb23643b3128208b280be86
Steve Kenworthy <steveyken@gmail.com>
2019-03-12 16:46:10 +0800

    Added counts for offer packages states

--- spec/serializers/api/v1/offer_summary_serializer_spec.rb
+++ spec/serializers/api/v1/offer_summary_serializer_spec.rb
@@ -1,28 +1,39 @@
 require 'rails_helper'
 
-describe Api::V1::OfferSummarySerializer do
+context Api::V1::OfferSummarySerializer do
 
   let(:offer)      { create(:offer, :with_items) }
   let(:serializer) { Api::V1::OfferSummarySerializer.new(offer, root: 'offer').as_json }
-  let(:json)       { JSON.parse( serializer.to_json ) }
+  
+  subject { JSON.parse( serializer.to_json ) }
 
   it "should only have the user and images associations" do
-    expect(json.keys).to include("user")
-    expect(json.keys).to include("images")
-    expect(json.keys).to include("offer")
-    expect(json.keys).not_to include("items")
-    expect(json.keys).not_to include("messages")
-    expect(json.keys).not_to include("gogovan_transport")
-    expect(json.keys).not_to include("crossroads_transport")
-    expect(json.keys).not_to include("cancellation_reason")
+    expect(subject.keys).to include("user")
+    expect(subject.keys).to include("images")
+    expect(subject.keys).to include("offer")
+    expect(subject.keys).not_to include("items")
+    expect(subject.keys).not_to include("messages")
+    expect(subject.keys).not_to include("gogovan_transport")
+    expect(subject.keys).not_to include("crossroads_transport")
+    expect(subject.keys).not_to include("cancellation_reason")
   end
 
   it "should include a display image" do
-    rec = json['offer']
+    rec = subject['offer']
     expect(rec['display_image_id']).not_to be_nil
     expect(rec['display_image_id']).to eq(offer.items.first.images.first.id)
 
-    img = json['images'].detect { |im| im['id'] == rec['display_image_id'] }
+    img = subject['images'].detect { |im| im['id'] == rec['display_image_id'] }
     expect(img).not_to eq(nil)
   end
+
+  it "should include count attributes on offer" do
+    expect(subject['offer'].keys).to include("submitted_items_count")
+    expect(subject['offer'].keys).to include("accepted_items_count")
+    expect(subject['offer'].keys).to include("rejected_items_count")
+    expect(subject['offer'].keys).to include("expecting_packages_count")
+    expect(subject['offer'].keys).to include("missing_packages_count")
+    expect(subject['offer'].keys).to include("received_packages_count")
+  end
+
 end
