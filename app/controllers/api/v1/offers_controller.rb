a6f63b0006b91dc29a6a5b01778c86942461c64f
Steve Kenworthy <steveyken@gmail.com>
2019-03-14 14:55:49 +0800

    Don't need eager_load when using PES.

--- app/controllers/api/v1/offers_controller.rb
+++ app/controllers/api/v1/offers_controller.rb
@@ -62,12 +62,10 @@ module Api
         end
         @offers = filter_created_by(@offers)
         @offers = @offers.reviewed_by(params["reviewed_by_id"]) if params["reviewed_by_id"].present?
-        #@offers = (params[:summarize] == 'true') ? @offers.with_summary_eager_load : @offers.with_eager_load
         @options = { each_serializer: select_serializer, include_orders_packages: false,
           exclude_messages: params["exclude_messages"] == "true", root: 'offers' }
         @options.merge!(summary: true) if params[:summarize] == 'true'
-        # render json: ActiveModel::ArraySerializer.new(@offers, @options).as_json
-        render json: @offers.with_eager_load, **@options # PES
+        render json: @offers, **@options
       end
 
       api :GET, '/v1/offers/1', "List an offer"
