1284ab005305f646351c8b8fa0434a860825589c
Namrata Ukirde <namrata.ukirde@kiprosh.com>
2019-03-25 12:53:53 +0530

    Revert "action mailer implementation"

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: ENV["FROM_EMAIL"]
-
-  def send_pin_email(user)
-    @user = user
-    @pin = user.most_recent_token.otp_code
-    mail(to: @user.email, subject: "GoodCity.HK pin code")
-  end
-end
