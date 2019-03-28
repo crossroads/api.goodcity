918f27846a875efd62abf82da3af83038a599b70
AbulAsar <abul.a@kiprosh.com>
2019-03-28 19:44:43 +0530

    refactor designator to 3 actions methods

--- lib/classes/designator.rb
+++ lib/classes/designator.rb
@@ -9,21 +9,10 @@ class Designator
   end
 
   #checks if already designated before redesignating
-  def designate_or_redesignate
-    if designated?
-      return designated_to_same_order? unless designated_to_same_order?&.errors.blank?
-      redesignate
-    end
-    designate_to_goodcity_and_stockit
-  end
-
   def designate
-    @orders_package.order_id = @order_id
-    @orders_package.quantity = quantity.to_i
-    @orders_package.updated_by = User.current_user
-    @orders_package.state = 'designated'
-    @orders_package.save
-    @orders_package
+    return already_designated_error! unless designated_to_order?&.errors.blank?
+    redesignate if designated?
+    designate_to_goodcity_and_stockit
   end
 
   def undesignate(undesignate_package = nil) #undesignate_package params is passed from redesignate
@@ -32,14 +21,11 @@ class Designator
     @package.undesignate_from_stockit_order
   end
 
-  def designate_stockit_item
-    @package.designate_to_stockit_order(@order_id)
-  end
-
-  def designated_to_same_order?
-    orders_package = OrdersPackage.find_by_id(@params[:orders_package_id])
-    orders_package.errors.add("package_id", "Already designated to this Order") if orders_package.try(:order_id) === @order_id
-    return orders_package
+  def redesignate
+    undesignate_package = {}
+    @params[:quantity] = @params[:received_quantity]
+    undesignate_package["0"] = @params
+    undesignate(undesignate_package)
   end
 
   private
@@ -52,15 +38,32 @@ class Designator
     designated? ? @params[:received_quantity] : @params[:quantity]
   end
 
-  def redesignate
-    undesignate_package = {}
-    @params[:quantity] = @params[:received_quantity]
-    undesignate_package["0"] = @params
-    undesignate(undesignate_package)
-  end
-
   def designate_to_goodcity_and_stockit
-    return designate if designate.errors
+    return designate_item if designate_item.errors
     designate_stockit_item
   end
+
+  def designate_stockit_item
+    @package.designate_to_stockit_order(@order_id)
+  end
+
+  def designate_item
+    @orders_package.order_id = @order_id
+    @orders_package.quantity = quantity.to_i
+    @orders_package.updated_by = User.current_user
+    @orders_package.state = 'designated'
+    @orders_package.save
+    @orders_package
+  end
+
+  def already_designated_error!
+    return designated_to_order? if designated?
+  end
+
+  def designated_to_order?
+    orders_package = OrdersPackage.find_by_id(@params[:orders_package_id])
+    orders_package && orders_package.check_valid_order!(@order_id)
+    orders_package
+  end
+
 end
