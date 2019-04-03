46f8f52b59b64d922f211f76724059f26e84732e
Steve Kenworthy <steveyken@gmail.com>
2019-04-03 20:06:08 +0800

    Set staging mail to mailhog

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -49,13 +49,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    :address  => "smtp.postmarkapp.com",
-    :port  => 587,
-    :user_name => ENV['POSTMARK_API_KEY'],
-    :password  => ENV['POSTMARK_API_KEY'],
-    :authentication  => :plain,
-    :enable_starttls_auto => true,
-    :domain => 'postmarkapp.com'
+    :address  => "localhost",
+    :port  => 1025,
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
