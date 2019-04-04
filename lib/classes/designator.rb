365f6c134a11e8e41a639e66b27f8e199f6d1d24
AbulAsar <abul.a@kiprosh.com>
2019-04-04 17:46:37 +0530

    add minor spacing and naming refactoring

--- lib/classes/designator.rb
+++ lib/classes/designator.rb
@@ -6,26 +6,26 @@ class Designator
     @params = package_params
     @order_id = package_params[:order_id].to_i
     @orders_package = @package.orders_packages.new
-    @existing_designation =  OrdersPackage.find_by_id(@params[:orders_package_id])
+    @existing_designation = OrdersPackage.find_by_id(@params[:orders_package_id])
   end
 
   def designate
     if designating_to_existing_designation?
       add_error_and_return_existing_designation
     else
-      undesignate_designated_package if @package.quantity.zero?
+      form_nested_params_for_undesignate if @package.quantity.zero?
       designate_item
     end
   end
 
-  # undesignate_package params is passed from 'undesignate_designated_package'
+  # undesignate_package params is passed from 'form_nested_params_for_undesignate'
   def undesignate(undesignate_package = nil)
     packages = undesignate_package ? undesignate_package : @params
     OrdersPackage.undesignate_partially_designated_item(packages)
     @package.undesignate_from_stockit_order
   end
 
-  def undesignate_designated_package
+  def form_nested_params_for_undesignate
     undesignate_package = {}
     @params[:quantity] = @package.received_quantity
     undesignate_package["0"] = @params
@@ -45,7 +45,7 @@ class Designator
 
   def designate_item
     @orders_package.order_id = @order_id
-    @orders_package.quantity = @params[:quantity_to_designate] ? @params[:quantity_to_designate] :  @params[:quantity]
+    @orders_package.quantity = @params[:quantity_to_designate] ? @params[:quantity_to_designate] : @params[:quantity]
     @orders_package.updated_by = User.current_user
     @orders_package.state = 'designated'
     @orders_package.save
