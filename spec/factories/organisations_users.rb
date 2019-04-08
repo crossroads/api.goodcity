7e407452b72838867e13b8f4ec6a011a5a5b4817
namrataukirde <namrata.ukirde@kiprosh.com>
2019-04-08 15:32:13 +0530

    fix mobile number not getting saved up on sign-up using email

--- spec/factories/organisations_users.rb
+++ spec/factories/organisations_users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :organisations_user do
-    association     :organisation, factory: :organisation
-    association     :user, factory: :user
+    association :organisation, factory: :organisation
+    association :user, factory: :user
     position "MyString"
+    preferred_contact_number { generate(:phone_number) }
   end
 end
