52b56e7df019310539e3ca953fe572d566e4b52b
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-04-10 15:12:03 +0800

    Rename of package_images to images

--- app/serializers/api/v1/package_serializer.rb
+++ app/serializers/api/v1/package_serializer.rb
@@ -3,7 +3,7 @@ module Api::V1
     embed :ids, include: true
 
     has_one :package_type, serializer: PackageTypeSerializer
-    has_many :images, serializer: ImageSerializer, root: :package_images
+    has_many :images, serializer: ImageSerializer
     has_many :packages_locations, serializer: PackagesLocationSerializer
     has_many :orders_packages, serializer: OrdersPackageSerializer
 
