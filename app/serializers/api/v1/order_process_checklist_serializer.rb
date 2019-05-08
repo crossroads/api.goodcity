d84c2dab7b862ea6056803f240a4c6e3ad043491
Darshan Chauhan <darshan.chauhan@kiprosh.com>
2019-05-08 09:57:07 +0530

    DesignationId added to process checklist serializer

--- app/serializers/api/v1/order_process_checklist_serializer.rb
+++ app/serializers/api/v1/order_process_checklist_serializer.rb
@@ -1,6 +1,14 @@
 module Api::V1
   class OrderProcessChecklistSerializer < ApplicationSerializer
     embed :ids, include: true
-    attributes :id, :order_id, :process_checklist_id
+    attributes :id, :order_id, :process_checklist_id, :designation_id
+    
+    def designation_id
+      object.order_id
+    end
+
+    def designation_id__sql
+      "order_id"
+    end
   end
 end
