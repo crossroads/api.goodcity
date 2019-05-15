6d5323760676f81c0cdaeb531abe6555d88d5d65
Steve Kenworthy <steveyken@gmail.com>
2019-05-15 15:03:14 +0800

    Log the job actions

--- app/jobs/cloudinary_image_cleanup_job.rb
+++ app/jobs/cloudinary_image_cleanup_job.rb
@@ -3,7 +3,11 @@ class CloudinaryImageCleanupJob < ActiveJob::Base
 
   def perform(cloudinary_image_id)
     return if %w(development test).include?(Rails.env)
-    return if (ENV['PREVENT_CLOUDINARY_IMAGE_DELETION'] == 'true')
-    Cloudinary::Api.delete_resources([cloudinary_image_id])
+    if (ENV['PREVENT_CLOUDINARY_IMAGE_DELETION'] == 'true')
+      Rails.logger.info("CloudinaryImageCleanupJob: prevented from deleting image '#{cloudinary_image_id}'")
+    else
+      Rails.logger.info("CloudinaryImageCleanupJob: deleting image '#{cloudinary_image_id}'")
+      Cloudinary::Api.delete_resources([cloudinary_image_id])
+    end
   end
 end
