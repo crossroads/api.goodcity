25352a7b009a011efb774db329e8ea67b69dd213
namrataukirde <namrata.ukirde@kiprosh.com>
2019-05-17 11:40:46 +0530

    WIP refactoring

--- lib/classes/organisations_user_builder.rb
+++ lib/classes/organisations_user_builder.rb
@@ -11,7 +11,7 @@ class OrganisationsUserBuilder
   #     :email
   #   }
 
-  def initialize(params)
+  def initialize(params, app_name)
     @organisations_user = OrganisationsUser.find_by_id(params["id"]) if params["id"]
     @user = @organisations_user.user if @organisations_user
     @organisation_id = params["organisation_id"].presence.try(:to_i)
@@ -20,12 +20,13 @@ class OrganisationsUserBuilder
     @email = @user_attributes["email"].presence
     @position = params["position"]
     @preferred_contact_number = params["preferred_contact_number"]
+    @app_name = app_name
     fail_with_error(I18n.t("organisations_user_builder.organisation.blank")) unless @organisation_id
     fail_with_error(I18n.t("organisations_user_builder.user.mobile.blank")) unless @mobile
   end
 
-  def build(app_name)
-    @user = build_user(app_name)
+  def build
+    @user = build_user
     return fail_with_error(@user.errors) unless @user.valid?
     return fail_with_error(I18n.t("organisations_user_builder.organisation.not_found")) unless organisation
     if !user_belongs_to_organisation(@user)
@@ -40,18 +41,18 @@ class OrganisationsUserBuilder
 
   def build_user(app_name)
     @user = User.where("email = (?) OR mobile = (?)", @email, @mobile).first_or_initialize(@user_attributes)
-    assign_user_app_acessor(app_name)
-    @user.save
-    @user
+    # assign_user_app_acessor
+    # @user.save
+    # @user
   end
 
-  def assign_user_app_acessor(app_name)
-    @user.request_from_stock = (app_name == STOCK_APP)
-    @user.request_from_browse = (app_name == BROWSE_APP)
+  def assign_user_app_acessor
+    @user.request_from_stock = (@app_name == STOCK_APP)
+    @user.request_from_browse = (@app_name == BROWSE_APP)
   end
 
   def update
-    return fail_with_error(update_user["errors"]) if update_user && update_user["errors"]
+    return fail_with_error(update_user["errors"]) if assign_charity_role_and_save_user && @user.errors?
     @organisations_user.update(position: @position, preferred_contact_number: @preferred_contact_number)
     return_success.merge!("organisations_user" => @organisations_user.reload)
   end
@@ -63,8 +64,17 @@ class OrganisationsUserBuilder
   end
 
   def update_user
-    @user.roles << charity_role unless @user.roles.include?(charity_role)
+    @user.user_roles.new(role: charity_role) unless @user.roles.include?(charity_role)
     if @user.update(@user_attributes)
+      @user
+    else
+      fail_with_error(@user.errors.full_messages.join(" "))
+    end
+  end
+
+  def assign_charity_role_and_save_user
+    @user.user_roles.new(role: charity_role)  unless @user.charity?
+    if @user.save
       @user
     else
       fail_with_error(@user.errors.full_messages.join(" "))
