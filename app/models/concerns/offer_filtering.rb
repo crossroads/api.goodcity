bbfeb9c1e485ba3bf96a8f2aa679b7ca2a93c267
AbulAsar <abul.a@kiprosh.com>
2019-06-28 18:31:45 +0530

    priority logic added | searching according to states

--- app/models/concerns/offer_filtering.rb
+++ app/models/concerns/offer_filtering.rb
@@ -13,6 +13,12 @@ module OfferFiltering
     end
 
     def self.priority
+      where <<-SQL
+        (offers.state = 'scheduled' AND schedules.scheduled_at::timestamptz < timestamptz '#{Time.zone.now}') OR
+        (offers.state = 'reviewed' AND timestamptz '#{Time.zone.now}' - review_completed_at::timestamptz > '#{48.hours}') OR
+        (offers.state = 'under_review' AND timestamptz '#{Time.zone.now}' - reviewed_at::timestamptz > '#{24.hours}') OR
+        (offers.state = 'receiving' AND start_receiving_at::timestamptz < timestamptz '#{last_6pm}')
+      SQL
     end
 
     def self.self_reviewer
@@ -25,6 +31,14 @@ module OfferFiltering
 
     def self.due_before(time)
       where('schedules.scheduled_at <= (?)', time)
+    end
+
+    # Helpers
+    def self.last_6pm
+      now = Time.now.in_time_zone
+      t = now.change(hour: 18, min: 0, sec: 0)
+      t -= 24.hours if now < t
+      t
     end
   end
 end
