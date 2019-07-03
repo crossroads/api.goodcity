9ed3079e806448dcc2fcfc7b36c4284747ae4883
AbulAsar <abul.a@kiprosh.com>
2019-07-03 20:53:28 +0530

    hours ago added in time comparison

--- app/models/concerns/offer_filtering.rb
+++ app/models/concerns/offer_filtering.rb
@@ -15,8 +15,8 @@ module OfferFiltering
     def self.priority
       where <<-SQL
         (offers.state = 'scheduled' AND schedules.scheduled_at::timestamptz < timestamptz '#{Time.zone.now}') OR
-        (offers.state = 'reviewed' AND timestamptz '#{Time.zone.now}' - review_completed_at::timestamptz > '#{48.hours}') OR
-        (offers.state = 'under_review' AND timestamptz '#{Time.zone.now}' - reviewed_at::timestamptz > '#{24.hours}') OR
+        (offers.state = 'reviewed' AND review_completed_at::timestamptz < '#{48.hours.ago}') OR
+        (offers.state = 'under_review' AND reviewed_at::timestamptz < '#{24.hours.ago}') OR
         (offers.state = 'receiving' AND start_receiving_at::timestamptz < timestamptz '#{last_6pm}')
       SQL
     end
