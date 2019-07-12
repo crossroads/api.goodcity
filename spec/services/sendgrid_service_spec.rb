3b598d28761113b2f1ed04a000e8df07a40697bd
Abhinav Garg <abhinav.g@kiprosh.com>
2019-07-12 12:30:15 +0530

    adding specs

--- spec/services/sendgrid_service_spec.rb
+++ spec/services/sendgrid_service_spec.rb
@@ -17,11 +17,15 @@ describe SendgridService do
     it "returns english template constant name if locale is en" do
       I18n.locale = :en
       expect(sendgrid.pin_template_id).to eq "SENDGRID_PIN_TEMPLATE_ID_EN"
+      expect(sendgrid.pickup_template_id).to eq "SENDGRID_PICKUP_TEMPLATE_ID_EN"
+      expect(sendgrid.delivery_template_id).to eq "SENDGRID_DELIVERY_TEMPLATE_ID_EN"
     end
 
     it "returns chinese template constant name if locale is zh-tw" do
       I18n.locale = :"zh-tw"
       expect(sendgrid.pin_template_id).to eq "SENDGRID_PIN_TEMPLATE_ID_ZH_TW"
+      expect(sendgrid.pickup_template_id).to eq "SENDGRID_PICKUP_TEMPLATE_ID_ZH_TW"
+      expect(sendgrid.delivery_template_id).to eq "SENDGRID_DELIVERY_TEMPLATE_ID_ZH_TW"
     end
   end
 
@@ -83,5 +87,97 @@ describe SendgridService do
       end
     end
 
+  end
+
+  describe "Order submission email for pickup" do
+    let!(:order) { create :order, :with_state_submitted }
+    let!(:order_transport) { create :order_transport, order: order, transport_type: 'self' }
+    let!(:beneficiary) { create :beneficiary, order: order }
+    let(:organisation) { create :organisation }
+
+    before(:each) do
+      allow(sendgrid).to receive(:send_to_sendgrid).and_return(true)
+      allow(sendgrid).to receive(:send_email).and_return(true)
+    end
+
+    context "Template data" do
+
+      it "should send the user's information" do
+        user.organisations = [organisation]
+        user.save
+        expect(sendgrid).to receive(:send_email) do
+          data_sent = sendgrid.substitution_hash
+          expect(data_sent['contact_name']).to eq(user.first_name + ' ' + user.last_name)
+          expect(data_sent['contact_organisation_name_en']).to eq(user.organisations.first.name_en)
+          expect(data_sent['contact_organisation_name_zh_tw']).to eq(user.organisations.first.name_zh_tw)
+        end
+
+        sendgrid.send_order_pickup_email order
+      end
+
+
+      it "should send the order's information" do
+        expect(sendgrid).to receive(:send_email) do
+          data_sent = sendgrid.substitution_hash
+          expect(data_sent['order_code']).to eq(order.code)
+          expect(data_sent['domail']).to eq("browse")
+          expect(data_sent['booking_type']).to eq(order.booking_type.name_en)
+          expect(data_sent['booking_type_zh']).to eq(order.booking_type.name_zh_tw)
+
+          expect(data_sent['scheduled_at']).to eq(order.order_transport.scheduled_at.in_time_zone.strftime("%e %b %Y %H:%M%p"))
+          expect(data_sent['client']).not_to be_nil
+          expect(data_sent['client'][:name]).to eq(order.beneficiary.first_name + ' ' + order.beneficiary.last_name)
+          expect(data_sent['client'][:phone]).to eq(order.beneficiary.phone_number)
+          expect(data_sent['client'][:id_type]).to eq(order.beneficiary.identity_type.name_en)
+          expect(data_sent['client'][:id_no]).to eq(order.beneficiary.identity_number)
+        end
+        sendgrid.send_order_pickup_email order
+      end
+    end
+  end
+
+  describe "Order submission email for delivery" do
+    let!(:order) { create :order, :with_state_submitted }
+    let!(:order_transport) { create :order_transport, order: order }
+    let!(:beneficiary) { create :beneficiary, order: order }
+    let(:organisation) { create :organisation }
+
+    before(:each) do
+      allow(sendgrid).to receive(:send_to_sendgrid).and_return(true)
+      allow(sendgrid).to receive(:send_email).and_return(true)
+    end
+
+    context "Template data" do
+      it "should send the user's information" do
+        user.organisations = [organisation]
+        user.save
+        expect(sendgrid).to receive(:send_email) do
+          data_sent = sendgrid.substitution_hash
+          expect(data_sent['contact_name']).to eq(user.first_name + ' ' + user.last_name)
+          expect(data_sent['contact_organisation_name_en']).to eq(user.organisations.first.name_en)
+          expect(data_sent['contact_organisation_name_zh_tw']).to eq(user.organisations.first.name_zh_tw)
+        end
+        sendgrid.send_order_delivery_email order
+      end
+
+
+      it "should send the order's information" do
+        expect(sendgrid).to receive(:send_email) do
+          data_sent = sendgrid.substitution_hash
+          expect(data_sent['order_code']).to eq(order.code)
+          expect(data_sent['domail']).to eq("browse")
+          expect(data_sent['booking_type']).to eq(order.booking_type.name_en)
+          expect(data_sent['booking_type_zh']).to eq(order.booking_type.name_zh_tw)
+
+          expect(data_sent['scheduled_at']).to eq(order.order_transport.scheduled_at.in_time_zone.strftime("%e %b %Y %H:%M%p"))
+          expect(data_sent['client']).not_to be_nil
+          expect(data_sent['client'][:name]).to eq(order.beneficiary.first_name + ' ' + order.beneficiary.last_name)
+          expect(data_sent['client'][:phone]).to eq(order.beneficiary.phone_number)
+          expect(data_sent['client'][:id_type]).to eq(order.beneficiary.identity_type.name_en)
+          expect(data_sent['client'][:id_no]).to eq(order.beneficiary.identity_number)
+        end
+        sendgrid.send_order_delivery_email order
+      end
+    end
   end
 end
