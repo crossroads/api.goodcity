7e5a69117782306dbbbc5e5de4b3a158d4d4ad78
Abhinav Garg <abhinav.g@kiprosh.com>
2019-07-15 11:15:00 +0530

    redirect users to packages controller if they hit

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace "api" do
     namespace "v1", defaults: { format: "json" } do
 
+      get "/browse/fetch_packages", to: "packages#index"
       post "auth/signup", to: "authentication#signup"
       post "auth/verify", to: "authentication#verify"
       post "auth/send_pin", to: "authentication#send_pin"
