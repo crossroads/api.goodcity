f499e72319f03e7c9c5574fae0805d6f0010b1bb
Patrick R <patrick.rabier@gmail.com>
2019-08-08 16:26:28 +0800

    Update mark_all_read ability
    Co-Authored-By: Steve Kenworthy <steveyken@gmail.com>

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -182,7 +182,8 @@ class Ability
         message.offer.created_by_id == @user_id && !message.is_private
       end
     end
-    can [:mark_read, :mark_all_read], Message, id: @user.subscriptions.pluck(:message_id)
+    can [:mark_read], Message, id: @user.subscriptions.pluck(:message_id)
+    can [:mark_all_read]
   end
 
   def offer_abilities
