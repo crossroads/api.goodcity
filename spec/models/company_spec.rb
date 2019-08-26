6434be13e6bc6626b39cdcd818bc05ecca0a6a01
namrataukirde <namrata.ukirde@kiprosh.com>
2019-08-26 19:22:01 +0530

    fix test cases

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Company, type: :model do
   describe "Associations" do
-    it { is_expected.to has_many :offers  }
+    it { is_expected.to have_many :offers  }
   end
 end
