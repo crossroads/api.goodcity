c540b1fb966e795738d42256dbbb107b67bbad50
Abhinav Garg <abhinav.g@kiprosh.com>
2019-08-27 12:10:29 +0530

    add validation spec to company model

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Company, type: :model do
   describe "Associations" do
     it { is_expected.to have_many :offers  }
   end
+
+  describe "Validations" do
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
 end
