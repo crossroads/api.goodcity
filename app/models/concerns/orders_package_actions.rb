423a0263430a71ca1aa13b5d60e078f32c7dae36
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-09-27 10:02:51 +0800

    OrdersPackages actions

---
+++ app/models/concerns/orders_package_actions.rb
@@ -0,0 +1,95 @@
+module OrdersPackageActions
+  extend ActiveSupport::Concern
+
+  #
+  # Enum of all available actions
+  #
+  module Actions
+    class ActionWrapper
+      def initialize(name)
+        @name = name
+      end
+
+      def on
+        serialize(true)
+      end
+
+      def off
+        serialize(false)
+      end
+
+      def serialize(enabled = true)
+        { name: @name, enabled: enabled }
+      end
+
+      alias_method :if, :serialize
+    end
+
+    REDESIGNATE     = ActionWrapper.new('redesignate')
+    EDIT_QUANTITY   = ActionWrapper.new('edit_quantity')
+    CANCEL          = ActionWrapper.new('cancel')
+    DISPATCH        = ActionWrapper.new('dispatch')
+    UNDISPATCH      = ActionWrapper.new('undispatch')
+  end
+
+  #
+  # Returns a list of actions which can be run against the orders_package
+  #
+  def allowed_actions
+    ActionResolver.resolve(self)
+  end
+
+  private
+
+  # Small helper class that resolves the available actions
+  # of an orders_package
+  #
+  # @example
+  #   actions = ActionResolver.resolve(orders_package)
+  #   puts(actions)  # => [{ name: 'cancel', enabled: true }, ... ]
+  #
+  #
+  class ActionResolver
+
+    def initialize(orders_package)
+      @model = orders_package
+    end
+
+    def self.resolve(orders_package)
+      ActionResolver.new(orders_package).resolve
+    end
+
+    def resolve
+      case @model
+        when ORDER_FINISHED then []
+        when PACKAGE_CANCELLED then [ Actions::REDESIGNATE.on ]
+        when PACKAGE_DESIGNATED then [
+          Actions::EDIT_QUANTITY.if(can_edit_qty?),
+          Actions::CANCEL.on,
+          Actions::DISPATCH.on
+        ]
+        when PACKAGE_DISPATCHED then [ Actions::UNDISPATCH.on ]
+        else [] # default
+      end
+    end
+
+    private
+
+    ORDER_FINISHED = -> (model) { Order::INACTIVE_STATES.include? model.order.state }
+    PACKAGE_CANCELLED = -> (model) { model.cancelled? }
+    PACKAGE_DESIGNATED = -> (model) { model.designated? }
+    PACKAGE_DISPATCHED = -> (model) { model.dispatched? }
+
+    def can_decrease_qty?
+      @model.quantity > 1
+    end
+
+    def can_increae_qty?
+      @model.package.quantity > 0
+    end
+
+    def can_edit_qty?
+      can_decrease_qty? || can_increae_qty?
+    end
+  end
+end
