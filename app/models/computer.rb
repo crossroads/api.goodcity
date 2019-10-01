9e281f9d60d15fc2e9c2cc31701b140d35fdc5a5
Abhinav Garg <abhinav.g@kiprosh.com>
2019-10-01 12:52:12 +0530

    create a service

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -3,20 +3,6 @@ class Computer < ActiveRecord::Base
   has_one :package, as: :detail, dependent: :destroy
   after_save :sync_to_stockit
 
-  def self.create_detail(attributes)
-    params = ActiveSupport::HashWithIndifferentAccess.new(attributes)
-    begin
-      create(params)
-    rescue => exception
-      puts exception
-    end
-  end
-
-  def update_detail(attributes)
-    params = ActiveSupport::HashWithIndifferentAccess.new(attributes)
-    update(params)
-  end
-
   private
   def sync_to_stockit
     # do sync related stuff here
--- app/serializers/api/v1/computer_serializer.rb
+++ app/serializers/api/v1/computer_serializer.rb
@@ -5,6 +5,6 @@ module Api::V1
           :cpu, :ram, :hdd, :optical, :video, :sound, :lan, :wireless,
           :usb, :comp_voltage, :os, :os_serial_num, :ms_office_serial_num,
           :mar_os_serial_num, :mar_ms_office_serial_num, :updated_by_id
-    has_one :package, serializer: StockitItemSerializer
+    # has_one :package, serializer: StockitItemSerializer, root: :items
   end
 end
