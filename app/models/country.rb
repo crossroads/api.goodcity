6f95e76b24ca9b58fba2734b481ad0dfa86fad4a
Abhinav Garg <abhinav.g@kiprosh.com>
2019-10-07 11:42:40 +0530

    add search index to countries

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,13 @@
 class Country < ActiveRecord::Base
   include RollbarSpecification
-
   translates :name
   validates :name_en, presence: true
+
+  def self.search(options = {})
+    search_text = options[:search_text] || ''
+    search_query = ['name_en', 'name_zh_tw'].
+          map { |f| "countries.#{f} ILIKE :search_text" }.
+          join(" OR ")
+    query = where(search_query, search_text: "%#{search_text}%")
+  end
 end
