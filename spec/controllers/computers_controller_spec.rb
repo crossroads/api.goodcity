d55df52516578bd9b4e3ba5d25336c52f8cfb4ed
Abhinav Garg <abhinav.g@kiprosh.com>
2019-10-14 12:28:06 +0530

    add specs for models

--- spec/controllers/computers_controller_spec.rb
+++ spec/controllers/computers_controller_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe ComputersController, type: :controller do
+# RSpec.describe ComputersController, type: :controller do
 
-end
+# end
