d6df52a3fc1252383454c1b5daf6ea5b5a827801
Abhinav Garg <abhinav.g@kiprosh.com>
2019-10-14 12:24:09 +0530

    add specs for models

--- spec/factories/computers.rb
+++ spec/factories/computers.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :computer do
-    
+    brand  "Macbook"
+    country_id "210"
+    size "13\""
+    cpu "1.2Ghz"
+    ram "4GB DDR3"
+    hdd "500GB SATA"
+    optical "VersionTECH"
+    video "Radeon RX 470"
+    sound "Bose Companion 2 Series III"
+    lan "Ethernet Adapter"
+    wireless "Andrew"
+    usb "2.0, 3.0"
+    comp_voltage "100/240V"
+    os "Mac OS High Sierra"
+    comp_test_status "ACTIVE"
   end
 end
