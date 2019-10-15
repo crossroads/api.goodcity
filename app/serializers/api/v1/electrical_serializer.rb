f204a75eeeb2f1415db4eca2c083b48c7806ec37
Abhinav Garg <abhinav.g@kiprosh.com>
2019-10-15 16:25:17 +0530

    remove country from serializer

--- app/serializers/api/v1/electrical_serializer.rb
+++ app/serializers/api/v1/electrical_serializer.rb
@@ -1,7 +1,6 @@
 module Api::V1
   class ElectricalSerializer < ApplicationSerializer
     embed :ids, include: true
-    has_one :country
     attributes :id, :brand, :model, :serial_number, :country_id, :standard,
         :voltage, :frequency, :power, :system_or_region, :test_status,
         :tested_on, :updated_by_id
