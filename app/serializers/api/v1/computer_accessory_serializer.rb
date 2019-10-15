e4bbb3bff9877976f2033a4ef08cfd84e315774c
Abhinav Garg <abhinav.g@kiprosh.com>
2019-10-15 15:46:09 +0530

    send country along with detail

--- app/serializers/api/v1/computer_accessory_serializer.rb
+++ app/serializers/api/v1/computer_accessory_serializer.rb
@@ -1,6 +1,7 @@
 module Api::V1
   class ComputerAccessorySerializer < ApplicationSerializer
     embed :ids, include: true
+    has_one :country
     attributes :id, :brand, :model, :serial_num, :country_id, :size,
           :interface, :comp_voltage, :comp_test_status, :updated_by_id
   end
--- app/serializers/api/v1/electrical_serializer.rb
+++ app/serializers/api/v1/electrical_serializer.rb
@@ -1,6 +1,7 @@
 module Api::V1
   class ElectricalSerializer < ApplicationSerializer
     embed :ids, include: true
+    has_one :country
     attributes :id, :brand, :model, :serial_number, :country_id, :standard,
         :voltage, :frequency, :power, :system_or_region, :test_status,
         :tested_on, :updated_by_id
