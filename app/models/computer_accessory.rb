2b6dd4ad2f61fa1508d750e2747d6f96277b2f72
Abhinav Garg <abhinav.g@kiprosh.com>
2019-10-18 16:45:27 +0530

    fix code climate issues

--- app/models/computer_accessory.rb
+++ app/models/computer_accessory.rb
@@ -2,12 +2,11 @@ class ComputerAccessory < ActiveRecord::Base
   include SubformCallbacks
   has_paper_trail class_name: "Version"
 
-  belongs_to :country, required:false
+  belongs_to :country, required: false
   has_one :package, as: :detail, dependent: :destroy
   after_create :create_on_stockit
   after_update :update_on_stockit
   before_save :downcase_brand, if: :brand_changed?
   before_save :save_correct_country, if: :request_from_stockit?
   before_save :set_updated_by
-
 end
