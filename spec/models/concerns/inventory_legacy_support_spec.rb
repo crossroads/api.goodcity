edb0067716ef1519da15e43bffe80b28193b828a
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-10-28 11:09:00 +0800

    Support sync for all PackageLocation changes

--- spec/models/concerns/inventory_legacy_support_spec.rb
+++ spec/models/concerns/inventory_legacy_support_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
 describe InventoryLegacySupport do
+  let(:location) { create(:location) }
+  let(:other_location) { create(:location) }
   let(:user) { create(:user) }
-  let(:packages_location) { create(:packages_location, quantity: quantity) }
-  let(:package) { packages_location.package }
+  let(:package) { create(:package) }
+  let(:other_package) { create(:package) }
+  let(:packages_location) { create(:packages_location, quantity: quantity, location: location, package: package) }
   let(:computer) { PackagesInventory::Computer }
 
   before { User.current_user = user }
+
+  after do
+    # After each test, we assert that the two tables are in sync
+
+    # 1. The total number of packages is the same on the two tables
+    expect(computer.total_quantity.now).to eq(PackagesLocation.sum(:quantity))
+
+    # 2. Each package has the same quantity on both tables
+    Package.all.each do |pkg|
+      expect(computer.package_quantity(pkg).now).to eq(
+        PackagesLocation.where(package: pkg).sum(:quantity)
+      )
+    end
+  end
 
   describe "Syncing PackagesLocation <-> PackagesInventory" do
 
@@ -105,6 +122,165 @@ describe InventoryLegacySupport do
             }.by(-1 * removed_quantity)
 
             expect(computer.package_quantity(package).now).to eq(quantity - removed_quantity)
+          end
+        end
+
+        context "by zero-ing its quantity" do
+          let!(:available_qty) {  packages_location.quantity }
+
+          it "records a LOSS in the inventory" do
+            expect {
+              packages_location.update(quantity: 0)
+            }.to change(PackagesInventory, :count).by(1)
+
+            expect(PackagesInventory.count).to eq(2)
+            record = PackagesInventory.last
+            expect(record.quantity).to eq(-1 * available_qty)
+            expect(record.package).to eq(packages_location.package)
+            expect(record.location).to eq(packages_location.location)
+            expect(record.action).to eq('loss')
+            expect(record.source).to be_nil
+          end
+
+          it "accounts in the total computed quantity" do
+            expect {
+              packages_location.update(quantity: 0)
+            }.to change {
+              computer.package_quantity(package).now
+            }.by(-1 * available_qty)
+
+            expect(computer.package_quantity(package).now).to eq(0)
+          end
+        end
+
+        context "by changing the location" do
+          it "records a LOSS and a GAIN in the inventory" do
+            expect {
+              packages_location.update(location_id: other_location.id)
+            }.to change(PackagesInventory, :count).by(2)
+
+            expect(PackagesInventory.count).to eq(3)
+
+            first = PackagesInventory.all[1]
+            expect(first.quantity).to eq(-1 * packages_location.quantity)
+            expect(first.package).to eq(packages_location.package)
+            expect(first.location).to eq(location)
+            expect(first.action).to eq('loss')
+            expect(first.source).to be_nil
+
+            second = PackagesInventory.all[2]
+            expect(second.quantity).to eq(packages_location.quantity)
+            expect(second.package).to eq(packages_location.package)
+            expect(second.location).to eq(other_location)
+            expect(second.action).to eq('gain')
+            expect(second.source).to be_nil
+          end
+
+          context "and zero-ing the quantity" do
+            it "only records a LOSS" do
+              available_qty = packages_location.quantity
+
+              expect {
+                packages_location.update(location_id: other_location.id, quantity: 0)
+              }.to change(PackagesInventory, :count).by(1)
+
+              expect(PackagesInventory.count).to eq(2)
+
+              record = PackagesInventory.all[1]
+              expect(record.quantity).to eq(-1 * available_qty)
+              expect(record.package).to eq(packages_location.package)
+              expect(record.location).to eq(location)
+              expect(record.action).to eq('loss')
+              expect(record.source).to be_nil
+            end
+          end
+        end
+
+        context "by changing the package" do
+          it "records a LOSS and a GAIN in the inventory" do
+            expect {
+              packages_location.update(package_id: other_package.id)
+            }.to change(PackagesInventory, :count).by(2)
+
+            expect(PackagesInventory.count).to eq(3)
+
+            first = PackagesInventory.all[1]
+            expect(first.quantity).to eq(-1 * packages_location.quantity)
+            expect(first.package).to eq(package)
+            expect(first.location).to eq(location)
+            expect(first.action).to eq('loss')
+            expect(first.source).to be_nil
+
+            second = PackagesInventory.all[2]
+            expect(second.quantity).to eq(packages_location.quantity)
+            expect(second.package).to eq(other_package)
+            expect(second.location).to eq(location)
+            expect(second.action).to eq('gain')
+            expect(second.source).to be_nil
+          end
+
+          context "and zero-ing the quantity" do
+            it "only records a LOSS" do
+              available_qty = packages_location.quantity
+
+              expect {
+                packages_location.update(package_id: other_package.id, quantity: 0)
+              }.to change(PackagesInventory, :count).by(1)
+
+              expect(PackagesInventory.count).to eq(2)
+
+              record = PackagesInventory.all[1]
+              expect(record.quantity).to eq(-1 * available_qty)
+              expect(record.package).to eq(package)
+              expect(record.location).to eq(location)
+              expect(record.action).to eq('loss')
+              expect(record.source).to be_nil
+            end
+          end
+        end
+
+        context "by changing the package and the location" do
+          it "records a LOSS and a GAIN in the inventory" do
+            available_qty = packages_location.quantity
+
+            expect {
+              packages_location.update(package_id: other_package.id, location_id: other_location.id)
+            }.to change(PackagesInventory, :count).by(2)
+
+            expect(PackagesInventory.count).to eq(3)
+
+            first = PackagesInventory.all[1]
+            expect(first.quantity).to eq(-1 * available_qty)
+            expect(first.package).to eq(package)
+            expect(first.location).to eq(location)
+            expect(first.action).to eq('loss')
+            expect(first.source).to be_nil
+
+            second = PackagesInventory.all[2]
+            expect(second.quantity).to eq(available_qty)
+            expect(second.package).to eq(other_package)
+            expect(second.location).to eq(other_location)
+            expect(second.action).to eq('gain')
+            expect(second.source).to be_nil
+          end
+
+          context "and zero-ing the quantity" do
+            it "only records a LOSS" do
+              available_qty = packages_location.quantity
+
+              expect {
+                packages_location.update(package_id: other_package.id, location_id: other_location.id, quantity: 0)
+              }.to change(PackagesInventory, :count).by(1)
+
+              expect(PackagesInventory.count).to eq(2)
+
+              record = PackagesInventory.all[1]
+              expect(record.quantity).to eq(-1 * available_qty)
+              expect(record.package).to eq(package)
+              expect(record.location).to eq(location)
+              expect(record.action).to eq('loss')
+              expect(record.source).to be_nil
+            end
           end
         end
       end
