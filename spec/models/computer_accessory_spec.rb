8d35732f0913637997591f75497a98cca16ddebe
Abhinav Garg <abhinav.g@kiprosh.com>
2019-10-30 09:44:02 +0530

    uncomment and fix specs

--- spec/models/computer_accessory_spec.rb
+++ spec/models/computer_accessory_spec.rb
@@ -19,34 +19,23 @@ RSpec.describe ComputerAccessory, type: :model do
     it{ is_expected.to have_db_column(:stockit_id).of_type(:integer)}
   end
 
-  # describe "before_save" do
-  #   it "should convert brand to lowercase" do
-  #     expect(Stockit::ItemDetailSync).to receive(:create).and_return({"status"=>201, "computer_accessory_id"=> 12})
-  #     computer_accessory = create :computer_accessory, brand: "DeLL"
-  #     expect(computer_accessory.brand).to eq("dell")
-  #   end
-  # end
+  describe "before_save" do
+    it "should convert brand to lowercase" do
+      computer_accessory = build :computer_accessory, brand: "DeLL"
+      allow(Stockit::ItemDetailSync).to receive(:create).with(computer_accessory).and_return({"status"=>201, "computer_id"=> 12})
+      computer_accessory.save
+      expect(computer_accessory.brand).to eq("dell")
+    end
 
-  #   it "should set updated_by if changes done" do
-  #     user = create(:user, :reviewer)
-  #     User.current_user = user
-  #     computer_accessory = build :computer_accessory, brand: "DeLL"
-  #     expect(Stockit::ItemDetailSync).to receive(:create).and_return({"status"=>201, "computer_accessory_id"=> 12})
-  #     expect {
-  #       computer_accessory.save
-  #     }.to change(ComputerAccessory, :count).by(1)
-  #     expect(computer_accessory.updated_by_id).to eq(user.id)
-  #   end
-  # end
-
-  # describe "after_save" do
-  #   it "should sync to stockit after save" do
-  #     computer_accessory = build :computer_accessory
-  #     expect(Stockit::ItemDetailSync).to receive(:create).and_return({"status"=>201, "computer_accessory_id"=> 12})
-  #     expect {
-  #       computer_accessory.save
-  #     }.to change(ComputerAccessory, :count).by(1)
-  #     expect(computer_accessory.stockit_id).to eq(12)
-  #   end
-  # end
+    it "should set updated_by if changes done" do
+      user = create(:user, :reviewer)
+      User.current_user = user
+      computer_accessory = build :computer_accessory, brand: "DeLL"
+      allow(Stockit::ItemDetailSync).to receive(:create).with(computer_accessory).and_return({"status"=>201, "computer_accessory_id"=> 12})
+      expect {
+        computer_accessory.save
+      }.to change(ComputerAccessory, :count).by(1)
+      expect(computer_accessory.updated_by_id).to eq(user.id)
+    end
+  end
 end
