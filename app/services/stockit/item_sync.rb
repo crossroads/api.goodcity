4c8a63e76dbe9ffb4b0804647e6aad650d37bf66
Steve Kenworthy <steveyken@gmail.com>
2019-11-12 11:47:27 +0800

    Cover all cases for dispatched packages. Wrote specs

--- app/services/stockit/item_sync.rb
+++ app/services/stockit/item_sync.rb
@@ -140,7 +140,7 @@ module Stockit
     # GoodCity doesn't keep location records for dispatched packages
     #   but Stockit likes to put them in the Dispatched location
     def stockit_location_id
-      if package.stockit_sent_on.present?
+      if package.stockit_sent_on.present? or package.orders_packages.where(state: 'dispatched').any?
         Location.dispatch_location.stockit_id
       elsif package.packages_locations.count > 1
         Location.multiple_location.try(:stockit_id)
