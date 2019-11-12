da334be52f2b52ba498ae88b10e5f0a4ab4236bf
Steve Kenworthy <steveyken@gmail.com>
2019-11-12 12:09:27 +0800

    refactored sporadically failing test

--- spec/lib/goodcity/health_checks/orders_packages_order_id_nil_check_spec.rb
+++ spec/lib/goodcity/health_checks/orders_packages_order_id_nil_check_spec.rb
@@ -4,23 +4,18 @@ require 'goodcity/health_checks/orders_packages_order_id_check'
 context Goodcity::HealthChecks::OrdersPackagesOrderIdCheck do
 
   subject { described_class.new }
+  let!(:orders_package) { create(:orders_package) }
 
   it { expect(subject.desc).to eql("OrdersPackages should contain an order_id reference.") }
 
   it "passes" do
-    WebMock.disable!
-    order = create :order
-    package = create :package, :received
-    create :orders_package, order: order, package: package
     subject.run
     expect(subject.passed?).to eql(true)
-    WebMock.enable!
   end
 
-  # it "fails" do
-  #   orders_package = build :orders_package, order_id: nil
-  #   expect(orders_package.valid?).to eql(false)
-  #   expect(orders_package.errors.messages).to eql({:order=>["can't be blank"]})
-  # end
+  it "fails" do
+    orders_package.update_column(:order_id, nil)
+    expect(subject.passed?).to eql(false)
+  end
 
 end
