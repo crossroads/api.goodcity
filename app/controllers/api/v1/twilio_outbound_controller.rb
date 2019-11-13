1cb4791771809fda29a245a39548db550ce38b67
AbulAsar <abul.a@kiprosh.com>
2019-11-13 17:33:25 +0530

    testing branch on staging

--- app/controllers/api/v1/twilio_outbound_controller.rb
+++ app/controllers/api/v1/twilio_outbound_controller.rb
@@ -10,7 +10,7 @@ module Api
       skip_before_action :validate_token, except: :generate_call_token
       skip_before_action :verify_authenticity_token, except: :generate_call_token
 
-      before_action :validate_twilio_request, except: :generate_call_token
+      # before_action :validate_twilio_request, except: :generate_call_token
       after_filter :set_header, except: :generate_call_token
 
       resource_description do
