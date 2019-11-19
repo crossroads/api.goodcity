7db709d69896486e82b5d3e5048672607e90b26b
Abhinav Garg <abhinav.g@kiprosh.com>
2019-11-19 14:42:01 +0530

    code climate fixes

--- app/models/concerns/subform_utilities.rb
+++ app/models/concerns/subform_utilities.rb
@@ -8,8 +8,8 @@ module SubformUtilities
   module ClassMethods
     def distinct_by_column(column)
       raise StandardError.new("Column name invalid!") unless column_names.include?(column)
-      tbl_name = self.name.tableize
-      sql_select = self.send(
+      tbl_name = name.tableize
+      sql_select = send(
         :sanitize_sql, ["DISTINCT ON (#{tbl_name}.#{column}) #{tbl_name}.*"]
       )
       select(sql_select)
