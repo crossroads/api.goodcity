fbed1795cddf015d61f418b3d42b3504154e2336
Steve Kenworthy <steveyken@gmail.com>
2019-11-23 11:27:41 +0800

    Reflow the class structure for compare base.

--- lib/goodcity/compare/base.rb
+++ lib/goodcity/compare/base.rb
@@ -1,14 +1,13 @@
 require 'csv'
-require 'goodcity/compare/goodcity_table_loader'
-require 'goodcity/compare/stockit_table_loader'
+require 'pg'
 
 module Goodcity
   class Compare
 
     class Base
 
-      def initialize(options = {})
-        @table = options[:table]
+      def initialize
+        @table = self.class.name.demodulize.underscore
         @differences = {}
         @counters = {}
         @missing_in_goodcity = []
@@ -25,6 +24,10 @@ module Goodcity
         write_differences_to_file
         write_missing_file("stockit")
         write_missing_file("goodcity")
+      end
+
+      def self.run
+        new.run
       end
 
       private
@@ -66,32 +69,14 @@ module Goodcity
         @counters.each{|header, count| puts "#{header}: #{count}"}
       end
 
-      # {"stockit_id"=>"341248", "created_at"=>"2019-11-16 03:41:01.477177+00", "updated_at"=>"2019-11-19 02:56:50.329426+00",
-      #  "inventory_number"=>"12345", "received_quantity"=>"1", "location"=>"32Incoming", "code"=>"S1234",
-      #  "name"=>"Org name", "first_name"=>"Contact first name", "last_name"=>"Contact last name"}
-      def stockit_data
-        bm("Running Stockit #{@table} SQL") do
-          raw_data = Goodcity::Compare::StockitTableLoader.load("items")
-          Hash[raw_data.map{|h| [h["stockit_id"], h] }]
-        end
-      end
-
-      # Transform data into a hash keyed by stockit_id
-      # row1 and row2 are hashes
-      # { "123245" => row1, "1232457" => row2 }
-      def goodcity_data
-        bm("Running GoodCity #{@table} SQL") do
-          raw_data = Goodcity::Compare::GoodcityTableLoader.load(@table)
-          Hash[raw_data.map{|h| [h["stockit_id"], h] }]
-        end
-      end
-
       # Return the result of any differences between 2 hashes
+      #   Treat nil and '' as equal
       #  a = {"width" => 1}; b = {"width" => 2}
       #  returns { "width" => [1, 2] }
       def diff(a,b)
-        a.merge(b) { |k, v1, v2| v1 == v2 ? :equal : [v1, v2] }.
-          reject { |_, v| v == :equal }
+        a.merge(b) do |k, v1, v2|
+          (v1 || '') == (v2 || '') ? :equal : [v1, v2]
+        end.reject { |_, v| v == :equal }
       end
 
       def bm(label = '', &block)
@@ -146,9 +131,50 @@ module Goodcity
         puts "Wrote #{file_name}"
       end
 
+      # Transform data into a hash keyed by stockit_id
+      # row1 and row2 are hashes
+      # { "123245" => row1, "1232457" => row2 }
+      def goodcity_data
+        bm("Running GoodCity #{@table} SQL") do
+          raw_data = ActiveRecord::Base.connection.execute(goodcity_sql)
+          Hash[raw_data.map{|h| [h["stockit_id"], h] }]
+        end
+      end
+
+      # executes 'stockit_sql' method and returns an enumerator where each row is a Hash
+      # {"stockit_id"=>"341248", "created_at"=>"2019-11-16 03:41:01.477177+00", "updated_at"=>"2019-11-19 02:56:50.329426+00",
+      #  "inventory_number"=>"12345", "received_quantity"=>"1", "location"=>"32Incoming", "code"=>"S1234",
+      #  "name"=>"Org name", "first_name"=>"Contact first name", "last_name"=>"Contact last name"}
+      def stockit_data
+        bm("Running Stockit #{@table} SQL") do
+          db_params = YAML.load_file("#{Rails.root}/config/database.yml")["stockit"]
+          raise_missing_database_params_error if db_params.nil?
+          conn = PG.connect(db_params.symbolize_keys!)
+          raw_data = conn.exec(stockit_sql)
+          Hash[raw_data.map{|h| [h["stockit_id"], h] }]
+        end
+      end
+
+      def raise_missing_database_params_error
+        raise MissingDatabaseParamsError.new(
+          <<-MSG
+          Please provide the following 'stockit' database connection params in database.yml:
+            stockit:
+              host:
+              dbname: 
+              user: 
+              password: 
+              sslmode: require
+          MSG
+        )
+      end
+
     end
 
     class UnmatchedFieldsError < Exception
+    end
+
+    class MissingDatabaseParamsError < Exception
     end
 
   end
