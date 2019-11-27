a370889b5f41aea17034723376c7b3f5d5654b85
AbulAsar Sayyad <abul.a@kiprosh.com>
2019-11-27 15:38:47 +0530

    delete detail stockit callback added

--- app/models/electrical.rb
+++ app/models/electrical.rb
@@ -7,11 +7,11 @@ class Electrical < ActiveRecord::Base
   belongs_to :voltage, class_name: 'Lookup', required: false
   belongs_to :frequency, class_name: "Lookup", required: false
   has_one :package, as: :detail
+  after_destroy :delete_on_stockit
   before_save :set_tested_on, if: :test_status_id_changed?
   before_save :downcase_brand, if: :brand_changed?
   before_save :save_correct_country, if: :request_from_stockit?
   before_save :set_updated_by
-  # after-destroy :delete_on_stockit
   after_commit :create_on_stockit, on: :create
   after_update :update_on_stockit
 end
