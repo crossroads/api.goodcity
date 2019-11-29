8d9b1e5da9724290885131dcf64313a99cdf47aa
namrataukirde <namrata.ukirde@kiprosh.com>
2019-11-29 18:14:43 +0530

    move existing logic to service

--- app/services/print_label.rb
+++ app/services/print_label.rb
@@ -2,20 +2,21 @@ class PrintLabel
   attr_accessor :printer, :label
 
   def initialize(printer, label_file)
-    @printer = printer
-    @label_file   = label
+    @printer    = printer
+    @label_file = label
   end
 
   def print
-    print_id, errors, status = Open3.capture3(options, Rails.root.join("app", "services", "barcode_service.exp").to_s)
+    print_id, errors, status = Open3.capture3(print_options,
+      Rails.root.join("app", "services", "barcode_service.exp").to_s)
 
-    log_hash = { printer_name: "\"#{options["NAME"]}\"",
-            printer_host: "\"#{options["HOST"]}\"",
-            printer_user: "\"#{options["USER"]}\"",
-            # print_job_id: "\"#{print_id}\"",
+    log_hash = { printer_name: "\"#{print_options["NAME"]}\"",
+            printer_host: "\"#{print_options["HOST"]}\"",
+            printer_user: "\"#{print_options["USER"]}\"",
             print_job_errors: "\"#{errors}\"",
             print_job_status: "\"#{status}\"",
             class: self.class.name }
+
     Rails.logger.info(log_hash)
 
     label_file.delete
@@ -26,12 +27,11 @@ class PrintLabel
   private
 
   def print_options
-    barcode = Rails.application.secrets.barcode
     {
-      "NAME" => barcode["printer_name"],
-      "HOST" => barcode["printer_host"],
-      "USER" => barcode["printer_user"],
-      "PWD" => barcode["printer_pwd"],
+      "NAME" => printer.name,
+      "HOST" => printer.host,
+      "USER" => printer.username,
+      "PWD" => printer.password,
       "FILE" => @label_file.path,
     }
   end
