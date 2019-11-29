46726d9a85f3ab97bdcbe1a278088e0b43cd637c
namrataukirde <namrata.ukirde@kiprosh.com>
2019-11-29 18:14:43 +0530

    move existing logic to service

--- app/services/label/base_label.rb
+++ app/services/label/base_label.rb
@@ -5,37 +5,14 @@ module Label
   class BaseLabel
     attr_accessor :dots, :width, :field_orientation, :height, :file
 
-    def initialize(dots, width, field_orientation, height)
-      @dots              = dots
-      @width             = width
-      @field_orientation = field_orientation
-      @height            = height
-    end
-
-    def initialize_label
-      Easyzpl::Label.new(dots: dots, width: label_width, field_orientation: label_fields_orientation,
-        height: label_height)
+    def initialize(label)
+      @label = label
     end
 
     def tmp_label_file(label)
       @file = Tempfile.new("cupsjob")
       @file.write label.to_s
       @file.close
-    end
-
-    private
-
-    def log_details(errors, status)
-      log_hash = {
-        printer_name: "\"#{print_options["NAME"]}\"",
-        printer_host: "\"#{print_options["HOST"]}\"",
-        printer_user: "\"#{print_options["USER"]}\"",
-        # print_job_id: "\"#{print_id}\"",
-        print_job_errors: "\"#{errors}\"",
-        print_job_status: "\"#{status}\"",
-        class: self.class.name
-      }
-      Rails.logger.info(log_hash)
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -654,6 +654,7 @@ ActiveRecord::Schema.define(version: 20191105094920) do
     t.string   "case_number"
     t.boolean  "allow_web_publish"
     t.integer  "received_quantity"
+    t.boolean  "last_allow_web_published"
     t.integer  "weight"
     t.integer  "pieces"
     t.integer  "detail_id"
@@ -710,6 +711,18 @@ ActiveRecord::Schema.define(version: 20191105094920) do
     t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "printers", force: :cascade do |t|
+    t.boolean  "active"
+    t.integer  "location_id"
+    t.string   "name"
+    t.string   "host"
+    t.string   "port"
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "process_checklists", force: :cascade do |t|
@@ -901,7 +914,7 @@ ActiveRecord::Schema.define(version: 20191105094920) do
     t.datetime "created_at"
   end
 
-  add_index "versions", ["created_at", "whodunnit"], name: "partial_index_recent_locations", where: "(((event)::text = ANY (ARRAY[('create'::character varying)::text, ('update'::character varying)::text])) AND (object_changes ? 'location_id'::text))", using: :btree
+  add_index "versions", ["created_at", "whodunnit"], name: "partial_index_recent_locations", where: "(((event)::text = ANY ((ARRAY['create'::character varying, 'update'::character varying])::text[])) AND (object_changes ? 'location_id'::text))", using: :btree
   add_index "versions", ["created_at"], name: "index_versions_on_created_at", using: :btree
   add_index "versions", ["event"], name: "index_versions_on_event", using: :btree
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
