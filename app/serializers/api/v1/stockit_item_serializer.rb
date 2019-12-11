c5f2e32b726c84f0fb9545ae67b8b829ab2b64ca
Abhinav Garg <abhinav.g@kiprosh.com>
2019-12-11 14:36:28 +0530

    create storage type serializer

--- app/serializers/api/v1/stockit_item_serializer.rb
+++ app/serializers/api/v1/stockit_item_serializer.rb
@@ -9,6 +9,7 @@ module Api::V1
     has_one :set_item, serializer: Api::V1::StockitSetItemSerializer, include_items: false
     has_many :images, serializer: StockitImageSerializer
     has_many :orders_packages, serializer: OrdersPackageSerializer
+    has_one :storage_type, serializer: StorageTypeSerializer
 
     attributes :id, :quantity, :length, :width, :height, :weight, :pieces, :notes,
       :inventory_number, :created_at, :updated_at, :item_id, :is_set, :grade,
