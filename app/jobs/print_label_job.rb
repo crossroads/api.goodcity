aff885bc46239cf1c1b899ab4d4b0ad1cf6437ab
Steve Kenworthy <steveyken@gmail.com>
2019-12-14 09:47:16 +0800

    Printer refactoring
    Move file creating out of label and into PrintLabel control class.
    
    Use sockets to print rather than files.
    
    Removed unnecessary file

--- app/jobs/print_label_job.rb
+++ app/jobs/print_label_job.rb
@@ -1,8 +1,9 @@
 class PrintLabelJob < ActiveJob::Base
   def perform(package_id, current_user_id, label_type, print_count)
-    package     = Package.find(package_id)
-    label       = "Label::#{label_type.classify}".safe_constantize.new(
-      package.inventory_number, print_count).label_to_print
-    PrintLabel.new(label, current_user_id).print
+    package = Package.find(package_id)
+    options = { inventory_number: package.inventory_number, print_count: print_count }
+    label = "Label::#{label_type.classify}".safe_constantize.new(options)
+    printer = User.find_by_id(current_user_id).try(:printer)
+    PrintLabel.new(printer, label).print
   end
 end
--- app/services/print_label.rb
+++ app/services/print_label.rb
@@ -1,39 +1,18 @@
-class PrintLabel
-  attr_accessor :printer, :label_file
+require 'socket'
 
-  def initialize(label_file, current_user_id)
-    @printer     = User.find_by_id(current_user_id).try(:printer)
-    @label_file  = label_file
+class PrintLabel
+  attr_accessor :printer, :label
+
+  def initialize(printer, label)
+    @printer = printer
+    @label = label
   end
 
   def print
-    print_id, errors, status = Open3.capture3(print_options, Rails.root.join("app", "services", "barcode_service.exp").to_s)
-
-    log_hash = {
-      printer_name: print_options['NAME'],
-      printer_host: print_options['HOST'],
-      printer_user: print_options['USER'],
-      print_job_errors: errors,
-      print_job_status: status,
-      class: self.class.name
-    }
-
-    Rails.logger.info(log_hash)
-
-    label_file.delete
-
-    return print_id, errors, status
+    Socket.tcp(@printer.host, @printer.port) do |sock|
+      sock.print @label.to_s 
+      sock.close_write
+    end
   end
 
-  private
-
-  def print_options
-    {
-      "NAME" => printer.name,
-      "HOST" => printer.host,
-      "USER" => printer.username,
-      "PWD" => printer.password,
-      "FILE" => label_file.path
-    }
-  end
 end
