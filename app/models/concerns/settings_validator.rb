5eab4077acfc81a437870aaf7ab9bd8212ca5fca
Abhinav Garg <abhinav.g@kiprosh.com>
2019-12-20 18:52:15 +0530

    fix minor logic error

--- app/models/concerns/settings_validator.rb
+++ app/models/concerns/settings_validator.rb
@@ -5,13 +5,17 @@ class SettingsValidator < ActiveModel::Validator
   end
 
   def validate(record)
-    error_message = ""
+    error_messages = []
     @keys.each do |key|
-      error_message_key = "activerecord.errors.models.#{record.class.name.underscore}.#{key}"
-      error_message += " #{I18n.t(error_message_key)}" unless action_allowed?(key)
+      unless action_allowed?(key)
+        error_message_key = "activerecord.errors.models.#{record.class.name.underscore}.#{key}"
+        error_messages << I18n.t(error_message_key)
+      end
     end
-    record.errors.add(:base, error_message)
+    record.errors.add(:base, error_messages.join(" ")) if error_messages.length > 0
   end
+
+  private
 
   def action_allowed?(key)
     GoodcitySetting.where("key ILIKE ?", "%#{key}%").last&.value&.eql?("true")
