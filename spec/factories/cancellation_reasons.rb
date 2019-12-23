3445da115ac8f9d4241ec483d04a7aceeca876ec
AbulAsar Sayyad <abul.a@kiprosh.com>
2019-12-23 20:26:24 +0530

    fix failed specs

--- spec/factories/cancellation_reasons.rb
+++ spec/factories/cancellation_reasons.rb
@@ -2,16 +2,16 @@ FactoryBot.define do
   factory :cancellation_reason do
     name_en         { generate(:cancellation_reasons).keys.sample }
     name_zh_tw      { generate(:cancellation_reasons)[name_en][:name_zh_tw] }
-    visible_to_admin { generate(:cancellation_reasons)[name_en][:visible_to_admin] }
+    visible_to_offer { generate(:cancellation_reasons)[name_en][:visible_to_offer] }
     # initialize_with { CancellationReason.find_or_initialize_by(name_en: name_en) }
 
     trait :visible do
-      visible_to_admin true
+      visible_to_offer true
     end
 
     trait :invisible do
       name_en "Unwanted"
-      visible_to_admin false
+      visible_to_offer false
     end
   end
 end
