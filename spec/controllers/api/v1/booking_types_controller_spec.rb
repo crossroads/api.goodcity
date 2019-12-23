479ffcbb620d8b0ee0d9d5e3f906696552852baf
AbulAsar Sayyad <abul.a@kiprosh.com>
2019-12-23 13:14:56 +0530

    fixed failing booking_type specs

--- spec/controllers/api/v1/booking_types_controller_spec.rb
+++ spec/controllers/api/v1/booking_types_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Api::V1::BookingTypesController, type: :controller do
   let(:booking_type) { create(:booking_type) }
   let(:parsed_body) { JSON.parse(response.body) }
 
-  describe "GET booking_type" do   
+  describe "GET booking_type" do
     before { generate_and_set_token(reviewer) }
 
     it "returns 200", :show_in_doc do
@@ -20,7 +20,7 @@ RSpec.describe Api::V1::BookingTypesController, type: :controller do
     it "return serialized booking_type" do
       booking_type
       get :index
-      expect(parsed_body['booking_types'].length).to eq(1)
+      expect(parsed_body['booking_types'].length).to eq(2)
       expect(parsed_body['booking_types'][0]['name_en']).to eq(booking_type.name_en)
     end
   end
