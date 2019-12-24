11fa8437a4ada4aa5059500e63d0d1aa3ae5b19e
AbulAsar Sayyad <abul.a@kiprosh.com>
2019-12-24 20:22:44 +0530

    returned orders cancellation_reasons in index

--- app/controllers/api/v1/cancellation_reasons_controller.rb
+++ app/controllers/api/v1/cancellation_reasons_controller.rb
@@ -6,8 +6,10 @@ module Api
       def index
         if params["ids"].present?
           @cancellation_reasons = @cancellation_reasons.where(id: params["ids"].split(",").flatten)
+        elsif params["isStock"]
+          @cancellation_reasons = @cancellation_reasons.visible_to_order
         else
-          @cancellation_reasons = @cancellation_reasons.visible
+          @cancellation_reasons = @cancellation_reasons.visible_to_offer
         end
         render json: @cancellation_reasons, each_serializer: serializer
       end
