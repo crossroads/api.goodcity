6c66dcb4c5c287c3052ae7a12d591119b3388430
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2020-01-08 10:31:36 +0800

    Fix randomly failing test

--- spec/controllers/api/v1/booking_types_controller_spec.rb
+++ spec/controllers/api/v1/booking_types_controller_spec.rb
@@ -2,26 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::BookingTypesController, type: :controller do
   let(:reviewer) { create(:user, :reviewer) }
-  let(:booking_type) { create(:booking_type) }
   let(:parsed_body) { JSON.parse(response.body) }
+
+  before do
+    generate(:booking_types).keys.each do |name_en|
+      create(:booking_type, name_en: name_en)
+    end
+  end
 
   describe "GET booking_type" do
     before { generate_and_set_token(reviewer) }
 
     it "returns 200", :show_in_doc do
-      # Generate all booking_types options for listing in API docs
-      generate(:booking_types).keys.each do |name_en|
-        create(:booking_type, name_en: name_en)
-      end
       get :index
       expect(response.status).to eq(200)
     end
 
     it "return serialized booking_type" do
-      booking_type
       get :index
-      expect(parsed_body['booking_types'].length).to eq(1)
-      expect(parsed_body['booking_types'][0]['name_en']).to eq(booking_type.name_en)
+      expect(parsed_body['booking_types'].length).to eq(2)
+      expect(
+        parsed_body['booking_types'].map { |bt| bt['identifier'] }
+      ).to contain_exactly(*BookingType.all.pluck(:identifier))
     end
   end
 end
