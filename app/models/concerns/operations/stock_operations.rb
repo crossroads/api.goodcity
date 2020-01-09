a5b6531c8c55f46c01e19d12cc4fc90ad838789a
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2020-01-09 14:25:22 +0800

    Fix tests failing due to randomized factory fields

--- app/models/concerns/operations/stock_operations.rb
+++ app/models/concerns/operations/stock_operations.rb
@@ -13,13 +13,16 @@ module StockOperations
     # @param [Location|Id] from the location to negate the quantity from(or its id)
     #
     def register_loss(package, quantity:, from_location:)
-      designated_count = PackagesInventory::Computer.designated_quantity_of(package)
       available_count = PackagesInventory::Computer.available_quantity_of(package)
 
-      if (available_count - quantity < designated_count)
-        raise MissingQuantityRequiredError.new(
-          OrdersPackage.designated.where(package: package).map(&:order).uniq
-        )
+      if (quantity > available_count)
+        designated_count = PackagesInventory::Computer.designated_quantity_of(package)
+        if designated_count.positive?
+          orders = package.orders_packages.designated.map(&:order).uniq
+          raise Goodcity::QuantityDesignatedError.new(orders)
+        else
+          raise Goodcity::InsufficientQuantityError.new(quantity)
+        end
       end
 
       PackagesInventory.append_loss(
@@ -27,17 +30,6 @@ module StockOperations
         quantity: quantity.abs * -1,
         location: from_location
       )
-    end
-
-    # --- Exceptions
-
-    class OperationsError < StandardError; end
-
-    class MissingQuantityRequiredError < OperationsError
-      def initialize(orders)
-        order_text = orders.count == 1 ? orders.first.code : "#{orders.count}x"
-        super(I18n.t('operations.mark_lost.required_for_orders', orders: order_text))
-      end
     end
   end
 end
