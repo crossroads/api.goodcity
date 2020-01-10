d3678e2c171152c3b8ce0adfcba51d62f235f0f0
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2020-01-10 10:09:51 +0800

    migration fix + typo on '&&'

--- db/migrate/20200107034249_add_dispatched_quantity_to_orders_packages.rb
+++ db/migrate/20200107034249_add_dispatched_quantity_to_orders_packages.rb
@@ -1,5 +1,15 @@
 class AddDispatchedQuantityToOrdersPackages < ActiveRecord::Migration
   def change
     add_column :orders_packages, :dispatched_quantity, :integer, default: 0
+
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          UPDATE orders_packages
+            SET dispatched_quantity=quantity
+            WHERE state='dispatched'
+        SQL
+      end
+    end
   end
 end
