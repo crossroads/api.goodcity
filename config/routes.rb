922a09a1d67f530202b933542c69f4442d317e13
Abhinav Garg <abhinav.g@kiprosh.com>
2020-01-14 18:03:01 +0530

    create endpoint for fetching records

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,7 @@ Rails.application.routes.draw do
 
       resources :packages, only: [:index, :show, :create, :update, :destroy] do
         get :print_inventory_label, on: :member
+        get :fetch_associated_packages, on: :member
         put :move, on: :member
         put :designate, on: :member
         put :perform_action, on: :member
