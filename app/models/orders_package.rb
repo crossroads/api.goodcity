9405bfa194cbb00930f0cc745c94915575902458
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-01-15 13:41:07 +0530

    removed item root for package association

--- app/models/orders_package.rb
+++ app/models/orders_package.rb
@@ -26,7 +26,7 @@ class OrdersPackage < ActiveRecord::Base
   scope :get_dispatched_records_with_order_id, ->(order_id) { where(order_id: order_id, state: 'dispatched') }
   scope :designated, ->{ where(state: 'designated') }
   scope :dispatched, ->{ where(state: 'dispatched') }
-  scope :for_order, ->(order_id) { joins(:order).where(orders: {id: order_id}) }
+  scope :for_order, ->(order_id) { joins(:order).where(orders: { id: order_id }) }
 
   scope :with_eager_load, ->{
     includes([
