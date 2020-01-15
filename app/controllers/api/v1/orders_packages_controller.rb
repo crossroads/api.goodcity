becba53b44acad958055b2160ad1def47e0f8d64
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-01-15 13:41:07 +0530

    removed item root for package association

--- app/controllers/api/v1/orders_packages_controller.rb
+++ app/controllers/api/v1/orders_packages_controller.rb
@@ -89,7 +89,8 @@ module Api
       def orders_package_by_order_id
         records = @orders_packages.for_order(params["order_id"])
         orders_packages = records.page(params["page"]).per(params["per_page"])
-        render json: ActiveModel::ArraySerializer.new(orders_packages,
+        render json: ActiveModel::ArraySerializer.new(
+          orders_packages,
           each_serializer: serializer,
           root: "orders_packages",
           include_package: true,
