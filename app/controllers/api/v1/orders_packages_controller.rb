d269e8a2e16cc08e42b93df9d7496c3e151a1e43
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-01-20 18:42:50 +0530

    added specs for meta tag in orders show action

--- app/controllers/api/v1/orders_packages_controller.rb
+++ app/controllers/api/v1/orders_packages_controller.rb
@@ -85,7 +85,7 @@ module Api
       def orders_package_by_order_id
         orders_packages = @orders_packages.with_eager_load.for_order(params["order_id"])
         @orders_packages = orders_packages.page(page).per(per_page)
-        render json: {meta: {orders_packages_count: orders_packages.size}}.merge(serialized_orders_packages)
+        render json: { meta: { orders_packages_count: orders_packages.size } }.merge(serialized_orders_packages)
       end
 
       def orders_packages_params
