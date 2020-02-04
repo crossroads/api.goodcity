d8a25cd055bda396c602b06f479c3b59a475594b
Abhinav Garg <abhinav.g@kiprosh.com>
2020-02-04 17:15:36 +0530

    remove unwanted attributes from the stock_item_serializer

--- app/serializers/api/v1/stockit_item_serializer.rb
+++ app/serializers/api/v1/stockit_item_serializer.rb
@@ -16,7 +16,7 @@ module Api::V1
       :designation_name, :designation_id, :sent_on, :code_id, :image_id,
       :donor_condition_id, :set_item_id, :has_box_pallet, :case_number,
       :allow_web_publish, :received_quantity, :detail_type, :detail_id, :storage_type_id,
-      :in_hand_quantity, :added_quantity
+      :added_quantity
 
     def include_images?
       @options[:include_images]
@@ -32,26 +32,13 @@ module Api::V1
 
     alias_method :include_designation_id?, :include_order?
 
-    def include_in_hand_quantity?
-      @options[:include_in_hand_quantity]
-    end
-
     def include_added_quantity?
       @options[:include_added_quantity]
     end
 
-    def in_hand_quantity
-      sql = "select sum(quantity) from packages_inventories where package_id = #{object.id}"
-      object.run_sql(sql)
-    end
-
-    def in_hand_quantity__sql
-      "select sum(quantity) from packages_inventories where package_id = #{object.id}"
-    end
-
     def added_quantity
-      sql = "select sum(quantity) from packages_inventories where package_id = #{object.id} and packages_inventories.action in ('pack', 'unpack')"
-      object.run_sql(sql)
+      sql = added_quantity__sql
+      object.abs_quantity(sql)
     end
 
     def added_quantity__sql
