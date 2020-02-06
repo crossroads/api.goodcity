28adb6d1b832c8a1cb5ae5447a953acba6cae289
Abhinav Garg <abhinav.g@kiprosh.com>
2020-02-06 15:25:38 +0530

    add errors for various scenarios

--- app/models/concerns/operations/stock_operations.rb
+++ app/models/concerns/operations/stock_operations.rb
@@ -86,6 +86,9 @@ module StockOperations
       end
 
       def pack
+        return error(I18n.t("box_pallet.errors.adding_box_to_box")) if adding_box_to_a_box?
+        return error(I18n.t("box_pallet.errors.item_designated")) if item_designated?
+        return error(I18n.t("box_pallet.errors.invalid_quantity")) if invalid_quantity?
         pkg_inventory = pack_or_unpack(PackagesInventory::Actions::PACK)
         response(pkg_inventory)
       end
@@ -93,16 +96,6 @@ module StockOperations
       def unpack
         pkg_inventory = pack_or_unpack(PackagesInventory::Actions::UNPACK)
         response(pkg_inventory)
-      end
-
-      def response(pkg_inventory)
-        return unless pkg_inventory
-
-        if pkg_inventory.save
-          { packages_inventory: pkg_inventory, success: true }
-        else pkg_inventory.errors
-          { errors: pkg_inventory.errors.full_messages, success: false }
-        end
       end
 
       private
@@ -121,6 +114,31 @@ module StockOperations
 
       def quantity(task)
         task.eql?("pack") ? @quantity * -1 : @quantity
+      end
+
+      def error(error)
+        { errors: [error], success: false }
+      end
+
+      def response(pkg_inventory)
+        return unless pkg_inventory
+        if pkg_inventory.save
+          { packages_inventory: pkg_inventory, success: true }
+        else pkg_inventory.errors
+          error(pkg_inventory.errors.full_messages)
+        end
+      end
+
+      def invalid_quantity?
+        @quantity > @item.quantity
+      end
+
+      def adding_box_to_a_box?
+        @item.is_box? && @cause.is_box?
+      end
+
+      def item_designated?
+        @item.order.presence
       end
     end
 
