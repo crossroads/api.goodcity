e5e6cb582765a1e61f87c42f030cb6c4834fb0ce
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-02-18 17:02:11 +0530

    added negative specs for offers_package delete action

--- spec/controllers/api/v1/offers_packages_controller_spec.rb
+++ spec/controllers/api/v1/offers_packages_controller_spec.rb
@@ -2,15 +2,28 @@ require "rails_helper"
 
 RSpec.describe Api::V1::OffersPackagesController, type: :controller do
   let(:supervisor) { create(:user, :supervisor, :with_can_manage_offers_packages_permission )}
+  let(:reviewer) { create(:user, :reviewer)}
   let(:offers_package) {create :offers_package}
 
-  describe "DELETE offers_package/1 " do
-    before { generate_and_set_token(supervisor) }
+  describe "DELETE offers_package/1" do
 
-    it "returns 200", :show_in_doc do
-      delete :destroy, id: offers_package.id
-      expect(response.status).to eq(200)
-      expect(JSON.parse(response.body)).to eq( {} )
+    context 'With Supervisor role' do
+      before { generate_and_set_token(supervisor) }
+
+      it "returns 200", :show_in_doc do
+        delete :destroy, id: offers_package.id
+        expect(response.status).to eq(200)
+        expect(JSON.parse(response.body)).to eq( {} )
+      end
+    end
+
+    context 'With Reviewer role' do
+      before { generate_and_set_token(reviewer) }
+
+      it "returns 403", :show_in_doc do
+        delete :destroy, id: offers_package.id
+        expect(response.status).to eq(403)
+      end
     end
   end
 end
