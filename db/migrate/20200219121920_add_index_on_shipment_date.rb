0a6f412cb9b9640e2a04d85b254abb2d6434541a
namrataukirde <namrata.ukirde@kiprosh.com>
2020-02-20 16:18:39 +0530

    refactoring for code climate array of words issue

--- db/migrate/20200219121920_add_index_on_shipment_date.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexOnShipmentDate < ActiveRecord::Migration
-  def change
-    add_index :orders, :shipment_date
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -485,9 +485,9 @@ ActiveRecord::Schema.define(version: 20200226111634) do
     t.text     "cancel_reason"
     t.integer  "booking_type_id"
     t.string   "staff_note",              default: ""
-    t.integer  "cancellation_reason_id"
     t.boolean  "continuous",              default: false
     t.date     "shipment_date"
+    t.integer  "cancellation_reason_id"
   end
 
   add_index "orders", ["address_id"], name: "index_orders_on_address_id", using: :btree
@@ -618,9 +618,9 @@ ActiveRecord::Schema.define(version: 20200226111634) do
     t.integer  "stockit_id"
     t.integer  "location_id"
     t.boolean  "allow_requests",     default: true
-    t.boolean  "allow_stock",        default: false
     t.boolean  "allow_pieces",       default: false
     t.string   "subform"
+    t.boolean  "allow_stock",        default: false
     t.boolean  "allow_box",          default: false
     t.boolean  "allow_pallet",       default: false
   end
@@ -668,6 +668,7 @@ ActiveRecord::Schema.define(version: 20200226111634) do
     t.string   "case_number"
     t.boolean  "allow_web_publish"
     t.integer  "received_quantity"
+    t.boolean  "last_allow_web_published"
     t.integer  "weight"
     t.integer  "pieces"
     t.integer  "detail_id"
@@ -971,7 +972,7 @@ ActiveRecord::Schema.define(version: 20200226111634) do
     t.datetime "created_at"
   end
 
-  add_index "versions", ["created_at", "whodunnit"], name: "partial_index_recent_locations", where: "(((event)::text = ANY (ARRAY[('create'::character varying)::text, ('update'::character varying)::text])) AND (object_changes ? 'location_id'::text))", using: :btree
+  add_index "versions", ["created_at", "whodunnit"], name: "partial_index_recent_locations", where: "(((event)::text = ANY ((ARRAY['create'::character varying, 'update'::character varying])::text[])) AND (object_changes ? 'location_id'::text))", using: :btree
   add_index "versions", ["created_at"], name: "index_versions_on_created_at", using: :btree
   add_index "versions", ["event"], name: "index_versions_on_event", using: :btree
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
