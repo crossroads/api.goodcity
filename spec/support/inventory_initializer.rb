bf1a8057dc98c53d6cf54afeb807b7688a253a2e
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2020-02-25 15:36:56 +0800

    Fix initialize_inventory

--- spec/support/inventory_initializer.rb
+++ spec/support/inventory_initializer.rb
@@ -1,12 +1,14 @@
 module InventoryInitializer
   def initialize_inventory(*packages, location: nil)
-    location ||= pkg.locations.first || FactoryBot.create(:location)
     [packages].flatten.each do |pkg|
       return if PackagesInventory.where(package: pkg).count.positive? # Already initialized
-      create :packages_inventory, package: pkg, quantity: pkg.received_quantity, action: 'inventory', location: location
+
+      dest_location = location || pkg.locations.first || FactoryBot.create(:location)
+
+      create :packages_inventory, package: pkg, quantity: pkg.received_quantity, action: 'inventory', location: dest_location
 
       pkg.orders_packages.dispatched.each do |orders_package|
-        create :packages_inventory, package: pkg, quantity: - orders_package.quantity, action: 'dispatch', location: location
+        create :packages_inventory, package: pkg, quantity: - orders_package.quantity, action: 'dispatch', location: dest_location
       end
 
       pkg.requested_packages.each(&:update_availability!)
