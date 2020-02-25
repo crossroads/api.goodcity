414b5181f330bdcf9af4be8564d3e6271c62f62b
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2020-02-25 15:13:56 +0800

    Cannot cancel an order with dispatched orders_packages

--- features/step_definitions/order_state_transitions_steps.rb
+++ features/step_definitions/order_state_transitions_steps.rb
@@ -2,14 +2,18 @@ Given(/^I have an order of state "([^"]*)"/) do |state|
   @order = create :order, state: state
 end
 
-And(/^It has a orders_package of state "([^"]*)"/) do |state|
+And(/^It has an orders_package of state "([^"]*)"/) do |state|
   package = create :package, received_quantity: 5
   create :packages_inventory, package: package, quantity: package.received_quantity, action: 'inventory'
 
   @orders_package = create :orders_package, order: @order, package_id: package.id, state: state
 end
 
-Then(/Dispatching the orders_package should "([^"]*)"/) do |result|
+When(/^I dispatch the orders_package/) do
+  OrdersPackage::Operations.dispatch(@orders_package, quantity: @orders_package.quantity, from_location: @orders_package.package.locations.first)
+end
+
+Then(/^Dispatching the orders_package should "([^"]*)"/) do |result|
   expect_fail     = (result == "fail")
 
   expect {
@@ -18,8 +22,25 @@ Then(/Dispatching the orders_package should "([^"]*)"/) do |result|
       quantity:       @orders_package.package.received_quantity,
     )
   }.to (
-    expect_fail ?
-      raise_error(Goodcity::OperationsError)
-      : change(PackagesInventory, :count).by(1)
+    expect_fail ? raise_error(Goodcity::OperationsError) : change(PackagesInventory, :count).by(1)
+  )
+end
+
+Then(/^The order transitions to the "([^"]*)" state/) do |expected_state|
+  expect(@order.reload.state).to eq(expected_state)
+end
+
+Then(/^Applying the "([^"]*)" transition to the order "([^"]*)"/) do |transition, expected_result|
+  should_fail = expected_result.match(/^fail/)
+
+  current_state   = @order.state
+  expected_state  = transition
+
+  expect {
+    @order.fire_state_event(transition)
+  }.to (
+    should_fail ?
+      raise_error(Goodcity::InvalidStateError) :
+      change { @order.reload.state }.from(current_state).to(expected_state)
   )
 end
--- spec/support/inventory_initializer.rb
+++ spec/support/inventory_initializer.rb
@@ -1,11 +1,12 @@
 module InventoryInitializer
-  def initialize_inventory(*packages)
+  def initialize_inventory(*packages, location: nil)
+    location ||= pkg.locations.first || FactoryBot.create(:location)
     [packages].flatten.each do |pkg|
       return if PackagesInventory.where(package: pkg).count.positive? # Already initialized
-      create :packages_inventory, package: pkg, quantity: pkg.received_quantity, action: 'inventory'
+      create :packages_inventory, package: pkg, quantity: pkg.received_quantity, action: 'inventory', location: location
 
       pkg.orders_packages.dispatched.each do |orders_package|
-        create :packages_inventory, package: pkg, quantity: - orders_package.quantity, action: 'dispatch', location: pkg.locations.first
+        create :packages_inventory, package: pkg, quantity: - orders_package.quantity, action: 'dispatch', location: location
       end
 
       pkg.requested_packages.each(&:update_availability!)
