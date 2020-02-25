9993c1554c3737b51b8e2f3ec86862debfcb0c78
Abhinav Garg <abhinav.g@kiprosh.com>
2020-02-25 15:21:04 +0530

    define methods in the model for calculating available quantity

--- app/models/package.rb
+++ app/models/package.rb
@@ -171,6 +171,10 @@ class Package < ActiveRecord::Base
     PackagesInventory::Computer.package_quantity(self)
   end
 
+  def total_available_quantity
+    PackagesInventory::Computer.available_quantity_of(self)
+  end
+
   def total_quantity_in_box
     box_or_pallet? ? PackagesInventory::Computer.total_quantity_in_box(self) : nil
   end
