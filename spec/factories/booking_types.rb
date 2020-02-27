7135ef393ca497d5e8a1515b619d2db57ca503f8
namrataukirde <namrata.ukirde@kiprosh.com>
2020-02-27 14:23:52 +0530

    GCW-3051 fix factories initialization for booking_type

--- spec/factories/booking_types.rb
+++ spec/factories/booking_types.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :booking_type do
     identifier { generate(:booking_types).keys.sample }
-    name_en    { generate(:booking_types)[:name_en] }
-    name_zh_tw { generate(:booking_types)[:name_zh_tw] }
+    name_en    { generate(:booking_types)[identifier][:name_en] }
+    name_zh_tw { generate(:booking_types)[identifier][:name_zh_tw] }
     initialize_with { BookingType.find_or_initialize_by(identifier: identifier) }
   end
 end
