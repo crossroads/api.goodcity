069492ab9f75b0df4264d7f922fbb731055e1ade
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-03-05 18:57:11 +0530

    added received_at in offer-company serializer

--- spec/serializers/api/v1/offer_company_serializer_spec.rb
+++ spec/serializers/api/v1/offer_company_serializer_spec.rb
@@ -12,6 +12,7 @@ context Api::V1::OfferCompanySerializer do
     expect(subject['offer']['id']).to eq(offer.id)
     expect(subject['offer']['company_id']).to eq(offer.company.id)
     expect(subject['offer']['created_by_id']).to eq(offer.created_by.id)
+    expect(subject['offer']['received_at']).to eq(offer.received_at)
   end
 
   it "should only have the user and companies associations" do
