a6230709da8bd7dff16a38c609d7aeac3f488990
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-03-05 18:57:11 +0530

    added received_at in offer-company serializer

--- app/serializers/api/v1/offer_company_serializer.rb
+++ app/serializers/api/v1/offer_company_serializer.rb
@@ -2,7 +2,7 @@ module Api::V1
   class OfferCompanySerializer < ApplicationSerializer
     embed :ids, include: true
 
-    attributes :id, :company_id, :created_by_id
+    attributes :id, :company_id, :created_by_id, :received_at
 
     has_one  :created_by, serializer: UserSummarySerializer, root: :user
     has_one  :company, serializer: CompanySerializer
