9b5023214fb0fb9613f21daffea3f4873f0a621f
bharat619 <bharat.h@kiprosh.com>
2020-04-06 15:03:21 +0530

    GCW-3107 remove prettier effect

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,19 +1,16 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
   TOTAL_REQUESTS_STATES = ["submitted", "awaiting_dispatch", "closed", "cancelled"]
-  let(:user) { create(:user_with_token, :with_can_read_or_modify_user_permission, role_name: "Reviewer") }
+  let(:user) { create(:user_with_token, :with_can_read_or_modify_user_permission, role_name: 'Reviewer') }
   let(:serialized_user) { Api::V1::UserSerializer.new(user) }
-  let(:serialized_user_json) { JSON.parse(serialized_user.to_json) }
+  let(:serialized_user_json) { JSON.parse( serialized_user.to_json ) }
 
   let(:users) { create_list(:user, 2) }
 
-  let(:charity_users) {
-    ("a".."z").map { |i|
-      create(:user, :with_multiple_roles_and_permissions,
-             roles_and_permissions: {"Charity" => ["can_login_to_browse"]}, first_name: "Jane_#{i}", last_name: "Doe")
-    }
-  }
+  let(:charity_users) { ('a'..'z').map { |i|
+    create(:user, :with_multiple_roles_and_permissions,
+    roles_and_permissions: { 'Charity' => ['can_login_to_browse']}, first_name: "Jane_#{i}", last_name: 'Doe')}}
 
   let(:parsed_body) { JSON.parse(response.body) }
 
@@ -36,7 +33,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     it "return serialized users" do
       get :index
       body = JSON.parse(response.body)
-      expect(body["users"].length).to eq(User.count)
+      expect( body['users'].length ).to eq(User.count)
     end
   end
 
@@ -46,18 +43,18 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     it "returns searched user according to params" do
       get :index, searchText: charity_users.first.first_name, role_name: "Charity"
       expect(response.status).to eq(200)
-      expect(parsed_body["users"].count).to eq(1)
+      expect(parsed_body['users'].count).to eq(1)
     end
 
     it "returns only first 25 results" do
       get :index, searchText: charity_users.first.last_name, role_name: "Charity"
       expect(response.status).to eq(200)
-      expect(parsed_body["users"].count).to eq(25)
+      expect(parsed_body['users'].count).to eq(25)
     end
 
     it "will not return any user if params does not matches any users" do
       get :index, searchText: "zzzzzz", role_name: "Charity"
-      expect(parsed_body["users"].count).to eq(0)
+      expect(parsed_body['users'].count).to eq(0)
     end
 
     it "will return charity user if params has role as 'Charity'" do
@@ -68,13 +65,13 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     it "does not return searched user if the specified role is different" do
       get :index, searchText: charity_users.first.first_name, role_name: "Supervisor"
       expect(response.status).to eq(200)
-      expect(parsed_body["users"].count).to eq(0)
+      expect(parsed_body['users'].count).to eq(0)
     end
 
     it "returns searched user if role isn't specified" do
       get :index, searchText: charity_users.first.first_name
       expect(response.status).to eq(200)
-      expect(parsed_body["users"].count).to eq(1)
+      expect(parsed_body['users'].count).to eq(1)
     end
   end
 
@@ -93,11 +90,11 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       it "creates user and returns 201", :show_in_doc do
         expect {
           post :create, user: @user_params
-        }.to change(User, :count).by(1)
+          }.to change(User, :count).by(1)
         expect(response.status).to eq(201)
-        expect(parsed_body["user"]["first_name"]).to eql(@user_params[:first_name])
-        expect(parsed_body["user"]["last_name"]).to eql(@user_params[:last_name])
-        expect(parsed_body["user"]["mobile"]).to eql(@user_params[:mobile])
+        expect(parsed_body['user']['first_name']).to eql(@user_params[:first_name])
+        expect(parsed_body['user']['last_name']).to eql(@user_params[:last_name])
+        expect(parsed_body['user']['mobile']).to eql(@user_params[:mobile])
       end
     end
 
@@ -106,17 +103,17 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       it "returns 422 and doesn't create a user if error" do
         expect {
           post :create, user: @user_params2
-        }.to_not change(User, :count)
+          }.to_not change(User, :count)
         expect(response.status).to eq(422)
-        expect(parsed_body["errors"]).to eql([{"message" => "Mobile has already been taken", "status" => 422}])
+        expect(parsed_body['errors']).to eql([{"message" => "Mobile has already been taken", "status" => 422}])
       end
 
       it "returns 422 and doesn't create a user if error" do
         expect {
           post :create, user: @user_params3
-        }.to_not change(User, :count)
+          }.to_not change(User, :count)
         expect(response.status).to eq(422)
-        expect(parsed_body["errors"]).to eql([{"message" => "Mobile is invalid", "status" => 422}])
+        expect(parsed_body['errors']).to eql([{"message" => "Mobile is invalid", "status" => 422}])
       end
     end
   end
@@ -128,49 +125,49 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     context "Reviewer" do
       before { generate_and_set_token(user) }
       it "update user last_connected time", :show_in_doc do
-        put :update, id: user.id, user: {last_connected: 5.days.ago.to_s, user_role_ids: [role.id]}
+        put :update, id: user.id, user: { last_connected: 5.days.ago.to_s, user_role_ids: [role.id] }
         expect(response.status).to eq(200)
         expect(user.reload.last_connected.to_date).to eq(5.days.ago.to_date)
       end
 
       it "update user last_disconnected time", :show_in_doc do
-        put :update, id: user.id, user: {last_disconnected: 3.days.ago.to_s, user_role_ids: [role.id]}
+        put :update, id: user.id, user: { last_disconnected: 3.days.ago.to_s, user_role_ids: [role.id] }
         expect(response.status).to eq(200)
         expect(user.reload.last_disconnected.to_date).to eq(3.days.ago.to_date)
       end
 
       it "adds user role", :show_in_doc do
-        expect {
-          put :update, id: reviewer.id, user: {user_role_ids: [role.id]}
+        expect{
+          put :update, id: reviewer.id, user: { user_role_ids: [role.id] }
         }.to change(UserRole, :count).by(1)
         expect(response.status).to eq(200)
         expect(reviewer.reload.roles).to include(role)
       end
 
       it "removes user role if existing role_id is not present in params", :show_in_doc do
-        put :update, id: reviewer.id, user: {user_role_ids: []}
+        put :update, id: reviewer.id, user: { user_role_ids: [] }
         expect(reviewer.reload.roles.count).to eq(0)
       end
 
       it "removes user roles if user_role_ids parameter is nil" do
-        put :update, id: reviewer.id, user: {user_role_ids: []}
+        put :update, id: reviewer.id, user: { user_role_ids: [] }
         expect(reviewer.reload.roles.count).to eq(0)
       end
 
       it "do not removes user roles if user_role_ids parameter is not present in request" do
-        put :update, id: reviewer.id, user: {first_name: "abc"}
+        put :update, id: reviewer.id, user: { first_name: "abc" }
         expect(reviewer.reload.roles.count).to eq(1)
       end
 
       it "adds new roles and removes old roles as per params", :show_in_doc do
         existing_user_roles = reviewer.roles
-        put :update, id: reviewer.id, user: {user_role_ids: [role.id]}
+        put :update, id: reviewer.id, user: { user_role_ids: [role.id] }
         expect(reviewer.reload.roles).to include(role)
         expect(reviewer.reload.roles).not_to include(existing_user_roles)
       end
     end
 
-    describe "Users Order Count " do
+    describe 'Users Order Count ' do
       before { generate_and_set_token(user) }
       TOTAL_REQUESTS_STATES.each do |state|
         let!(:"#{state}_order_user") { create :order, :with_orders_packages, :"with_state_#{state}", created_by_id: user.id }
@@ -181,26 +178,27 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         expect(response.status).to eq(200)
       end
 
-      it "returns each orders count for user" do
+      it 'returns each orders count for user' do
         get :orders_count, id: user.id
         expect(response.status).to eq(200)
-        expect(parsed_body["submitted"]).to eq(1)
-        expect(parsed_body["awaiting_dispatch"]).to eq(1)
-        expect(parsed_body["closed"]).to eq(1)
-        expect(parsed_body["cancelled"]).to eq(1)
+        expect(parsed_body['submitted']).to eq(1)
+        expect(parsed_body['awaiting_dispatch']).to eq(1)
+        expect(parsed_body['closed']).to eq(1)
+        expect(parsed_body['cancelled']).to eq(1)
       end
     end
-  end
 
-  context "get /recent_users" do
-    context "if user has order adminstrator or supervisor role" do
-      it "allows to fetch the recent users" do
-        [:order_administrator, :supervisor].map do |role|
-          user = create(:user, role, :with_can_read_or_modify_user_permission)
-          generate_and_set_token(user)
-          expect(response.status).to eq(200)
+    context "get /recent_users" do
+      context "if user has order adminstrator or supervisor role" do
+        it "allows to fetch the recent users" do
+          [:order_administrator, :supervisor].map do |role|
+            user = create(:user, role, :with_can_read_or_modify_user_permission)
+            generate_and_set_token(user)
+            expect(response.status).to eq(200)
+          end
         end
       end
     end
   end
 end
+
