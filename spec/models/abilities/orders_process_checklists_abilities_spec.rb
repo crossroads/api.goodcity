15f4f847bfd4e5fd55acf757392377f366aebd88
swatijadhav <swati.nakshtra17@gmail.com>
2020-04-07 17:24:39 +0530

    GCW-3109 allow only supervisors and order-fulfilment rols users to access orders_process_checklists

---
+++ spec/models/abilities/orders_process_checklists_abilities_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+require "cancan/matchers"
+
+describe "OrdersProcessChecklist abilities" do
+  subject(:ability) { Ability.new(user) }
+
+  context "when Supervisor" do
+    let(:user) { create(:user, :supervisor, :with_can_access_orders_process_checklists) }
+    let(:orders_process_checklist) { create :orders_process_checklist }
+
+    it { is_expected.to be_able_to(:index, orders_process_checklist) }
+  end
+
+  context "when Reviewer" do
+    let(:user) { create(:user, :order_fulfilment, :with_can_access_orders_process_checklists) }
+    let(:orders_process_checklist) { create :orders_process_checklist }
+
+    it { is_expected.to be_able_to(:index, orders_process_checklist) }
+  end
+
+  context "when Anonymous" do
+    let(:user) { nil }
+    let(:orders_process_checklist) { create :orders_process_checklist }
+
+    it { is_expected.to_not be_able_to(:index, orders_process_checklist) }
+  end
+end
