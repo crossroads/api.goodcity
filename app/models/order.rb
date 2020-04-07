3615d91c57c925297496a7dbe83a1a61c001c374
shreyas098 <shreyash@kiprosh.com>
2020-04-07 11:11:30 +0530

    Fixed Code Climate issue

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,7 +13,6 @@ class Order < ActiveRecord::Base
     ]
   end
 
-
   belongs_to :cancellation_reason
   belongs_to :detail, polymorphic: true, dependent: :destroy
   belongs_to :stockit_activity
@@ -46,7 +45,7 @@ class Order < ActiveRecord::Base
   has_many :process_checklists, through: :orders_process_checklists
   has_many :orders_process_checklists, inverse_of: :order
 
-  validates :people_helped, numericality: {greater_than_or_equal_to: 1}, presence: true
+  validates :people_helped, numericality: { greater_than_or_equal_to: 1} , presence: true
 
   after_initialize :set_initial_state
   before_create :assign_code
