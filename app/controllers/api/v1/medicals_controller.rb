11d581feb667ae0cf9987ef34cd17850f997b44c
bharat619 <bharat.h@kiprosh.com>
2020-04-09 14:24:19 +0530

    Adding delete actions and updating specs

--- app/controllers/api/v1/medicals_controller.rb
+++ app/controllers/api/v1/medicals_controller.rb
@@ -2,6 +2,7 @@
 
 module Api
   module V1
+    # MedicalsController
     class MedicalsController < Api::V1::ApiController
       load_and_authorize_resource :medical, parent: false
 
@@ -23,22 +24,35 @@ module Api
         end
       end
 
+      api :GET, '/v1/medicals', 'Return all medical items'
       def index
-        @medicals = @medicals.distinct_by_column(params['distinct']) if params['distinct']
+        @medicals = distinct_by_column
         render json: @medicals, each_serializer: serializer
       end
 
+      api :SHOW, '/v1/medicals/1', 'Return a medical item record'
       def show
         render json: @medical, serializer: serializer, include_country: true
       end
 
+      api :UPDATE, '/v1/medicals/1', 'Updates a medical item record'
       param_group :medical
       def update
         @medical.assign_attributes(permitted_params)
         update_and_render_object_with_errors(@medical)
       end
 
+      api :DELETE, '/v1/medicals/1', 'Deletes a medical item record'
+      def destroy
+        @medical.destroy
+        render json: {}
+      end
+
       private
+
+      def distinct_by_column
+        @medicals.distinct_by_column(params['distinct']) if params['distinct']
+      end
 
       def permitted_params
         attributes = %i[
