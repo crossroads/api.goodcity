82eecb5e327468af46435b599c12e5976b1fc101
shreyas098 <shreyash@kiprosh.com>
2020-04-14 17:44:09 +0530

    Changed value from 0 to 1

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -58,7 +58,7 @@ RSpec.describe Order, type: :model do
   end
 
   describe "validations" do
-    it{ is_expected.to validate_numericality_of(:people_helped).is_greater_than_or_equal_to(1) }
+    it{ is_expected.to validate_numericality_of(:people_helped).is_greater_than_or_equal_to(0) }
    end
 
   describe 'Database columns' do
