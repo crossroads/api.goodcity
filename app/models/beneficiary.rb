6497e09b47568295636da4b81c01d7bffd86b549
shreyas098 <shreyash@kiprosh.com>
2020-04-16 14:59:20 +0530

    Added reuired validation and specs for the benficiary fields

--- app/models/beneficiary.rb
+++ app/models/beneficiary.rb
@@ -3,6 +3,8 @@ class Beneficiary < ActiveRecord::Base
   belongs_to :created_by, class_name: 'User'
   has_one :order
 
-  validates :first_name, length: { maximum: 50 }
-  validates :last_name, length: { maximum: 50 }
+  validates :identity_type_id, :identity_number, :title,presence: true
+  validates :first_name, length: { maximum: 50 }, presence: true
+  validates :last_name, length: { maximum: 50 }, presence: true
+  validates :phone_number, length: { is: 8 }, presence: true
 end
