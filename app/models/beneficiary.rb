a7d6b6e69e5e65c80e0658d93cd0b0f8f9ca488a
shreyas098 <shreyash@kiprosh.com>
2020-04-17 14:12:46 +0530

    Validation Changes

--- app/models/beneficiary.rb
+++ app/models/beneficiary.rb
@@ -3,8 +3,7 @@ class Beneficiary < ActiveRecord::Base
   belongs_to :created_by, class_name: 'User'
   has_one :order
 
-  validates :identity_type_id, :identity_number, :title, presence: true
+  validates :identity_type_id, :identity_number, :title, :phone_number, presence: true
   validates :first_name, length: { maximum: 50 }, presence: true
   validates :last_name, length: { maximum: 50 }, presence: true
-  validates :phone_number, length: { is: 12 }, presence: true
 end
