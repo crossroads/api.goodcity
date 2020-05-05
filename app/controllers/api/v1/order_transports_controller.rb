74a96e44935c17fdd8e16ffedec94eee2190b6bc
bharat619 <bharat.h@kiprosh.com>
2020-05-05 12:20:47 +0530

    GCW-2909 Fix able to update appointments after order cancellation

--- app/controllers/api/v1/order_transports_controller.rb
+++ app/controllers/api/v1/order_transports_controller.rb
@@ -8,6 +8,11 @@ module Api
       end
 
       def update
+        if @order_transport.order.state == 'cancelled'
+          return render json: { error: I18n.t('order.already_cancelled') },
+                        status: :unprocessable_entity
+        end
+
         @order_transport.assign_attributes(order_transport_params)
         save_and_render_object_with_errors(@order_transport)
       end
@@ -25,7 +30,7 @@ module Api
         params.require(:order_transport).permit(:order_id, :scheduled_at,
           :timeslot, :transport_type, :contact_id, :gogovan_order_id,
           :need_english, :need_cart, :need_carry, :need_over_6ft,
-          :gogovan_transport_id, :remove_net, 
+          :gogovan_transport_id, :remove_net,
           contact_attributes: [:name, :mobile, { address_attributes: [:district_id] }])
       end
 
---
+++ spec/controllers/api/v1/order_transport_controller_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::OrderTransportsController, type: :controller do
+  let(:charity_user) { create :user, :charity, :with_can_manage_orders_permission }
+  let(:order) { create :order, :with_state_submitted, created_by: charity_user }
+  let(:cancelled_order) { create(:order, :with_state_cancelled, created_by: charity_user) }
+  let(:order_transport) { create(:order_transport, order: order) }
+  let(:parsed_body) { JSON.parse(response.body) }
+
+  before do
+    generate_and_set_token(charity_user)
+  end
+
+  describe 'PUT order_transports/1' do
+    it 'updates the order_transport' do
+      put :update, id: order_transport.id,
+                   order_transport: { scheduled_at: '2020-05-11T16:30:00+08:00',
+                                      timeslot: '16:30', order_id: order.id }
+
+      expect(response).to have_http_status(:success)
+      expect(parsed_body['order_transport']['timeslot']).to eq('16:30')
+      expect(parsed_body['order_transport']['scheduled_at'].to_date).to eq('2020-05-11T16:30:00+08:00'.to_date)
+    end
+
+    context 'if an update is made on cancelled order' do
+      it 'performs no update operation for that order' do
+        order_transport = create(:order_transport, order: cancelled_order)
+        put :update, id: order_transport.id,
+             order_transport: {scheduled_at: "2020-05-11T16:30:00+08:00",
+                               timeslot: "16:30", order_id: cancelled_order.id}
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(parsed_body['error']).to eq(I18n.t('order.already_cancelled'))
+      end
+    end
+  end
+end
