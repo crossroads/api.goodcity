a2bfd184ce14c23bdaa6ee52d460c1738e46969d
bharat619 <bharat.h@kiprosh.com>
2020-05-07 12:29:03 +0530

    removing unused method

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -5,8 +5,8 @@ module Api
       before_action :eager_load_designation, only: :show
 
       resource_description do
-        short 'Retrieve a list of designations, information about stock items that have been designated to a group or person.'
-        formats ['json']
+        short "Retrieve a list of designations, information about stock items that have been designated to a group or person."
+        formats ["json"]
         error 401, "Unauthorized"
         error 404, "Not Found"
         error 422, "Validation Error"
@@ -23,14 +23,16 @@ module Api
           param :stockit_id, String, desc: "stockit designation record id", allow_nil: true
           param :beneficiary_id, String, allow_nil: true
           param :address_id, String
-          param :booking_type_id, String, desc: 'Booking type.(Online order or appointment)'
-          param :staff_note, String, desc: 'Notes for internal use'
+          param :booking_type_id, String, desc: "Booking type.(Online order or appointment)"
+          param :staff_note, String, desc: "Notes for internal use"
         end
       end
 
       api :POST, "/v1/orders", "Create or Update a order"
       param_group :order
+
       def create
+        root = is_browse_app? ? "order" : "designation"
         if order_record.save
           render json: @order, serializer: serializer, root: root, status: 201
         else
@@ -38,47 +40,51 @@ module Api
         end
       end
 
-      api :GET, '/v1/orders', "List all orders"
+      api :GET, "/v1/orders", "List all orders"
+
       def index
         return my_orders if is_browse_app?
-        return recent_designations if params['recently_used'].present?
+        return recent_designations if params["recently_used"].present?
         records = apply_filters(@orders)
-          .search(params['searchText'], params['toDesignateItem'].presence)
+          .search(params["searchText"], params["toDesignateItem"].presence)
           .page(params["page"]).per(params["per_page"] || DEFAULT_SEARCH_COUNT)
         orders = order_response(records)
-        render json: {meta: {total_pages: records.total_pages, search: params['searchText']}}.merge(orders)
+        render json: {meta: {total_pages: records.total_pages, search: params["searchText"]}}.merge(orders)
       end
 
-      api :GET, '/v1/designations/1', "Get a order"
+      api :GET, "/v1/designations/1", "Get a order"
+
       def show
+        root = is_browse_app? ? "order" : "designation"
         render json: serializer.new(@order,
-          root: root,
-          exclude_code_details: true,
-          include_packages: bool_param(:include_packages, true),
-          include_order: bool_param(:include_order, false),
-          include_territory: true,
-          include_images: true,
-          include_allowed_actions: true,
-          include_orders_packages: bool_param(:include_orders_packages, true),
-          exclude_stockit_set_item: true
-        )
+                                    root: root,
+                                    exclude_code_details: true,
+                                    include_packages: bool_param(:include_packages, true),
+                                    include_order: bool_param(:include_order, false),
+                                    include_territory: true,
+                                    include_images: true,
+                                    include_allowed_actions: true,
+                                    include_orders_packages: bool_param(:include_orders_packages, true),
+                                    exclude_stockit_set_item: true)
       end
 
       def transition
-        event = params['transition'].to_sym
+        event = params["transition"].to_sym
         @order.update_transition_and_reason(event, cancel_params) if @order.state_events.include?(event)
         render json: @order, serializer: serializer
       end
 
-      api :PUT, '/v1/orders/1', "Update an order"
+      api :PUT, "/v1/orders/1", "Update an order"
       param_group :order
+
       def update
+        root = is_browse_app? ? "order" : "designation"
         @order.assign_attributes(order_params)
         # use valid? to ensure submit event errors get caught
-        if @order.valid? && @order.save
+        if @order.valid? and @order.save
           render json: @order, root: root, serializer: serializer
         else
-          render json: { errors: @order.errors.full_messages } , status: 422
+          render json: {errors: @order.errors.full_messages}, status: 422
         end
       end
 
@@ -108,19 +114,19 @@ module Api
 
       def order_response(records)
         ActiveModel::ArraySerializer.new(records,
-          each_serializer: select_serializer,
-          root: "designations",
-          include_packages: true,
-          include_order: false,
-          include_images: true,
-          include_messages: bool_param(:include_messages, false),
-          exclude_stockit_set_item: true).as_json
+                                         each_serializer: select_serializer,
+                                         root: "designations",
+                                         include_packages: true,
+                                         include_order: false,
+                                         include_images: true,
+                                         include_messages: bool_param(:include_messages, false),
+                                         exclude_stockit_set_item: true).as_json
       end
 
       def cancel_params
         {
           cancel_reason: params["cancel_reason"],
-          cancellation_reason_id: params['cancellation_reason_id']
+          cancellation_reason_id: params["cancellation_reason_id"],
         }
       end
 
@@ -138,7 +144,7 @@ module Api
           @order.detail_type = "GoodCity"
         end
 
-        if order_params['beneficiary_attributes'] and @order.beneficiary.try(:created_by).nil?
+        if order_params["beneficiary_attributes"] and @order.beneficiary.try(:created_by).nil?
           # New nested beneficiary
           @order.beneficiary.created_by = current_user
         end
@@ -153,24 +159,23 @@ module Api
           priority: bool_param(:priority, false),
           with_notifications: params[:with_notifications],
           before: time_epoch_param(:before),
-          after: time_epoch_param(:after)
+          after: time_epoch_param(:after),
         )
       end
 
       def order_params
         params.require(:order).permit(:district_id,
-          :created_by_id, :stockit_id, :code, :status,
-          :created_at, :organisation_id, :stockit_contact_id,
-          :detail_id, :detail_type, :description,
-          :state, :cancellation_reason, :state_event,
-          :stockit_organisation_id, :stockit_activity_id,
-          :people_helped, :beneficiary_id, :booking_type_id, :purpose_description,
-          :address_id,:submitted_by_id, :staff_note,
-          purpose_ids: [], cart_package_ids: [],
-          beneficiary_attributes: beneficiary_attributes,
-          address_attributes: address_attributes,
-          orders_process_checklists_attributes: orders_process_checklists_attributes
-        )
+                                      :created_by_id, :stockit_id, :code, :status,
+                                      :created_at, :organisation_id, :stockit_contact_id,
+                                      :detail_id, :detail_type, :description,
+                                      :state, :cancellation_reason, :state_event,
+                                      :stockit_organisation_id, :stockit_activity_id,
+                                      :people_helped, :beneficiary_id, :booking_type_id, :purpose_description,
+                                      :address_id, :submitted_by_id, :staff_note,
+                                      purpose_ids: [], cart_package_ids: [],
+                                      beneficiary_attributes: beneficiary_attributes,
+                                      address_attributes: address_attributes,
+                                      orders_process_checklists_attributes: orders_process_checklists_attributes)
       end
 
       def address_attributes
@@ -194,7 +199,7 @@ module Api
       end
 
       def select_serializer
-        params[:shallow] == 'true' ? shallow_serializer : serializer
+        params[:shallow] == "true" ? shallow_serializer : serializer
       end
 
       def stockit_activity
@@ -215,10 +220,6 @@ module Api
 
       def eager_load_designation
         @order = Order.accessible_by(current_ability).with_eager_load.find(params[:id])
-      end
-
-      def root
-        is_browse_app? ? 'order' : 'designation'
       end
     end
   end
