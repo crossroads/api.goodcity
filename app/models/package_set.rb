bcac25d15ec84a2c189054973523b69d2a6c338a
Patrick R <patrick.rabier@gmail.com>
2020-05-19 14:48:15 +0800

    FIX: rake task

--- app/models/package_set.rb
+++ app/models/package_set.rb
@@ -12,7 +12,7 @@ class PackageSet < ActiveRecord::Base
   # Auto-destroy
   # ---------------------
 
-  watch [Package], on: [:update, :destroy] do |package, event|
+  watch [Package], on: [:update, :destroy] do |package|
     package_set_id = package.package_set_id_changed? ? package.package_set_id_was : package.package_set_id
 
     package_set = PackageSet.find_by(id: package_set_id)
