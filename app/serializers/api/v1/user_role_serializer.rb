76b3edefbd8c3f7a651d71e9b4124b0df2e417fe
swatijadhav <swati.nakshtra17@gmail.com>
2020-05-22 09:32:44 +0530

    GCW-3133 user serializer update

--- app/serializers/api/v1/user_role_serializer.rb
+++ app/serializers/api/v1/user_role_serializer.rb
@@ -2,8 +2,10 @@ module Api::V1
   class UserRoleSerializer < ApplicationSerializer
     embed :ids, include: true
 
-    attributes :id, :role_id
+    attributes :id, :role_id, :user_id
+
     has_one :user, serializer: UserSerializer
     has_one :role, serializer: RoleSerializer
+
   end
 end
