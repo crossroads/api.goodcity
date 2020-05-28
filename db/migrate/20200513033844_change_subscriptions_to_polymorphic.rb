605b5bf1d0cd67463989dd6a130669b21dd2a9c6
bharat619 <bharat.h@kiprosh.com>
2020-05-28 12:41:53 +0530

    removing comments

--- db/migrate/20200513033844_change_subscriptions_to_polymorphic.rb
+++ db/migrate/20200513033844_change_subscriptions_to_polymorphic.rb
@@ -1,39 +1,39 @@
 class ChangeSubscriptionsToPolymorphic < ActiveRecord::Migration
   def up
-    # add_column :subscriptions, :subscribable_type, :string
-    # add_column :subscriptions, :subscribable_id, :int
+    add_column :subscriptions, :subscribable_type, :string
+    add_column :subscriptions, :subscribable_id, :int
 
-    # Subscription.all.map do |subs|
-    #   if subs.offer_id
-    #     offer = subs.offer
-    #     subs.subscribable_type = offer.class.name
-    #     subs.subscribable_id = offer.id
-    #   elsif subs.order_id
-    #     order = subs.order
-    #     subs.subscribable_type = order.class.name
-    #     subs.subscribable_id = order.id
-    #   end
-    #   subs.save!(validate: false)
-    # end
+    Subscription.all.map do |subs|
+      if subs.offer_id
+        offer = subs.offer
+        subs.subscribable_type = offer.class.name
+        subs.subscribable_id = offer.id
+      elsif subs.order_id
+        order = subs.order
+        subs.subscribable_type = order.class.name
+        subs.subscribable_id = order.id
+      end
+      subs.save!(validate: false)
+    end
 
     remove_column :subscriptions, :offer_id
     remove_column :subscriptions, :order_id
   end
 
-  # def down
-  #   add_column :subscriptions, :offer_id, :int
-  #   add_column :subscriptions, :order_id, :int
+  def down
+    add_column :subscriptions, :offer_id, :int
+    add_column :subscriptions, :order_id, :int
 
-  #   Subscription.all.map do |subs|
-  #     case subs.subscribable_type
-  #     when 'Offer'
-  #       subs.offer_id = subs.subscribable_id
-  #     when 'Order'
-  #       subs.order_id = subs.subscribable_id
-  #     end
-  #   end
+    Subscription.all.map do |subs|
+      case subs.subscribable_type
+      when 'Offer'
+        subs.offer_id = subs.subscribable_id
+      when 'Order'
+        subs.order_id = subs.subscribable_id
+      end
+    end
 
-  #   remove_column :subscriptions, :subscribable_type
-  #   remove_column :subscriptions, :subscribable_id
-  # end
+    remove_column :subscriptions, :subscribable_type
+    remove_column :subscriptions, :subscribable_id
+  end
 end
