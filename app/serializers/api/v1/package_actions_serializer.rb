8d08bc69ceeab0420554f659a88dee43465c341d
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-07-06 16:49:10 +0530

    added versions in stockit-item serializer

--- app/serializers/api/v1/package_actions_serializer.rb
+++ app/serializers/api/v1/package_actions_serializer.rb
@@ -4,7 +4,6 @@ module Api::V1
     attributes :id, :source_id, :source_type, :description, :created_at,
       :location_id, :user_id, :quantity, :action, :item_id, :package_id
 
-    has_one :package, serializer: PackageSerializer, root: 'item'
     has_one :user, serializer: UserSerializer
 
     alias_method :item_id, :package_id
