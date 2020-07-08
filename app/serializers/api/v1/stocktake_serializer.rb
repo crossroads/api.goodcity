a73578fb7b21564cd8ade617fd0047bf9a8e56d7
Patrick R <patrick.rabier@gmail.com>
2020-07-08 16:14:51 +0800

    Add: created_by to stocktake serializer

--- app/serializers/api/v1/stocktake_serializer.rb
+++ app/serializers/api/v1/stocktake_serializer.rb
@@ -3,10 +3,11 @@ module Api
     class StocktakeSerializer < ApplicationSerializer
       embed :ids, include: true
 
-      has_many :stocktake_revisions, serializer: StocktakeRevisionSerializer
-      has_one :location, serializer: LocationSerializer
+      has_many  :stocktake_revisions, serializer: StocktakeRevisionSerializer
+      has_one   :location, serializer: LocationSerializer
+      has_one   :created_by, serializer: UserSerializer
       
-      attributes :id, :name, :location_id, :state
+      attributes :id, :name, :location_id, :state, :created_at:, :updated_at
     end
   end
 end
