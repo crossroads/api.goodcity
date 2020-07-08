e45862d815c1586b75ae4338588e666912c49938
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-07-08 11:21:30 +0530

    config.random added in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,9 @@ ActiveRecord::Migration.maintain_test_schema! if defined?(ActiveRecord::Migratio
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 
+  config.order = :random
+  Kernel.srand config.seed
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
