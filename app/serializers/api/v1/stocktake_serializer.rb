a7d55a924ca470504796cf6a5f0fee18de9c8c8e
Patrick R <patrick.rabier@gmail.com>
2020-07-09 12:30:29 +0800

    GCW-3209 Add comment to stocktake table

--- app/serializers/api/v1/stocktake_serializer.rb
+++ app/serializers/api/v1/stocktake_serializer.rb
@@ -5,7 +5,7 @@ module Api
 
       has_many  :stocktake_revisions, serializer: StocktakeRevisionSerializer
       has_one   :location, serializer: LocationSerializer
-      has_one   :created_by, serializer: UserSerializer, root: :user
+      has_one   :created_by, serializer: UserSerializer, root: :user, user_summary: true
       
       attributes :id, :name, :location_id, :created_by_id, :state, :created_at, :updated_at
     end
--- spec/models/stocktake_spec.rb
+++ spec/models/stocktake_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Stocktake, type: :model do
     it { is_expected.to have_db_column(:location_id).of_type(:integer) }
     it { is_expected.to have_db_column(:name).of_type(:string) }
     it { is_expected.to have_db_column(:state).of_type(:string) }
+    it { is_expected.to have_db_column(:comment).of_type(:string) }
   end
 
   describe 'Populating revisions' do
