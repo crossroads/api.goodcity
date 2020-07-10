73da6e2d10c7a52b974eaa472e5373cc645c29fb
shreyas098 <shreyash@kiprosh.com>
2020-07-10 16:18:32 +0530

    Code climate issue fixed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -300,7 +300,7 @@ class Ability
   end
 
   def requested_packages_abilities
-    can [:create, :destroy, :update, :index, :checkout], RequestedPackage, user_id: @user_id
+    can %i[create destroy update index checkout], RequestedPackage, user_id: @user_id
   end
 
   def package_abilities
