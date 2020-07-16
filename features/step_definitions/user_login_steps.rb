8c0328f7559882b3a78c62c1bcd2ec88af44ae90
bharat619 <bharat.h@kiprosh.com>
2020-07-16 15:21:56 +0530

    circle ci build fix

--- features/step_definitions/user_login_steps.rb
+++ features/step_definitions/user_login_steps.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
 Given(/^I am a ([supervisor | reviewer]+) logging into "([^"]*)" app/) do |name, app|
-  @user = create(:user, :with_multiple_roles_and_permissions, roles_and_permissions: {"#{name.humanize}" => ['can_login_to_admin']})
+  @user = create(:user, :with_multiple_roles_and_permissions, roles_and_permissions: { name.humanize => ['can_login_to_admin'] })
   header 'X-GOODCITY-APP-NAME', "#{app}.goodcity"
 end
 
 Given(/^I am a ([order_administrator | order_fulfilment]+) logging into "([^"]*)" app/) do |name, app|
-  @user = create(:user, :with_multiple_roles_and_permissions, roles_and_permissions: {"#{name.humanize}" => ['can_login_to_stock']})
+  @user = create(:user, :with_multiple_roles_and_permissions, roles_and_permissions: { name.humanize => ['can_login_to_stock'] })
   header 'X-GOODCITY-APP-NAME', "#{app}.goodcity"
 end
 
 And('I enter the mobile number') do
-  request('api/v1/auth/send_pin', { method: :post, params: { mobile: @user.mobile } })
+  request('api/v1/auth/send_pin', method: :post, params: { mobile: @user.mobile } )
   @response = last_response
 end
 
 And('I enter the pin') do
   otp = @user.most_recent_token.otp_code
   otp_auth_key = parsed_body['otp_auth_key']
-  request('api/v1/auth/verify', { method: :post, params: { otp_auth_key: otp_auth_key, pin: otp } })
+  request('api/v1/auth/verify', method: :post, params: { otp_auth_key: otp_auth_key, pin: otp } )
   @response = last_response
 end
 
