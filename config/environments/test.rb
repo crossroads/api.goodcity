eed0e3ad726c48079d2e761a1caace8ea8bec737
bharat619 <bharat.h@kiprosh.com>
2020-07-17 09:12:49 +0530

    Reverting test/env.rb changes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,5 +36,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
-  config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
 end
