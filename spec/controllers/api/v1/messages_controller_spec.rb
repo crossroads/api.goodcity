50801acde10b62811f9b14bce9dd3635aa303307
bharat619 <bharat.h@kiprosh.com>
2020-07-28 11:13:07 +0530

    Fix the random failing of specs in circleci

--- spec/controllers/api/v1/messages_controller_spec.rb
+++ spec/controllers/api/v1/messages_controller_spec.rb
@@ -69,15 +69,19 @@ RSpec.describe Api::V1::MessagesController, type: :controller do
       end
 
       it "for one order" do
-        3.times { create :message, messageable: order }
-        3.times { create :message, messageable: order2 }
+        3.times { create :subscription, state: 'unread', subscribable: order, message: (create :message, messageable: order) }
+
+        3.times { create :subscription, state: 'unread', subscribable: order2, message: (create :message, messageable: order2) }
+
         get :index, order_id: order.id
         expect(subject['messages'].length).to eq(3)
       end
 
       it "for multiple orders" do
-        3.times { create :message, messageable: order }
-        3.times { create :message, messageable: order2 }
+        3.times { create :subscription, state: 'unread', subscribable: order, message: (create :message, messageable: order) }
+
+        3.times { create :subscription, state: 'unread', subscribable: order2, message: (create :message, messageable: order2) }
+
         get :index, order_id: "#{order.id},#{order2.id}"
         expect(subject['messages'].length).to eq(6)
       end
