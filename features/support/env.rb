58be0d9332615dc17acdcbda7165754a54111ca3
bharat619 <bharat.h@kiprosh.com>
2020-07-30 12:39:29 +0530

    Add webmock to cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,8 @@
 # files.
 
 require 'cucumber/rails'
+require "webmock/cucumber"
+WebMock.disable_net_connect!(:allow_localhost => true)
 
 # frozen_string_literal: true
 
@@ -71,4 +73,3 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
-
