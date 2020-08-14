9441ea845c593951f3cbc9a2e5eacc381e8059dc
Steve Kenworthy <steveyken@gmail.com>
2020-08-14 11:37:24 +0800

    Convert log output to json format.

--- app/jobs/twilio_job.rb
+++ app/jobs/twilio_job.rb
@@ -7,6 +7,7 @@ class TwilioJob < ActiveJob::Base
       twilio_conf = Rails.application.secrets.twilio
       client = Twilio::REST::Client.new(twilio_conf['account_sid'], twilio_conf['auth_token'])
       client.messages.create({ from: twilio_conf['phone_number'] }.merge(options))
+      Rails.logger.info(class: self.class.name, msg: "SMS sent", mobile: options[:to], body: options[:body])
     end
   end
 end
