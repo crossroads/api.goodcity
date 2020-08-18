11ccfd977a3b52589a506d71173c85b062e111b8
swatijadhav <swati.nakshtra17@gmail.com>
2020-08-18 11:11:42 +0530

    GCW-3273 preferred_language for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,7 @@ class User < ActiveRecord::Base
   validates :email, fake_email: true, :if => lambda { Rails.env.production? }
 
   validates :title, :inclusion => {:in => TITLE_OPTIONS}, :allow_nil => true
+  validates :preferred_language, inclusion: { in: USER_LANGUAGES }, allow_nil: true
 
   after_create :generate_auth_token
 
