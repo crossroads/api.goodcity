1683113df50583ba24868573114151336170ef1f
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-08-19 19:30:05 +0530

    added reverse migration for remove_printer

--- db/migrate/20200819054009_remove_printer_id.rb
+++ db/migrate/20200819054009_remove_printer_id.rb
@@ -1,5 +1,14 @@
 class RemovePrinterId < ActiveRecord::Migration
-  def change
-    remove_column :users, :printer_id
+  def up
+    User.find_each do |user|
+      if user.printer_id
+        PrintersUser.create(user: user, printer_id: user.printer_id, tag: "stock")
+      end
+    end
+    remove_column :users, :printer_id, :integer
+  end
+
+  def down
+    add_column :users, :printer_id, :integer
   end
 end
