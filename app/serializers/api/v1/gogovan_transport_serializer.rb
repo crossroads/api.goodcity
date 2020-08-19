78611db59ac151ae25e4dfb2cb26c7a21f3d0532
bharat619 <bharat.h@kiprosh.com>
2020-08-19 16:12:52 +0530

    Corrects the effects of removing __sql

--- app/serializers/api/v1/gogovan_transport_serializer.rb
+++ app/serializers/api/v1/gogovan_transport_serializer.rb
@@ -1,6 +1,9 @@
 module Api::V1
   class GogovanTransportSerializer < ApplicationSerializer
-    attributes :id, :disabled
-    attribute "name_#{current_language}".to_sym
+    attributes :id, :disabled, :name
+
+    def name
+      object.try("name_#{current_language}".to_sym)
+    end
   end
 end
--- app/serializers/api/v1/package_category_serializer.rb
+++ app/serializers/api/v1/package_category_serializer.rb
@@ -1,8 +1,11 @@
 module Api::V1
   class PackageCategorySerializer < ApplicationSerializer
     # embed :ids, include: true
-    attributes :id, :parent_id, :package_type_codes
-    attribute "name_#{current_language}".to_sym
+    attributes :id, :parent_id, :package_type_codes, :name
+
+    def name
+      object.try("name_#{current_language}".to_sym)
+    end
 
     def package_type_codes
       object.package_types.pluck(:code).join(',')
