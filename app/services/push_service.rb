9f5dbd29fcfaca8c44e72f439f5dd453916b0cc1
Steve Kenworthy <steveyken@gmail.com>
2020-08-28 12:18:31 +0800

    Provide a way to lock push_updates and prevent them from being processed.

--- app/services/push_service.rb
+++ app/services/push_service.rb
@@ -1,5 +1,15 @@
 class PushService
+  @@locked = false
+
+  def self.locked
+    @@locked = true
+    yield
+    @@locked = false
+  end
+
   def send_update_store(channels, data)
+    return if @@locked
+
     channels = [channels].flatten.uniq
     payload = default_payload.merge(data)
     if channels.any?
@@ -8,6 +18,8 @@ class PushService
   end
 
   def send_notification(channels, app_name, data)
+    return if @@locked
+
     data[:message] = ActionView::Base.full_sanitizer.sanitize(data[:message])
     data[:date] = Time.now.to_json.tr('"','')
     channels = [channels].flatten.uniq
