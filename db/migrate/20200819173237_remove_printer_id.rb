a98dc6a3c60dfa1c988f29466dbc8a6dcabfff16
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-09-01 13:48:44 +0530

    moved moved printer creation to crate_printers migration

--- db/migrate/20200819173237_remove_printer_id.rb
+++ db/migrate/20200819173237_remove_printer_id.rb
@@ -1,11 +1,5 @@
 class RemovePrinterId < ActiveRecord::Migration
   def up
-    create_table :printers_users do |t|
-      t.integer :printer_id
-      t.integer :user_id
-      t.string  :tag
-    end
-    execute("INSERT INTO printers_users(printer_id, user_id, tag) SELECT printer_id, id, 'stock' FROM users WHERE users.printer_id IS NOT NULL")
     remove_column :users, :printer_id, :integer
   end
 
