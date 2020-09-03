f38328bb07e8f9e9979fff94c3b3d9aaa86b4688
bharat619 <bharat.h@kiprosh.com>
2020-09-03 16:47:33 +0530

    Fix specs

--- spec/controllers/api/v1/organisation_types_controller_spec.rb
+++ spec/controllers/api/v1/organisation_types_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::OrganisationTypesController, type: :controller do
-  let(:user) { create(:user, :with_order_administrator_role, :with_can_access_organisation_types_permission) }
+  let(:user) { create(:user, :with_order_administrator_role) }
   let(:response_body) { JSON.parse(response.body) }
   let!(:organisation_types) { create_list(:organisation_type, 3) }
 
@@ -17,18 +17,9 @@ RSpec.describe Api::V1::OrganisationTypesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-    context 'for invalid user' do
-      let(:user) { create(:user) }
-      it 'returns unauthorized' do
-        get :index
-        expect(response).to have_http_status(:forbidden)
-      end
-    end
-
     it 'returns the organisation_types' do
       get :index
       expect(response_body['organisation_types'].length).to eq(OrganisationType.count)
-      # expect(response_body['organisation_types'].to include(organisation_types.map { |o| {"name" => o.name_en, "id" => o.id } } )
       expect(response_body['organisation_types']).to match_array(organisation_types.map{|o| { 'name' => o.name_en, 'id' => o.id, 'category' => o.category_en } })
     end
   end
