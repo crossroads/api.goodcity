49fe203672e463b9da7bf02a2f946f95445438df
bharat619 <bharat.h@kiprosh.com>
2020-09-03 23:07:57 +0530

    Fix specs

--- spec/controllers/api/v1/gc_organisations_controller_spec.rb
+++ spec/controllers/api/v1/gc_organisations_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::GcOrganisationsController, type: :controller do
   let(:supervisor) { create(:user, :with_token, :with_can_check_organisations_permission, role_name: 'Supervisor') }
+  let!(:country) { create(:country, name_en: DEFAULT_COUNTRY) }
   let(:parsed_body) { JSON.parse(response.body) }
 
   before { generate_and_set_token(supervisor) }
