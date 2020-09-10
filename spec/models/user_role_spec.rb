2072417d51ddef3e9e6e6521b6516d2cfbf188e4
swatijadhav <swati.nakshtra17@gmail.com>
2020-09-10 12:22:08 +0530

    GCW-3283 updated specs for user-role updates

--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe UserRole, type: :model do
     it { is_expected.to have_db_column(:role_id).of_type(:integer) }
     it { is_expected.to have_db_column(:expiry_date).of_type(:datetime) }
   end
+
+  describe 'expiry_date time' do
+    it "should set expiry_date time as 8pm HKT" do
+      user_role = create :user_role, expiry_date: nil
+      expiry_date = DateTime.now.in_time_zone.days_since(10)
+
+      user_role.expiry_date = expiry_date
+      user_role.save
+
+      expect(user_role.expiry_date.hour).to eq(20)
+      expect(user_role.expiry_date.to_i).to eql(expiry_date.change(hour: 20).to_i)
+    end
+  end
 end
