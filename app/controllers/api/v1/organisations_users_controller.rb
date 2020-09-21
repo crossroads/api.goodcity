6b29b9dfb3deddeaf300fb6a253510bad80cca55
bharat619 <bharat.h@kiprosh.com>
2020-09-21 13:45:52 +0530

    Refactor

--- app/controllers/api/v1/organisations_users_controller.rb
+++ app/controllers/api/v1/organisations_users_controller.rb
@@ -1,9 +1,9 @@
 module Api
   module V1
     class OrganisationsUsersController < Api::V1::ApiController
-      authorize_resource :organisations_user, parent: false, except: [:status_list, :organisation_user]
+      authorize_resource :organisations_user, parent: false, except: [:status_list]
       load_resource :organisations_user, only: [:show, :update]
-      skip_authorization_check only: [:status_list, :organisation_user]
+      skip_authorization_check only: [:status_list]
 
       resource_description do
         short "Get Organisations Users."
