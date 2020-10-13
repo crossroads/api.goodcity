f4d8ea0a7e3c396e1344202902983a39a335e8bb
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-10-13 19:16:51 +0530

    add printer from printer_user

--- app/jobs/print_label_job.rb
+++ app/jobs/print_label_job.rb
@@ -1,9 +1,9 @@
 class PrintLabelJob < ActiveJob::Base
-  def perform(package_id, current_user_id, label_type, print_count)
+  def perform(package_id, current_user_id, opts={})
     package = Package.find(package_id)
-    options = { inventory_number: package.inventory_number, print_count: print_count }
-    label = "Labels::#{label_type.classify}".safe_constantize.new(options)
-    printer = User.find_by_id(current_user_id).try(:printers).first
+    options = { inventory_number: package.inventory_number, print_count: opts[:print_count] }
+    label = "Labels::#{opts[:label_type].classify}".safe_constantize.new(options)
+    printer = PrintersUser.where(user_id: current_user_id, tag: opts[:tag]).first.try(:printer)
     PrintLabel.new(printer, label).print
   end
 end
