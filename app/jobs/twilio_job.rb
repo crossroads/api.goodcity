58641ea8b7b687d086cf92836f7c4b2f0a6b8987
bharat619 <bharat.h@kiprosh.com>
2020-10-15 16:01:46 +0530

    :ambulance: Fix Reading from Twilio Environment

--- app/jobs/twilio_job.rb
+++ app/jobs/twilio_job.rb
@@ -5,8 +5,8 @@ class TwilioJob < ActiveJob::Base
   def perform(options)
     if options[:to].present?
       twilio_conf = Rails.application.secrets.twilio
-      client = Twilio::REST::Client.new(twilio_conf['account_sid'], twilio_conf['auth_token'])
-      client.messages.create({ from: twilio_conf['phone_number'] }.merge(options))
+      client = Twilio::REST::Client.new(twilio_conf[:account_sid], twilio_conf[:auth_token])
+      client.messages.create({ from: twilio_conf[:phone_number] }.merge(options))
       Rails.logger.info(class: self.class.name, msg: "SMS sent", mobile: options[:to], body: options[:body])
     end
   end
