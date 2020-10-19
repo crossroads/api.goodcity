543d69064f33055b822ecef144173bf1498bd7f6
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-10-19 17:45:20 +0530

    added check for without printer

--- app/jobs/print_label_job.rb
+++ app/jobs/print_label_job.rb
@@ -1,9 +1,9 @@
 class PrintLabelJob < ActiveJob::Base
-  def perform(package_id, current_user_id, opts={})
+  def perform(package_id, printer_id, opts={})
     package = Package.find(package_id)
     options = { inventory_number: package.inventory_number, print_count: opts[:print_count] }
     label = "Labels::#{opts[:label_type].classify}".safe_constantize.new(options)
-    printer = PrintersUser.where(user_id: current_user_id, tag: opts[:tag]).first.try(:printer)
+    printer = Printers.find(printer_id)
     PrintLabel.new(printer, label).print
   end
 end
