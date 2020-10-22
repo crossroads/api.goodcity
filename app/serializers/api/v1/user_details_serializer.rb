ab37f1c4415bc104b5ac3f29314fd23d0a365e00
bharat619 <bharat.h@kiprosh.com>
2020-10-22 12:58:15 +0530

    :wrench: Fix Serializer

--- app/serializers/api/v1/user_details_serializer.rb
+++ app/serializers/api/v1/user_details_serializer.rb
@@ -3,12 +3,18 @@ module Api::V1
     embed :ids, include: true
     attributes :id, :first_name, :last_name, :mobile, :title,
       :created_at, :last_connected, :last_disconnected, :email,
-      :is_email_verified, :is_mobile_verified, :disabled, :preferred_language
+      :is_email_verified, :is_mobile_verified, :disabled, :preferred_language,
+      :organisations_users_ids
 
     has_one :image, serializer: ImageSerializer
     has_one :address, serializer: AddressSerializer
     has_many :printers_users, serializer: PrintersUserSerializer
     has_many :user_roles, serializer: UserRoleSerializer
     has_many :organisations_users, serializer: OrganisationsUserSerializer
+
+
+    def organisations_users_ids
+      object.organisations_users.pluck(:id)
+    end
   end
 end
