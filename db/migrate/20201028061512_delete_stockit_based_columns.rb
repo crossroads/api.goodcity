ab59464646f4963a5017bc9e6c0a2d252aaf5a5c
swatijadhav <swati.nakshtra17@gmail.com>
2020-11-02 11:12:55 +0530

    GCW-3373 fixed stockit related columns

--- db/migrate/20201028061512_delete_stockit_based_columns.rb
+++ db/migrate/20201028061512_delete_stockit_based_columns.rb
@@ -8,20 +8,9 @@ class DeleteStockitBasedColumns < ActiveRecord::Migration[5.2]
     remove_column :medicals, :stockit_id, :integer
 
     remove_column :orders, :stockit_id, :integer
-    remove_column :orders, :stockit_contact_id, :integer
-    remove_column :orders, :stockit_organisation_id, :integer
-    remove_column :orders, :stockit_activity_id, :integer
-
     remove_column :package_types, :stockit_id, :integer
 
     remove_column :packages, :stockit_id, :integer
-    remove_column :packages, :stockit_sent_on, :datetime
-    remove_column :packages, :stockit_designated_on, :datetime
-    remove_column :packages, :stockit_designated_by_id, :integer
-    remove_column :packages, :stockit_sent_by_id, :integer
-    remove_column :packages, :stockit_moved_on, :datetime
-    remove_column :packages, :stockit_moved_by_id, :integer
-
     remove_column :pallets, :stockit_id, :integer
   end
 end
