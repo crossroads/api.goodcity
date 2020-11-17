0c2d5f0b9791861e2ffd94783ea5a126dec29c47
bharat619 <bharat.h@kiprosh.com>
2020-11-17 16:54:30 +0530

    :globe_with_meridians: Handle internationalization

--- app/serializers/api/v1/canned_response_serializer.rb
+++ app/serializers/api/v1/canned_response_serializer.rb
@@ -2,7 +2,15 @@ module Api
   module V1
     class CannedResponseSerializer < ApplicationSerializer
       embed :ids, include: true
-      attributes :id, :name_en, :name_zh_tw, :content_en, :content_zh_tw
+      attributes :id, :content_en, :content_zh_tw, :content, :name
+
+      def name
+        object.try("name_#{current_language}".to_sym)
+      end
+
+      def content
+        object.try("content_#{current_language}".to_sym)
+      end
     end
   end
 end
