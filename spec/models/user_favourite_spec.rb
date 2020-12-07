b151592192bcc4615941274fed2d5273948a1077
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-12-07 19:31:49 +0530

    added spec for user_favourite model

--- spec/models/user_favourite_spec.rb
+++ spec/models/user_favourite_spec.rb
@@ -13,4 +13,34 @@ RSpec.describe UserFavourite, type: :model do
     it { is_expected.to have_db_column(:favourite_id).of_type(:integer) }
     it { is_expected.to have_db_column(:persistent).of_type(:boolean) }
   end
+
+  describe '#add_user_favourite' do
+
+    before { 
+      Package.auto_favourite_relations ['package_type']
+      User.current_user = user 
+    }
+
+    it 'creates user_favourites record for model' do
+      package_type = create :package_type 
+      package = create :package, package_type: package_type
+      expect(UserFavourite.count).to eq(2)
+      expect(UserFavourite.pluck(:favourite_id)).to include(package.id)
+    end
+
+    it 'creates user_favourites record for models relationships assigned to "auto_favourite_relations"' do
+      package_type = create :package_type 
+      package = create :package, package_type: package_type
+      expect(UserFavourite.count).to eq(2)
+      expect(UserFavourite.pluck(:favourite_id)).to include(package_type.id)
+    end
+
+    it 'does not create user_favourites record for models relationships assigned to "auto_favourite_relations"' do
+      location = create :location 
+      package_type = create :package_type 
+      package = create :package, package_type: package_type, location_id: location.id
+      expect(UserFavourite.count).to eq(2)
+      expect(UserFavourite.pluck(:favourite_id)).not_to include(location.id)
+    end
+  end
 end
