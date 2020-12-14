af42ca0a7e7cbc08aaa86a9aee3d9bb7da2eafde
bharat619 <bharat.h@kiprosh.com>
2020-12-14 13:27:01 +0530

    Adds documentation

--- app/controllers/api/v1/processing_destinations_lookups_controller.rb
+++ app/controllers/api/v1/processing_destinations_lookups_controller.rb
@@ -1,18 +1,30 @@
 # frozen_string_literal: true
+
 module Api
   module V1
     class ProcessingDestinationsLookupsController < Api::V1::ApiController
       load_and_authorize_resource :processing_destinations_lookup, parent: false
-    
-      def index
-        render json: @processing_destinations_lookups, each_serializer: serializer 
+
+      resource_description do
+        short 'List Processing Destination Items'
+        formats ['json']
+        error 401, 'Unauthorized'
+        error 404, 'Not Found'
+        error 422, 'Validation Error'
+        error 500, 'Internal Server Error'
       end
-    
+
+      api :GET, '/v1/processing_destinations_lookups', 'List all Processing Destination Lookups.'
+
+      def index
+        render json: @processing_destinations_lookups, each_serializer: serializer
+      end
+
       private
-    
+
       def serializer
         Api::V1::ProcessingDestinationsLookupSerializer
       end
-    end  
+    end
   end
 end
