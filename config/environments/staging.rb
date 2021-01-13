006ee04848acce3c5ad8761570c11224bdbfd677
bharat619 <bharat.h@kiprosh.com>
2021-01-13 14:57:10 +0530

    Add specs to test error capture

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
-    port: 587,
+    port: ENV['SMTP_PORT'],
     domain: ENV['SMTP_DOMAIN'],
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
--- spec/mailers/goodcity_mailer_spec.rb
+++ spec/mailers/goodcity_mailer_spec.rb
@@ -3,8 +3,9 @@ require "rails_helper"
 RSpec.describe GoodcityMailer, type: :mailer do
   let(:user) { create(:user, :charity) }
   let(:order) { create(:order, created_by: user) }
-  before(:each) do
-    I18n.locale = 'en'
+
+  after(:each) do
+    I18n.locale = :en
   end
 
   describe 'send_pin_email' do
@@ -14,6 +15,12 @@ RSpec.describe GoodcityMailer, type: :mailer do
       it "sets subject in #{locale} locale" do
         expect(mailer.subject).to eq(I18n.t('email.subject.login'))
       end
+    end
+
+    it 'handles any SMTP errors' do
+      allow_any_instance_of(ApplicationMailer).to receive(:capture_smtp_errors).with(Timeout::Error)
+      allow_any_instance_of(GoodcityMailer).to receive(:send_pin_email).and_raise(Timeout::Error)
+      GoodcityMailer.send_pin_email
     end
   end
 end
