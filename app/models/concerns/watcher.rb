ea84c547ce693408afc961300c9146898f5fcc07
Patrick R <patrick.rabier@gmail.com>
2021-01-13 10:04:35 +0800

    :zap: GCW-3444 ADD: Stocktake counter caches for gains/losses/counts/warnings

--- app/models/concerns/watcher.rb
+++ app/models/concerns/watcher.rb
@@ -1,12 +1,32 @@
 module Watcher
   extend ActiveSupport::Concern
 
-  class_methods do
-    def watch(model_klasses, on: [:create, :update, :destroy, :touch], timing: :after)
-      [model_klasses].flatten.uniq.each do |model_kass|
-        on.each do |event|
-          model_kass.set_callback event, timing, ->(rec) { yield(rec, event) }
+  included do
+    @@watcher_enabled = true
+
+    class << self
+      def watcher_enabled?
+        @@watcher_enabled 
+      end
+
+      def watch(model_klasses, on: [:create, :update, :destroy, :touch], timing: :after)
+        klass = self
+        [model_klasses].flatten.uniq.each do |model_kass|
+          on.each do |event|
+            model_kass.set_callback event, timing, ->(rec) {
+              if klass.watcher_enabled?
+                yield(rec, event)
+              end
+            }
+          end
         end
+      end
+
+      def watcher_off
+        @@watcher_enabled = false
+        yield
+      ensure
+        @@watcher_enabled = true
       end
     end
   end
--- spec/models/stocktake_spec.rb
+++ spec/models/stocktake_spec.rb
@@ -11,6 +11,85 @@ RSpec.describe Stocktake, type: :model do
     it { is_expected.to have_db_column(:name).of_type(:string) }
     it { is_expected.to have_db_column(:state).of_type(:string) }
     it { is_expected.to have_db_column(:comment).of_type(:string) }
+    it { is_expected.to have_db_column(:counts).of_type(:integer) }
+    it { is_expected.to have_db_column(:gains).of_type(:integer) }
+    it { is_expected.to have_db_column(:losses).of_type(:integer) }
+    it { is_expected.to have_db_column(:warnings).of_type(:integer) }
+  end
+
+  describe 'Counter caches' do
+    let(:location) { create(:location) }
+    let(:package) { create(:package, received_quantity: 5) }
+    let(:stocktake) { create(:stocktake, location: location) }
+  
+    before { initialize_inventory(package, location: location) }
+
+    before do
+      stocktake.compute_counters!
+    end
+
+    it "adds to the gains counter if a revision has a quantity greater than the packages on_hand_quantity" do
+      expect {
+        create :stocktake_revision, stocktake: stocktake, package: package, quantity: 6
+      }.to change(stocktake, :gains).from(0).to(1)
+
+      expect(stocktake.losses).to eq(0)
+      expect(stocktake.warnings).to eq(0)
+    end
+
+    it "adds to the counts counter if a revision has a quantity greater than the packages on_hand_quantity" do
+      expect {
+        create :stocktake_revision, stocktake: stocktake, package: package, quantity: 6
+      }.to change(stocktake, :counts).from(0).to(1)
+
+      expect(stocktake.losses).to eq(0)
+      expect(stocktake.warnings).to eq(0)
+    end
+
+    it "adds to the losses counter if a revision has a quantity lower than the packages on_hand_quantity" do
+      expect {
+        create :stocktake_revision, stocktake: stocktake, package: package, quantity: 4
+      }.to change(stocktake, :losses).from(0).to(1)
+
+      expect(stocktake.gains).to eq(0)
+      expect(stocktake.warnings).to eq(0)
+    end
+
+    it "adds to the counts counter if a revision has a quantity lower than the packages on_hand_quantity" do
+      expect {
+        create :stocktake_revision, stocktake: stocktake, package: package, quantity: 4
+      }.to change(stocktake, :counts).from(0).to(1)
+
+      expect(stocktake.gains).to eq(0)
+      expect(stocktake.warnings).to eq(0)
+    end
+
+    it "adds to the warning counter if the revision is dirty" do
+      expect {
+        create :stocktake_revision, stocktake: stocktake, package: package, quantity: 5, dirty: true
+      }.to change(stocktake, :warnings).from(0).to(1)
+    end
+
+    it "doesnt add to the counts if the revision is dirty" do
+      expect {
+        create :stocktake_revision, stocktake: stocktake, package: package, quantity: 5, dirty: true
+      }.not_to change(stocktake, :counts)
+    end
+
+    it "removes a count if it a revision is changed to be marked as dirty" do
+      revision = create(:stocktake_revision, stocktake: stocktake, package: package, quantity: 6)
+      expect(stocktake.reload.gains).to eq(1)
+      revision.update!(dirty: true)
+      expect(stocktake.reload.gains).to eq(0)
+    end
+
+    it "can be disabled by turning off the watcher module" do
+      expect {
+        Stocktake.watcher_off do
+          create :stocktake_revision, stocktake: stocktake, package: package, quantity: 6
+        end
+      }.not_to change(stocktake, :gains)
+    end
   end
 
   describe 'Populating revisions' do
@@ -37,6 +116,21 @@ RSpec.describe Stocktake, type: :model do
       expect(stocktake.revisions.map(&:dirty).uniq).to eq([true])
       expect(stocktake.revisions.map(&:quantity).uniq).to eq([0])
       expect(stocktake.revisions.map(&:state).uniq).to eq(['pending'])
+    end
+
+    it "initializes the counter caches" do
+      stocktake.populate_revisions!
+      stocktake.reload
+
+      expect(stocktake.counts).to eq(0)
+      expect(stocktake.gains).to eq(0)
+      expect(stocktake.losses).to eq(0)
+      expect(stocktake.warnings).to eq(3)
+    end
+
+    it "turns off computed properties during process" do
+      expect(Stocktake).to receive(:watcher_off).once
+      stocktake.populate_revisions!
     end
 
     it "doesn't modify an existing revision for a package" do
