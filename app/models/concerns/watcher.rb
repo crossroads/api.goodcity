c7887c3f2dd7b24f4fdb238d9f3f37d1514311f9
Patrick R <patrick.rabier@gmail.com>
2021-01-14 10:24:50 +0800

    :lock: GCW-3444 ADD: Thread safety on the Watcher lock

--- app/models/concerns/watcher.rb
+++ app/models/concerns/watcher.rb
@@ -2,19 +2,17 @@ module Watcher
   extend ActiveSupport::Concern
 
   included do
-    @@watcher_enabled = true
+    thread_mattr_accessor :watcher_enabled
+
+    self.watcher_enabled = true
 
     class << self
-      def watcher_enabled?
-        @@watcher_enabled 
-      end
-
       def watch(model_klasses, on: [:create, :update, :destroy, :touch], timing: :after)
         klass = self
         [model_klasses].flatten.uniq.each do |model_kass|
           on.each do |event|
             model_kass.set_callback event, timing, ->(rec) {
-              if klass.watcher_enabled?
+              if klass.watcher_enabled
                 yield(rec, event)
               end
             }
@@ -23,10 +21,10 @@ module Watcher
       end
 
       def watcher_off
-        @@watcher_enabled = false
+        self.watcher_enabled = false
         yield
       ensure
-        @@watcher_enabled = true
+        self.watcher_enabled = true
       end
     end
   end
