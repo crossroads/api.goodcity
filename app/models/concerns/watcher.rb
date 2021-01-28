398681bb58d2d32a6bb9b47fc536613393651248
Patrick R <patrick.rabier@gmail.com>
2021-01-28 10:47:22 +0800

    :pencil2: GCW-3489 Remove unused variable

--- app/models/concerns/watcher.rb
+++ app/models/concerns/watcher.rb
@@ -23,7 +23,6 @@ module Watcher
   included do
     class << self
       def watch(model_klasses, on: [:create, :update, :destroy, :touch], timing: :after)
-        klass = self
         [model_klasses].flatten.uniq.each do |model_kass|
           on.each do |event|
             model_kass.set_callback event, timing, ->(rec) { yield(rec, event) }
