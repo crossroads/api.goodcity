49c816e0f0e07f513ea1e1ff72e040cd75779c49
Patrick R <patrick.rabier@gmail.com>
2021-01-28 10:43:28 +0800

    :bug: GCW-3489: Remove watcher toggle
    Stocktake revision population code now uses a single query, that allows:
    
    - the removal of callback toggle on Watcher, that was causing the bug
    - improved performance as it's all done in a single query now

--- app/models/concerns/watcher.rb
+++ app/models/concerns/watcher.rb
@@ -21,29 +21,14 @@ module Watcher
   extend ActiveSupport::Concern
 
   included do
-    thread_mattr_accessor :watcher_enabled
-
-    self.watcher_enabled = true
-
     class << self
       def watch(model_klasses, on: [:create, :update, :destroy, :touch], timing: :after)
         klass = self
         [model_klasses].flatten.uniq.each do |model_kass|
           on.each do |event|
-            model_kass.set_callback event, timing, ->(rec) {
-              if klass.watcher_enabled
-                yield(rec, event)
-              end
-            }
+            model_kass.set_callback event, timing, ->(rec) { yield(rec, event) }
           end
         end
-      end
-
-      def watcher_off
-        self.watcher_enabled = false
-        yield
-      ensure
-        self.watcher_enabled = true
       end
     end
   end
