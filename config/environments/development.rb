1c09fdf5bfed10bc04547a8414d373fa33ae4a38
bharat619 <bharat.h@kiprosh.com>
2021-01-29 19:51:05 +0530

    :tada: Initial commit for rails 6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,12 +9,31 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
+  else
   config.action_controller.perform_caching = false
+
+  config.cache_store = :null_store
+end
+
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -22,7 +41,21 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  # Raises error for missing translations
+   # Highlight code that triggered database queries in logs.
+   config.active_record.verbose_query_logs = true
+
+   # Debug mode disables concatenation and preprocessing of assets.
+   # This option may cause significant delays in view rendering with a large
+   # number of complex assets.
+   config.assets.debug = true
+
+   # Suppress logger output for asset requests.
+   config.assets.quiet = true
+
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
   config.action_view.raise_on_missing_translations = true
 
   # Places email in tmp/mails/
