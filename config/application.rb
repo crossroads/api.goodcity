7a79f92f860a75bad3e5c71108e074026f88e9f2
bharat619 <bharat.h@kiprosh.com>
2021-01-29 19:51:05 +0530

    :tada: Initial commit for rails 6

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,15 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
+require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
+require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-# require "action_view/railtie"
+require "action_view/railtie"
+# require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -15,6 +19,8 @@ Bundler.require(*Rails.groups)
 
 module GoodCityServer
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
