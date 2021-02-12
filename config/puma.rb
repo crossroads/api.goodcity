94aeb62f258eb44f90cda980e035c2b196dcef0c
bharat619 <bharat.h@kiprosh.com>
2021-02-12 09:33:37 +0530

    :wrench: Update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -35,27 +35,3 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
-
-# TODO: CONFIRM IS THINGS WORK ON REMOVING BELOW LINES
-
-preload_app!
-
-rackup      DefaultRackup
-port        ENV['PORT']      || 3000
-environment ENV['RAILS_ENV'] || 'development'
-
-bind "unix://#{File.expand_path('tmp/sockets/puma.sock')}"
-
-on_worker_boot do
-  # worker specific setup
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 16
-    ActiveRecord::Base.establish_connection(config)
-  end
-  # Reconnect to Redis
-  #if defined?(Resque)
-  #   Resque.redis = ENV["<redis-uri>"] || "redis://127.0.0.1:6379"
-  #end
-end
