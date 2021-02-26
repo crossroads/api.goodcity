487ff5be203f5b450340ef6cd3247b72f110f87a
abulsayyad123 <abul.a@kiprosh.com>
2021-02-26 16:38:40 +0530

    added more specs for canned_response

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_065627) do
+ActiveRecord::Schema.define(version: 2021_01_18_111336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -106,7 +106,6 @@ ActiveRecord::Schema.define(version: 2021_02_22_065627) do
     t.string "respondable_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "message_type", default: "canned"
   end
 
   create_table "companies", id: :serial, force: :cascade do |t|
