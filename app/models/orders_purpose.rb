520d001927f45ef149f5a512cde210adcff5c7a3
swatijadhav <swati.nakshtra17@gmail.com>
2021-06-07 08:06:27 +0530

    remove more unwanted code from models

--- app/models/orders_purpose.rb
+++ app/models/orders_purpose.rb
@@ -1,5 +1,4 @@
 class OrdersPurpose < ApplicationRecord
-
   belongs_to :order
   belongs_to :purpose
 end
--- app/models/subpackage_type.rb
+++ app/models/subpackage_type.rb
@@ -1,5 +1,4 @@
 class SubpackageType < ApplicationRecord
-
   belongs_to :package_type
   belongs_to :child_package_type, class_name: :PackageType, foreign_key: :subpackage_type_id
 
