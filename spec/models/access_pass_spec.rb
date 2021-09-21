763d6986eb74522a15c0f97732dc2b296635a414
swatijadhav <swati.nakshtra17@gmail.com>
2021-09-21 07:38:59 +0530

    GCW-3688 test cases for access-pass model

--- spec/models/access_pass_spec.rb
+++ spec/models/access_pass_spec.rb
@@ -15,4 +15,35 @@ RSpec.describe AccessPass, type: :model do
     it{ is_expected.to have_db_column(:generated_at).of_type(:datetime)}
     it{ is_expected.to have_db_column(:access_expires_at).of_type(:datetime)}
   end
+
+  describe ".refresh_pass" do
+    it "would reset access-key" do
+      access_pass = create :access_pass
+      access_key = access_pass.access_key
+
+      expect{
+        access_pass.refresh_pass
+      }.to change(access_pass, :access_key)
+    end
+  end
+
+  describe ".is_valid_pass?" do
+    it "should return boolean value based on access-key generation-time" do
+      access_pass = create :access_pass
+      expect(access_pass.is_valid_pass?).to be_truthy
+
+      access_pass.update_column(:generated_at, 10.minutes.ago)
+      expect(access_pass.is_valid_pass?).to be_falsy
+    end
+  end
+
+  describe "#find_valid_pass" do
+    it "should return valid non-expired pass" do
+      access_pass = create :access_pass
+      expect(AccessPass.find_valid_pass(access_pass.access_key)).to eq(access_pass)
+
+      access_pass.update_column(:generated_at, 10.minutes.ago)
+      expect(AccessPass.find_valid_pass(access_pass.access_key)).to be_falsy
+    end
+  end
 end
