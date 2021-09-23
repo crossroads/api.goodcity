1fe05c73fb00c8cc8bf5d8732a71b4fee59198fd
swatijadhav <swati.nakshtra17@gmail.com>
2021-09-23 15:25:36 +0530

    GCW-3688 fixed access-pass-roles constant

--- app/models/access_passes_role.rb
+++ app/models/access_passes_role.rb
@@ -2,8 +2,11 @@ class AccessPassesRole < ApplicationRecord
   belongs_to :access_pass
   belongs_to :role
 
-  ALOWED_ROLES = [Role::ROLE_NAMES[:stock_fulfilment], Role::ROLE_NAMES[:order_fulfilment]]
-  ALOWED_ROLES_IDS = ALOWED_ROLES.map {|role| Role.find_by(name: role).try(:id) }
+  ALLOWED_ROLES = [Role::ROLE_NAMES[:stock_fulfilment], Role::ROLE_NAMES[:order_fulfilment]]
 
-  validates :role_id, inclusion: { in: ALOWED_ROLES_IDS }, allow_nil: false
+  validates :role_id, inclusion: { in: Proc.new {  AccessPassesRole.allowed_roles_ids } }, allow_nil: false
+
+  def self.allowed_roles_ids
+    Role.where(name: ALLOWED_ROLES).pluck(:id)
+  end
 end
