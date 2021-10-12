e687feb35317f93b38fd4eb1b2b269a4290817ac
Steve Kenworthy <steveyken@gmail.com>
2021-10-12 22:00:19 +0800

    Improved package_types load time from 1700ms to 700ms on dev machine.

--- app/models/package_type.rb
+++ app/models/package_type.rb
@@ -3,18 +3,13 @@ class PackageType < ApplicationRecord
 
   belongs_to :location
   has_many :subpackage_types
-  has_many :child_package_types, through: :subpackage_types,
-    source: :child_package_type
+  has_many :child_package_types, through: :subpackage_types, source: :child_package_type
 
-  has_many :default_subpackage_types, -> { where is_default: true },
-    class_name: :SubpackageType
-  has_many :default_child_package_types, through: :default_subpackage_types,
-    source: :default_child_package_type
+  has_many :default_subpackage_types, -> { where is_default: true }, class_name: :SubpackageType
+  has_many :default_child_package_types, through: :default_subpackage_types, source: :default_child_package_type
 
-  has_many :other_subpackage_types, -> { where is_default: false },
-    class_name: :SubpackageType
-  has_many :other_child_package_types, through: :other_subpackage_types,
-    source: :other_child_package_type
+  has_many :other_subpackage_types, -> { where is_default: false }, class_name: :SubpackageType
+  has_many :other_child_package_types, through: :other_subpackage_types, source: :other_child_package_type
 
   has_many :items, inverse_of: :package_type
   has_many :packages, inverse_of: :package_type
@@ -32,7 +27,7 @@ class PackageType < ApplicationRecord
   scope :pallet_types, -> { where(allow_pallet: true) }
 
   scope :with_eager_load, -> {
-    eager_load([:location])
+    eager_load([:location, :subpackage_types, :other_child_package_types, :default_child_package_types, :other_subpackage_types, :default_subpackage_types])
   }
 
   private
