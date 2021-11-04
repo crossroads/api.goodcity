ed95b6d9152da8002f39f341ba1fc6c83fe86366
Steve Kenworthy <steveyken@gmail.com>
2021-11-04 15:41:16 +0800

    Removed duplicate mail smtp settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,16 +60,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.smtp_settings = {
-    :address  => "smtp.postmarkapp.com",
-    :port  => 587,
-    :user_name => ENV['POSTMARK_API_KEY'],
-    :password  => ENV['POSTMARK_API_KEY'],
-    :authentication  => :plain,
-    :enable_starttls_auto => true,
-    :domain => 'postmarkapp.com'
-  }
-
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
