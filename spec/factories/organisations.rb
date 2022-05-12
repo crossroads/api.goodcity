19ba9e407447c1139abfdee45d05da14def531a4
Steve Kenworthy <steveyken@gmail.com>
2022-05-12 11:46:48 +0800

    Better data for organisations

--- spec/factories/organisations.rb
+++ spec/factories/organisations.rb
@@ -2,12 +2,12 @@
 
 FactoryBot.define do
   factory :organisation do
-    sequence(:name_en) { |n| "#{FFaker::Company.name}#{n}" }
-    name_zh_tw { "NGO" + FFaker::Company.name }
-    description_en { FFaker::Lorem.words(rand(2..10)).join(' ') }
-    description_zh_tw { FFaker::Lorem.words(rand(2..10)).join(' ') }
-    registration { (rand(89)+10).to_s+"/"+(rand(10000)+10000).to_s }
-    website { FFaker::Internet.http_url }
+    sequence(:name_en) { FFaker::Company.name }
+    name_zh_tw         { name_en }
+    description_en     { FFaker::Lorem.words(rand(2..10)).join(' ') }
+    description_zh_tw  { FFaker::Lorem.words(rand(2..10)).join(' ') }
+    registration       { (rand(89)+10).to_s+"/"+(rand(10000)+10000).to_s }
+    website            { FFaker::Internet.http_url }
     association :country
     association :district
     association :organisation_type
