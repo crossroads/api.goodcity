2966f8b15ec6da4cede52f53f00afc533a9d3468
Steve Kenworthy <steveyken@gmail.com>
2022-05-12 11:41:20 +0800

    Organisation Types are using seeds

--- spec/factories/organisation_types.rb
+++ spec/factories/organisation_types.rb
@@ -1,8 +1,20 @@
 FactoryBot.define do
   factory :organisation_type do
-    name_en { FFaker::Company.name }
-    name_zh_tw { FFaker::Company.name }
-    category_en { FFaker::Name.name }
-    category_zh_tw { FFaker::Name.name }
+    name_en         { org_type[:name_en] }
+    name_zh_tw      { org_type[:name_zh_tw] }
+    category_en     { org_type[:category_en] }
+    category_zh_tw  { org_type[:category_zh_tw] }
+
+    initialize_with { OrganisationType.find_or_initialize_by(name_en: name_en) }
+    transient do
+      org_type { generate(:organisation_types) }
+    end
+
   end
+
+  sequence(:organisation_types) do |n|
+    @organisation_types = YAML.load_file("#{Rails.root}/db/organisation_types.yml")
+    @organisation_types[n%@organisation_types.size]
+  end
+
 end
