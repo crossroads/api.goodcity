b965317ca40c60c0179905c26603ec70e4b79bc7
Stephen Kenworthy <steveyken@gmail.com>
2023-03-17 14:51:27 +0800

    Avoid duplication during demo data creation

--- spec/factories/identity_types.rb
+++ spec/factories/identity_types.rb
@@ -2,8 +2,13 @@
 
 FactoryBot.define do
   factory :identity_type do
-    sequence(:identifier) { |n| "Identity Type #{n}" }
-    name_en               { identifier }
-    name_zh_tw            { identifier }
+    sequence(:identifier) { |n| seq.keys.sort[n%seq.keys.size] }
+    name_en               { seq[identifier][:name_en] }
+    name_zh_tw            { seq[identifier][:name_zh_tw] }
+    initialize_with       { IdentityType.find_or_initialize_by(name_en: name_en) } # avoid duplicates
+
+    transient do
+      seq { @identity_types ||= YAML.load_file("#{Rails.root}/db/identity_types.yml") }
+    end
   end
 end
