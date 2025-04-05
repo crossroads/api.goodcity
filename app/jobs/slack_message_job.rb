e1b4d69ed5294d24e502d50d1233b1f5afc0b44f
Steve <steveyken@gmail.com>
2025-04-05 11:49:53 +0800

    Disable sending slack jobs using old API

--- app/jobs/slack_message_job.rb
+++ app/jobs/slack_message_job.rb
@@ -2,7 +2,7 @@ class SlackMessageJob < ActiveJob::Base
   queue_as :low
 
   def perform(message, channel)
-    client = Slack::Web::Client.new
-    client.chat_postMessage(channel: channel, text: message, as_user: true)
+    # client = Slack::Web::Client.new
+    # client.chat_postMessage(channel: channel, text: message, as_user: true)
   end
 end
