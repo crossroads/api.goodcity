ab2fcf5ece56551087aa4f191098a327c90b4868
Steve <steveyken@gmail.com>
2025-12-05 13:55:15 +0800

    Disable risk check on Twilio SMSs sent by our platform.

--- app/jobs/twilio_job.rb
+++ app/jobs/twilio_job.rb
@@ -5,7 +5,7 @@ class TwilioJob < ActiveJob::Base
   def perform(options)
     if send_to_twilio?(options)
       client = Twilio::REST::Client.new(twilio_conf[:account_sid], twilio_conf[:auth_token])
-      client.messages.create({ from: twilio_conf[:phone_number] }.merge(options))
+      client.messages.create({ from: twilio_conf[:phone_number], risk_check: 'disable' }.merge(options))
       Rails.logger.info(class: self.class.name, msg: "SMS sent", mobile: options[:to], body: options[:body])
     elsif Rails.env.staging?
       # We'll send the SMS text via email to mailcatcher instead
